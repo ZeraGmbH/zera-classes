8117ac02c02fe059a47e9cca3c0981aaa13d9738
Peter Lohmer <peterlohmer01@gmail.com>
2013-07-17 13:42:03 +0200

    i2cutils.cpp: bugfix, file descriptor had to be closed in case of error condition
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- zera-comm/zera-i2c/i2cutils.cpp
+++ zera-comm/zera-i2c/i2cutils.cpp
@@ -13,19 +13,25 @@ int I2CTransfer(QString deviceNode,int i2cadr, int debugLevel, i2c_rdwr_ioctl_da
         if (debugLevel & 1)  syslog(LOG_ERR,"error opening i2c device: %s\n",deviceNode.toLatin1().data());
         return(1); // error connection
     }
-    if ( ioctl(fd,I2C_RETRIES,0) < 0 ) {
+    if ( ioctl(fd,I2C_RETRIES,0) < 0 )
+    {
+        close(fd);
         if (debugLevel & 1)  syslog(LOG_ERR,"error setting retries of i2c device: %s\n",deviceNode.toLatin1().data());
         return(1); // error connection
     }
-    if ( ioctl(fd,I2C_TIMEOUT,500) < 0 ) {
+    if ( ioctl(fd,I2C_TIMEOUT,500) < 0 )
+    {
+        close(fd);
         if (debugLevel & 1)  syslog(LOG_ERR,"error setting retries of i2c device: %s\n",deviceNode.toLatin1().data());
         return(1); // error connection
     }
-    if ( ioctl(fd,I2C_RDWR,iodata) < 0 ) {
+    if ( ioctl(fd,I2C_RDWR,iodata) < 0 )
+    {
         if (debugLevel & 1)  syslog(LOG_ERR,"error read/write i2c slave at adress: %d\n",i2cadr);
         close(fd);
         return(2); // error device
     }
+
     if (debugLevel & 2)  syslog(LOG_INFO,"read/write i2c slave at adress: %d done\n",i2cadr);
     close(fd);
     return(0); // acknowledge
