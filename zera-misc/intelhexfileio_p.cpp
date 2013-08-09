f53bbdc12422bd86af332b7c61d7683f6fd07757
Peter Lohmer <peterlohmer01@gmail.com>
2013-08-09 10:44:42 +0200

    intelhexfileio_p.cpp: removed debug information
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- zera-misc/intelhexfileio_p.cpp
+++ zera-misc/intelhexfileio_p.cpp
@@ -1,7 +1,5 @@
 #include <QFile>
 #include <QList>
-#include <QDebug>
-
 #include "intelhexfileio_p.h"
 
 
@@ -113,7 +111,6 @@ bool cIntelHexFileIOPrivate::ReadHexFile(const QString& fileName)
 #define BUFF_LEN 1024
             char FileBuffer[BUFF_LEN];
             nByteRead = file.readLine(FileBuffer, BUFF_LEN);
-            qDebug() << nByteRead;
             if (nByteRead <= 0) // in case of error conditions
                 nByteRead = 0; // we read nothing
             else
