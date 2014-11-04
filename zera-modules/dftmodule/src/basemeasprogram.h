4603764555e0cf1d2895b199546a0670b8b55d87
Peter Lohmer <peterlohmer01@gmail.com>
2014-11-04 16:18:27 +0100

    dftmodule: adapted to new proxy and built in synchronisation for dspserver connection
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- zera-modules/dftmodule/src/basemeasprogram.h
+++ zera-modules/dftmodule/src/basemeasprogram.h
@@ -32,7 +32,7 @@ class cBaseMeasProgram: public cModuleActivist
     Q_OBJECT
 
 public:
-    cBaseMeasProgram(Zera::Proxy::cProxy* proxy, VeinPeer* peer, Zera::Server::cDSPInterface* iface);
+    cBaseMeasProgram(Zera::Proxy::cProxy* proxy, VeinPeer* peer);
     virtual ~cBaseMeasProgram();
     virtual void generateInterface() = 0; // here we export our interface (entities)
     virtual void deleteInterface() = 0; // we delete interface in case of reconfiguration
@@ -50,14 +50,15 @@ protected:
     // the module creates a central dsp interface and forwards this
     // the reason behind this is, that dsp server generates a new "environment" for each
     // new client. but we want to access the same environment's value from all over the module.
-    Zera::Server::cDSPInterface* m_pDSPIFace; // our interface to dsp
+    Zera::Server::cDSPInterface* m_pDSPInterFace; // our interface to dsp
+    Zera::Proxy::cProxyClient *m_pDspClient;
     Zera::Server::cRMInterface* m_pRMInterface;
+    Zera::Proxy::cProxyClient* m_pRMClient;
     // we hold an interface for every channel because it could be possible that our measuring
     // channels are spread over several pcb's
     QList<Zera::Server::cPCBInterface*> m_pcbIFaceList; // our interface(s) to pcb
     // so we must also keep a list of pcb client's we can
     QList<Zera::Proxy::cProxyClient*> m_pcbClientList; // our clients for pcb interfaces
-    Zera::Proxy::cProxyClient* m_pRMClient;
 
     QVector<float> m_ModuleActualValues; // a modules actual values
     QHash<quint32, int> m_MsgNrCmdList;
