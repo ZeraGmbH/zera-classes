2d8e33e245ef73deaf2fd28a7220e53452cb8ea3
Peter Lohmer <peterlohmer01@gmail.com>
2015-04-07 10:54:22 +0200

    zera-basemodule/secinputinfo.h: added class for standard error calculator input info
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

---
+++ zera-basemodule/secinputinfo.h
@@ -0,0 +1,26 @@
+#ifndef SECINPUTINFO_H
+#define SECINPUTINFO_H
+
+
+#include <QObject>
+#include <QString>
+
+#include "socket.h"
+
+namespace Zera {
+namespace Server {
+    class cPCBInterface;
+}
+}
+
+struct cSecInputInfo
+{
+    QString name; // the input's system name
+    QString alias; // the input's alias
+    QString resource; // the resource from which this input is served
+    quint8 muxchannel; // what must be set to select this input
+    cSocket pcbServersocket; // which pcbserver provides this input
+    Zera::Server::cPCBInterface* pcbIFace; // our interface to this server
+};
+
+#endif // SECINPUTINFO_H
