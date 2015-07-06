a55e0347ac6838069df2f86a5d4beb8707886d1e
Peter Lohmer <peterlohmer01@gmail.com>
2015-07-06 10:13:13 +0200

    Revert "moduleparameter.cpp,h: extended moduleparameter class"
    This reverts commit 6392c8fb497b33d124c9b35f7a07d1a929f7c403.

--- zera-basemodule/moduleparameter.h
+++ zera-basemodule/moduleparameter.h
@@ -7,13 +7,6 @@
 
 class VeinEntity;
 class VeinPeer;
-class cParamValidator;
-
-namespace VeinComponent
-{
-    class ComponentData;
-}
-
 
 // we use this class if we need an interface entity for parameter for input/output incl. description
 
@@ -21,22 +14,19 @@ class cModuleParameter: public QObject
 {
     Q_OBJECT
 public:
-    cModuleParameter(VeinPeer* peer,QString name, QVariant initval, bool readonly = false, cParamValidator *pValidator = 0);
+    cModuleParameter(VeinPeer* peer,QString name, QVariant initval, bool readonly = false);
     ~cModuleParameter();
     void setData(QVariant val);
     QVariant getData();
     QString getName();
-    bool paramTransaction(VeinComponent::ComponentData *cdata); // returns true if cdata belongs to this parameter
 
 signals:
     void updated(QVariant);
-    void updated(VeinComponent::ComponentData*);
 
 private:
     VeinPeer* m_pPeer;
     QString m_sName;
     VeinEntity *m_pParEntity;
-    cParamValidator *m_pParamValidator;
 };
 
 #endif // MODULEPARAMETER_H
