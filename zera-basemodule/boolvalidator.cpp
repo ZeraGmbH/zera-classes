c1d3f86b18b6f0580df8b3bb9b6b66bd1e7632dd
Peter Lohmer <peterlohmer01@gmail.com>
2016-08-17 15:42:22 +0200

    zera-basemodule/boolvalidator.cpp: bugfix ...only 0 or 1 is allowed for boolean instead of false or true
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- zera-basemodule/boolvalidator.cpp
+++ zera-basemodule/boolvalidator.cpp
@@ -1,6 +1,6 @@
 #include <QJsonArray>
 #include <QJsonObject>
-
+#include <QMetaType>
 #include "boolvalidator.h"
 
 
@@ -9,8 +9,13 @@ bool cBoolValidator::isValidParam(QVariant newValue)
    bool ok;
    int onoff;
 
-   onoff = newValue.toInt(&ok);
-   return (ok && ((onoff == 0) || (onoff ==1))); // we only accept 0,1
+   if (newValue.type() == QMetaType::Int)
+   {
+       onoff = newValue.toInt(&ok);
+       return (ok && ((onoff == 0) || (onoff ==1))); // we only accept 0,1
+   }
+   else
+       return false;
 }
 
 
