cd25874904a29d63cea4b3c0ea70d198a8560f8e
Peter Lohmer <peterlohmer@gmail.com>
2018-03-26 09:29:06 +0200

    zera-basemodule/rangeinfo.h: added mmode and sensor enums
    Signed-off-by: Peter Lohmer <peterlohmer@gmail.com>

--- zera-basemodule/rangeinfo.h
+++ zera-basemodule/rangeinfo.h
@@ -3,6 +3,22 @@
 
 #include <QObject>
 
+enum MMode
+{
+    modeAC = 1,
+    modeHF = 2,
+    modeDC = 4,
+    modeADJ = 8
+};
+
+
+enum SensorType
+{
+    Direct = 0x100,
+    Clamp = 0x200
+};
+
+
 struct cRangeInfo
 {
     QString name;
