39ae95219fca03b26d90a24f5ef0751f2d4078af
Peter Lohmer <peterlohmer@gmail.com>
2018-08-23 11:57:29 +0200

    zera-basemodule/rangeinfo.h: added copy constructor
    Signed-off-by: Peter Lohmer <peterlohmer@gmail.com>

--- zera-basemodule/rangeinfo.h
+++ zera-basemodule/rangeinfo.h
@@ -19,8 +19,12 @@ enum SensorType
 };
 
 
-struct cRangeInfo
+class cRangeInfo
 {
+public:
+    cRangeInfo(){}
+    cRangeInfo(const cRangeInfo &obj){name=obj.name;type=obj.type;avail=obj.avail;alias=obj.alias;urvalue=obj.urvalue;rejection=obj.rejection;ovrejection=obj.ovrejection;adcrejection=obj.adcrejection;}
+
     QString name;
     qint32 type;
     bool avail;
