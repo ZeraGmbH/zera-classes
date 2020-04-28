c646ac264d33ab99cafe653401e934bd10297ea7
Andreas Müller <schnitzeltony@gmail.com>
2020-04-28 19:21:40 +0200

    i2cutils: stylize - no functional change
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-comm/zera-i2c/i2cutils.cpp
+++ zera-comm/zera-i2c/i2cutils.cpp
@@ -11,29 +11,41 @@ int I2CTransfer(QString deviceNode,int i2cadr, int debugLevel, i2c_rdwr_ioctl_da
 {
     int fd;
     if ( (fd = open(deviceNode.toLatin1().data(),O_RDWR)) < 0 ) {
-        if (debugLevel & 1)  syslog(LOG_ERR,"error opening i2c device: %s / open() errno %i\n",deviceNode.toLatin1().data(), errno);
+        if (debugLevel & 1) {
+            syslog(LOG_ERR,"error opening i2c device: %s / open() errno %i\n",
+                   deviceNode.toLatin1().data(), errno);
+        }
         return(1); // error connection
     }
-    if ( ioctl(fd,I2C_RETRIES,0) < 0 )
-    {
+    if (ioctl(fd,I2C_RETRIES,0) < 0) {
         close(fd);
-        if (debugLevel & 1)  syslog(LOG_ERR,"error setting retries of i2c device: %s / ioctl(fd,I2C_RETRIES,0) errno %i\n",deviceNode.toLatin1().data(), errno);
+        if (debugLevel & 1) {
+            syslog(LOG_ERR,"error setting retries of i2c device: %s / ioctl(fd,I2C_RETRIES,0) errno %i\n",
+                   deviceNode.toLatin1().data(), errno);
+        }
         return(1); // error connection
     }
-    if ( ioctl(fd,I2C_TIMEOUT,500) < 0 )
-    {
+    if (ioctl(fd,I2C_TIMEOUT,500) < 0) {
         close(fd);
-        if (debugLevel & 1)  syslog(LOG_ERR,"error setting retries of i2c device: %s / ioctl(fd,I2C_TIMEOUT,500) errno %i\n",deviceNode.toLatin1().data(), errno);
+        if (debugLevel & 1) {
+            syslog(LOG_ERR,"error setting retries of i2c device: %s / ioctl(fd,I2C_TIMEOUT,500) errno %i\n",
+                   deviceNode.toLatin1().data(), errno);
+        }
         return(1); // error connection
     }
-    if ( ioctl(fd,I2C_RDWR,iodata) < 0 )
-    {
-        if (debugLevel & 1)  syslog(LOG_ERR,"error read/write i2c slave at adress: %d / ioctl(fd,I2C_RDWR,iodata) errno %i\n",i2cadr, errno);
+    if (ioctl(fd,I2C_RDWR,iodata) < 0) {
+        if (debugLevel & 1) {
+            syslog(LOG_ERR,"error read/write i2c slave at adress: %d / ioctl(fd,I2C_RDWR,iodata) errno %i\n",
+                   i2cadr, errno);
+        }
         close(fd);
         return(2); // error device
     }
 
-    if (debugLevel & 2)  syslog(LOG_INFO,"read/write i2c slave at adress: %d done\n",i2cadr);
+    if (debugLevel & 2) {
+        syslog(LOG_INFO,"read/write i2c slave at adress: %d done\n",
+               i2cadr);
+    }
     close(fd);
     return(0); // acknowledge
 }
