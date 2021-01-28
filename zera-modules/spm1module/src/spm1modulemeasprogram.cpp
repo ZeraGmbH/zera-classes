c882fd4759308086d68b2cb57c69ad4e6668be07
Andreas Müller <schnitzeltony@gmail.com>
2021-01-28 15:39:38 +0100

    sem/spm-module: Fix ECALCSTATUS::READY
    At current situation this fixes nothing but if we invent a multiple measurement
    here (as continuous measurement in sec) we'd face odd results.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/spm1module/src/spm1modulemeasprogram.cpp
+++ zera-modules/spm1module/src/spm1modulemeasprogram.cpp
@@ -887,7 +887,7 @@ void cSpm1ModuleMeasProgram::setUnits()
     // In case measurement is running, values are updated properly on next
     // interrupt (tested with vf-debugger). For a measuremnt finished we have to
     // recalc results with new units
-    if(m_nStatus == ECALCSTATUS::READY) {
+    if(m_nStatus & ECALCSTATUS::READY) {
         setEMResult();
     }
     m_pModule->exportMetaData();
