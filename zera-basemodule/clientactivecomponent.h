7d3c73ac6ba8c849128d26a245026c722aa810a4
Andreas Müller <schnitzeltony@gmail.com>
2021-02-16 15:19:44 +0100

    Add ClientActiveComponent: Clients can inform modules of active watching
    * This is the first step for dynamic module performance scaling
    * All details of why and how are written in class documentation
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-basemodule/clientactivecomponent.h
@@ -0,0 +1,60 @@
+#ifndef CLIENTACTIVECOMPONENT_H
+#define CLIENTACTIVECOMPONENT_H
+
+#include <QObject>
+#include <QTimer>
+#include <QVariant>
+
+class cVeinModuleParameter;
+
+/**
+ * @brief ClientActiveComponent is a simple helper fo performance/energy
+ * management of modules that create status data a view wants to see
+ * realtime-alike (but that costs..). Once the view is no more visible,
+ * module can go down to slow mode. To achieve dynamic switching, module needs
+ * to be informed about demanding clients.
+ * And it works like this:
+ * * A module creates an extra integer component
+ * * This component is passed to init() together with an incative timeout
+ *   duration
+ * * A visible view (or any other client wanting full-speed) increments
+ *   the component peroidically so that ClientActiveComponent does not
+ *   run into timeout
+ * * Module is notified by signal clientActiveStateChanged or can query state
+ *   by areClientsActive().
+ * Simple he?
+ */
+class ClientActiveComponent : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ClientActiveComponent(QObject *parent = nullptr);
+    /**
+     * @brief init: Init client notification component
+     * @param component: The component to watch for client changes
+     * @param iInactiveTimeoutMs: Maximum time inactive clients are detected as gone
+     */
+    void init(cVeinModuleParameter* component, int iInactiveTimeoutMs = 2000);
+    /**
+     * @brief areClientsActive: Client activity getter
+     * @return guess...
+     */
+    bool areClientsActive() { return m_bClientActive; }
+signals:
+    /**
+     * @brief clientActiveStateChanged: Once the client active state changes, this signal is fired
+     * @param bActive
+     */
+    void clientActiveStateChanged(bool bActive);
+
+private slots:
+    void onClientNotification(QVariant);
+    void onClientTimeout();
+
+private:
+    cVeinModuleParameter* m_pClientActiveNotifyPar = nullptr;
+    bool m_bClientActive = false;
+    QTimer m_inactiveTimer;
+};
+
+#endif // CLIENTACTIVECOMPONENT_H
