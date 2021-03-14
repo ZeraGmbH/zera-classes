6dd4c9ca1674a3f1b0519b9e2395ef62a9c4a48c
Andreas Müller <schnitzeltony@gmail.com>
2021-03-14 12:59:08 +0100

    sec1/multi: Introduce error range calculation
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sec1module/src/sec1modulemeasprogram.cpp
+++ zera-modules/sec1module/src/sec1modulemeasprogram.cpp
@@ -1883,6 +1883,10 @@ void cSec1ModuleMeasProgram::MultipleResultHelper::clear()
     m_iCountPass = 0;
     m_iCountUnfinish = 0;
     m_iCountFail = 0;
+    m_iIdxMinValue = -1;
+    m_iIdxMaxValue = -1;
+    m_fMinError = qQNaN();
+    m_fMaxError = qQNaN();
     m_jsonLimitArray = QJsonArray();
     m_fLastLowerLimit = qQNaN();
     m_fLastUpperLimit = qQNaN();
@@ -1937,21 +1941,32 @@ void cSec1ModuleMeasProgram::MultipleResultHelper::append(const double fResult,
     if(eRating == ECALCRESULT::RESULT_UNFINISHED) { // yes have seen that on HK
         value = -100.0;
     }
+    // value has been added above
+    int iResultCount = m_jsonResultArray.count();
+    // range
+    if(value < m_fMinError || iResultCount == 1) {
+        m_fMinError = value;
+        m_iIdxMinValue = iResultCount-1;
+    }
+    if(value > m_fMaxError || iResultCount == 1) {
+        m_fMaxError = value;
+        m_iIdxMaxValue = iResultCount-1;
+    }
     // mean value
     m_fAccumulSum += value;
-    double fResultCount = m_jsonResultArray.count();
+    double fResultCount = iResultCount;
     m_fMeanValue = m_fAccumulSum / fResultCount;
 
     // standard deviations: There have been loads of discussons about the n or
     // n-1 in the past. To avoid that: calc both
     double quadratDevSum = 0.0;
-    for(int idx=0; idx<m_jsonResultArray.count(); ++idx) {
+    for(int idx=0; idx<iResultCount; ++idx) {
         double currValue = m_jsonResultArray[idx].toObject()["V"].toDouble();
         double currDeviation = currValue - m_fMeanValue;
         quadratDevSum += currDeviation * currDeviation;
     }
     m_fStdDevn = sqrt(quadratDevSum / fResultCount);
-    if(m_jsonResultArray.count() > 1) {
+    if(iResultCount > 1) {
         m_fStdDevn1 = sqrt(quadratDevSum / (fResultCount - 1.0));
     }
     else {
@@ -1972,12 +1987,15 @@ const QJsonArray &cSec1ModuleMeasProgram::MultipleResultHelper::getJSONLimitsArr
 const QJsonObject cSec1ModuleMeasProgram::MultipleResultHelper::getJSONStatistics()
 {
     QJsonObject jsonObject {
-        {QStringLiteral("countPass"), m_iCountPass },
+        {QStringLiteral("countPass"), m_iCountPass},
         {QStringLiteral("countFail"), m_iCountFail},
-        {QStringLiteral("countUnfinish"), m_iCountUnfinish },
-        {QStringLiteral("mean"), m_fMeanValue  },
-        {QStringLiteral("stddevN"), m_fStdDevn  },
-        {QStringLiteral("stddevN1"), m_fStdDevn1  }
+        {QStringLiteral("countUnfinish"), m_iCountUnfinish},
+        {QStringLiteral("mean"), m_fMeanValue},
+        {QStringLiteral("range"), getCountTotal() > 0 ? m_fMaxError-m_fMinError : qQNaN()},
+        {QStringLiteral("idxMin"), m_iIdxMinValue},
+        {QStringLiteral("idxMax"), m_iIdxMaxValue},
+        {QStringLiteral("stddevN"), m_fStdDevn},
+        {QStringLiteral("stddevN1"), m_fStdDevn1}
     };
     return jsonObject;
 }
