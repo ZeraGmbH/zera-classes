157f5792d3b8095a9bdbeb86ca299bcd8488e624
bhamacher <b.hamacher@zera.de>
2021-08-13 10:34:10 +0200

    Add memory to store last value for inputcomponents.
    The value is valid with the first valueChanged event
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- zera-basemodule/veinmodulecomponentinput.h
+++ zera-basemodule/veinmodulecomponentinput.h
@@ -14,6 +14,11 @@ public:
         :m_nEntityId(entityid), m_sName(name){}
     int m_nEntityId;
     QString m_sName;
+    QVariant value() const;
+    void setValue(const QVariant &value);
+
+private:
+    QVariant m_value;
 
 
 signals:
