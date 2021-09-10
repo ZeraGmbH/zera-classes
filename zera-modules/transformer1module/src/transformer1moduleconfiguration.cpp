1971b2dbe85aba97a4f5328c5c1af0246a6f3555
Andreas Müller <schnitzeltony@gmail.com>
2021-09-10 18:55:27 +0200

    transformer1module: replace Qt4 syntax for signal/slot by Qt5 syntax
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/transformer1module/src/transformer1moduleconfiguration.cpp
+++ zera-modules/transformer1module/src/transformer1moduleconfiguration.cpp
@@ -12,8 +12,8 @@ namespace TRANSFORMER1MODULE
 cTransformer1ModuleConfiguration::cTransformer1ModuleConfiguration()
 {
     m_pTransformer1ModulConfigData = 0;
-    connect(m_pXMLReader, SIGNAL(valueChanged(const QString&)), this, SLOT(configXMLInfo(const QString&)));
-    connect(m_pXMLReader, SIGNAL(finishedParsingXML(bool)), this, SLOT(completeConfiguration(bool)));
+    connect(m_pXMLReader, &Zera::XMLConfig::cReader::valueChanged, this, &cTransformer1ModuleConfiguration::configXMLInfo);
+    connect(m_pXMLReader, &Zera::XMLConfig::cReader::finishedParsingXML, this, &cTransformer1ModuleConfiguration::completeConfiguration);
 }
 
 
--- zera-modules/transformer1module/src/transformer1modulefactory.cpp
+++ zera-modules/transformer1module/src/transformer1modulefactory.cpp
@@ -14,7 +14,7 @@ ZeraModules::VirtualModule* Transformer1ModuleFactory::createModule(Zera::Proxy:
 void Transformer1ModuleFactory::destroyModule(ZeraModules::VirtualModule *module)
 {
     m_ModuleList.removeAll(module);
-    connect(module, SIGNAL(deactivationReady()), module, SIGNAL(moduleDeactivated()));
+    connect(module, &ZeraModules::VirtualModule::deactivationReady, module, &ZeraModules::VirtualModule::moduleDeactivated);
     if (!module->m_DeactivationMachine.isRunning())
         module->m_DeactivationMachine.start();
 }
