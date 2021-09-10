a3ce43fe1e0ce0060cf9614fea409ff3a79694f2
Andreas Müller <schnitzeltony@gmail.com>
2021-09-10 15:59:04 +0200

    sec1module: replace Qt4 syntax for signal/slot by Qt5 syntax
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sec1module/src/sec1module.cpp
+++ zera-modules/sec1module/src/sec1module.cpp
@@ -26,33 +26,33 @@ cSec1Module::cSec1Module(quint8 modnr, Zera::Proxy::cProxy *proxy, int entityId,
     m_sModuleDescription = QString("This module povides a configurable error calculator");
     m_sSCPIModuleName = QString(BaseSCPIModuleName);
 
-    m_ActivationStartState.addTransition(this, SIGNAL(activationContinue()), &m_ActivationExecState);
-    m_ActivationExecState.addTransition(this, SIGNAL(activationContinue()), &m_ActivationDoneState);
-    m_ActivationDoneState.addTransition(this, SIGNAL(activationNext()), &m_ActivationExecState);
-    m_ActivationDoneState.addTransition(this, SIGNAL(activationContinue()), &m_ActivationFinishedState);
+    m_ActivationStartState.addTransition(this, &cSec1Module::activationContinue, &m_ActivationExecState);
+    m_ActivationExecState.addTransition(this, &cSec1Module::activationContinue, &m_ActivationDoneState);
+    m_ActivationDoneState.addTransition(this, &cSec1Module::activationNext, &m_ActivationExecState);
+    m_ActivationDoneState.addTransition(this, &cSec1Module::activationContinue, &m_ActivationFinishedState);
     m_ActivationMachine.addState(&m_ActivationStartState);
     m_ActivationMachine.addState(&m_ActivationExecState);
     m_ActivationMachine.addState(&m_ActivationDoneState);
     m_ActivationMachine.addState(&m_ActivationFinishedState);
     m_ActivationMachine.setInitialState(&m_ActivationStartState);
-    connect(&m_ActivationStartState, SIGNAL(entered()), SLOT(activationStart()));
-    connect(&m_ActivationExecState, SIGNAL(entered()), SLOT(activationExec()));
-    connect(&m_ActivationDoneState, SIGNAL(entered()), SLOT(activationDone()));
-    connect(&m_ActivationFinishedState, SIGNAL(entered()), SLOT(activationFinished()));
+    connect(&m_ActivationStartState, &QState::entered, this, &cSec1Module::activationStart);
+    connect(&m_ActivationExecState, &QState::entered, this, &cSec1Module::activationExec);
+    connect(&m_ActivationDoneState, &QState::entered, this, &cSec1Module::activationDone);
+    connect(&m_ActivationFinishedState, &QState::entered, this, &cSec1Module::activationFinished);
 
-    m_DeactivationStartState.addTransition(this, SIGNAL(deactivationContinue()), &m_DeactivationExecState);
-    m_DeactivationExecState.addTransition(this, SIGNAL(deactivationContinue()), &m_DeactivationDoneState);
-    m_DeactivationDoneState.addTransition(this, SIGNAL(deactivationNext()), &m_DeactivationExecState);
-    m_DeactivationDoneState.addTransition(this, SIGNAL(deactivationContinue()), &m_DeactivationFinishedState);
+    m_DeactivationStartState.addTransition(this, &cSec1Module::deactivationContinue, &m_DeactivationExecState);
+    m_DeactivationExecState.addTransition(this, &cSec1Module::deactivationContinue, &m_DeactivationDoneState);
+    m_DeactivationDoneState.addTransition(this, &cSec1Module::deactivationNext, &m_DeactivationExecState);
+    m_DeactivationDoneState.addTransition(this, &cSec1Module::deactivationContinue, &m_DeactivationFinishedState);
     m_DeactivationMachine.addState(&m_DeactivationStartState);
     m_DeactivationMachine.addState(&m_DeactivationExecState);
     m_DeactivationMachine.addState(&m_DeactivationDoneState);
     m_DeactivationMachine.addState(&m_DeactivationFinishedState);
     m_DeactivationMachine.setInitialState(&m_DeactivationStartState);
-    connect(&m_DeactivationStartState, SIGNAL(entered()), SLOT(deactivationStart()));
-    connect(&m_DeactivationExecState, SIGNAL(entered()), SLOT(deactivationExec()));
-    connect(&m_DeactivationDoneState, SIGNAL(entered()), SLOT(deactivationDone()));
-    connect(&m_DeactivationFinishedState, SIGNAL(entered()), SLOT(deactivationFinished()));
+    connect(&m_DeactivationStartState, &QState::entered, this, &cSec1Module::deactivationStart);
+    connect(&m_DeactivationExecState, &QState::entered, this, &cSec1Module::deactivationExec);
+    connect(&m_DeactivationDoneState, &QState::entered, this, &cSec1Module::deactivationDone);
+    connect(&m_DeactivationFinishedState, &QState::entered, this, &cSec1Module::deactivationFinished);
 
 }
 
@@ -77,9 +77,9 @@ void cSec1Module::setupModule()
     // we only have this activist
     m_pMeasProgram = new cSec1ModuleMeasProgram(this, m_pProxy, m_pConfiguration);
     m_ModuleActivistList.append(m_pMeasProgram);
-    connect(m_pMeasProgram, SIGNAL(activated()), SIGNAL(activationContinue()));
-    connect(m_pMeasProgram, SIGNAL(deactivated()), this, SIGNAL(deactivationContinue()));
-    connect(m_pMeasProgram, SIGNAL(errMsg(QVariant)), m_pModuleErrorComponent, SLOT(setValue(QVariant)));
+    connect(m_pMeasProgram, &cSec1ModuleMeasProgram::activated, this, &cSec1Module::activationContinue);
+    connect(m_pMeasProgram, &cSec1ModuleMeasProgram::deactivated, this, &cSec1Module::deactivationContinue);
+    connect(m_pMeasProgram, &cSec1ModuleMeasProgram::errMsg, m_pModuleErrorComponent, &cVeinModuleErrorComponent::setValue);
 
     for (int i = 0; i < m_ModuleActivistList.count(); i++)
         m_ModuleActivistList.at(i)->generateInterface();
--- zera-modules/sec1module/src/sec1moduleconfiguration.cpp
+++ zera-modules/sec1module/src/sec1moduleconfiguration.cpp
@@ -12,8 +12,8 @@ namespace SEC1MODULE
 cSec1ModuleConfiguration::cSec1ModuleConfiguration()
 {
     m_pSec1ModulConfigData = 0;
-    connect(m_pXMLReader, SIGNAL(valueChanged(const QString&)), this, SLOT(configXMLInfo(const QString&)));
-    connect(m_pXMLReader, SIGNAL(finishedParsingXML(bool)), this, SLOT(completeConfiguration(bool)));
+    connect(m_pXMLReader, &Zera::XMLConfig::cReader::valueChanged, this, &cSec1ModuleConfiguration::configXMLInfo);
+    connect(m_pXMLReader, &Zera::XMLConfig::cReader::finishedParsingXML, this, &cSec1ModuleConfiguration::completeConfiguration);
 }
 
 
