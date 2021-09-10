ffd0aa8e08047d18b730a3128a7466bf88d2fb3d
Andreas Müller <schnitzeltony@gmail.com>
2021-09-10 20:15:38 +0200

    lambdamodule: replace Qt4 syntax for signal/slot by Qt5 syntax
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/lambdamodule/src/lambdamodule.cpp
+++ zera-modules/lambdamodule/src/lambdamodule.cpp
@@ -29,33 +29,33 @@ cLambdaModule::cLambdaModule(quint8 modnr, Zera::Proxy::cProxy* proxy, int entit
     m_sModuleDescription = QString("This module measures configured number of harmonic power values from configured input values");
     m_sSCPIModuleName = QString("%1%2").arg(BaseSCPIModuleName).arg(modnr);
 
-    m_ActivationStartState.addTransition(this, SIGNAL(activationContinue()), &m_ActivationExecState);
-    m_ActivationExecState.addTransition(this, SIGNAL(activationContinue()), &m_ActivationDoneState);
-    m_ActivationDoneState.addTransition(this, SIGNAL(activationNext()), &m_ActivationExecState);
-    m_ActivationDoneState.addTransition(this, SIGNAL(activationContinue()), &m_ActivationFinishedState);
+    m_ActivationStartState.addTransition(this, &cLambdaModule::activationContinue, &m_ActivationExecState);
+    m_ActivationExecState.addTransition(this, &cLambdaModule::activationContinue, &m_ActivationDoneState);
+    m_ActivationDoneState.addTransition(this, &cLambdaModule::activationNext, &m_ActivationExecState);
+    m_ActivationDoneState.addTransition(this, &cLambdaModule::activationContinue, &m_ActivationFinishedState);
     m_ActivationMachine.addState(&m_ActivationStartState);
     m_ActivationMachine.addState(&m_ActivationExecState);
     m_ActivationMachine.addState(&m_ActivationDoneState);
     m_ActivationMachine.addState(&m_ActivationFinishedState);
     m_ActivationMachine.setInitialState(&m_ActivationStartState);
-    connect(&m_ActivationStartState, SIGNAL(entered()), SLOT(activationStart()));
-    connect(&m_ActivationExecState, SIGNAL(entered()), SLOT(activationExec()));
-    connect(&m_ActivationDoneState, SIGNAL(entered()), SLOT(activationDone()));
-    connect(&m_ActivationFinishedState, SIGNAL(entered()), SLOT(activationFinished()));
+    connect(&m_ActivationStartState, &QState::entered, this, &cLambdaModule::activationStart);
+    connect(&m_ActivationExecState, &QState::entered, this, &cLambdaModule::activationExec);
+    connect(&m_ActivationDoneState, &QState::entered, this, &cLambdaModule::activationDone);
+    connect(&m_ActivationFinishedState, &QState::entered, this, &cLambdaModule::activationFinished);
 
-    m_DeactivationStartState.addTransition(this, SIGNAL(deactivationContinue()), &m_DeactivationExecState);
-    m_DeactivationExecState.addTransition(this, SIGNAL(deactivationContinue()), &m_DeactivationDoneState);
-    m_DeactivationDoneState.addTransition(this, SIGNAL(deactivationNext()), &m_DeactivationExecState);
-    m_DeactivationDoneState.addTransition(this, SIGNAL(deactivationContinue()), &m_DeactivationFinishedState);
+    m_DeactivationStartState.addTransition(this, &cLambdaModule::deactivationContinue, &m_DeactivationExecState);
+    m_DeactivationExecState.addTransition(this, &cLambdaModule::deactivationContinue, &m_DeactivationDoneState);
+    m_DeactivationDoneState.addTransition(this, &cLambdaModule::deactivationNext, &m_DeactivationExecState);
+    m_DeactivationDoneState.addTransition(this, &cLambdaModule::deactivationContinue, &m_DeactivationFinishedState);
     m_DeactivationMachine.addState(&m_DeactivationStartState);
     m_DeactivationMachine.addState(&m_DeactivationExecState);
     m_DeactivationMachine.addState(&m_DeactivationDoneState);
     m_DeactivationMachine.addState(&m_DeactivationFinishedState);
     m_DeactivationMachine.setInitialState(&m_DeactivationStartState);
-    connect(&m_DeactivationStartState, SIGNAL(entered()), SLOT(deactivationStart()));
-    connect(&m_DeactivationExecState, SIGNAL(entered()), SLOT(deactivationExec()));
-    connect(&m_DeactivationDoneState, SIGNAL(entered()), SLOT(deactivationDone()));
-    connect(&m_DeactivationFinishedState, SIGNAL(entered()), SLOT(deactivationFinished()));
+    connect(&m_DeactivationStartState, &QState::entered, this, &cLambdaModule::deactivationStart);
+    connect(&m_DeactivationExecState, &QState::entered, this, &cLambdaModule::deactivationExec);
+    connect(&m_DeactivationDoneState, &QState::entered, this, &cLambdaModule::deactivationDone);
+    connect(&m_DeactivationFinishedState, &QState::entered, this, &cLambdaModule::deactivationFinished);
 
 }
 
@@ -82,9 +82,9 @@ void cLambdaModule::setupModule()
     // we need some program that does the measuring on dsp
     m_pMeasProgram = new cLambdaModuleMeasProgram(this, m_pConfiguration);
     m_ModuleActivistList.append(m_pMeasProgram);
-    connect(m_pMeasProgram, SIGNAL(activated()), SIGNAL(activationContinue()));
-    connect(m_pMeasProgram, SIGNAL(deactivated()), this, SIGNAL(deactivationContinue()));
-    connect(m_pMeasProgram, SIGNAL(errMsg(QVariant)), m_pModuleErrorComponent, SLOT(setValue(QVariant)));
+    connect(m_pMeasProgram, &cLambdaModuleMeasProgram::activated, this, &cLambdaModule::activationContinue);
+    connect(m_pMeasProgram, &cLambdaModuleMeasProgram::deactivated, this, &cLambdaModule::deactivationContinue);
+    connect(m_pMeasProgram, &cLambdaModuleMeasProgram::errMsg, m_pModuleErrorComponent, &cVeinModuleErrorComponent::setValue);
 
     emit addEventSystem(m_pMeasProgram->getEventSystem());
 
--- zera-modules/lambdamodule/src/lambdamodulefactory.cpp
+++ zera-modules/lambdamodule/src/lambdamodulefactory.cpp
@@ -14,7 +14,7 @@ ZeraModules::VirtualModule* LambdaModuleFactory::createModule(Zera::Proxy::cProx
 void LambdaModuleFactory::destroyModule(ZeraModules::VirtualModule *module)
 {
     m_ModuleList.removeAll(module);
-    connect(module, SIGNAL(deactivationReady()), module, SIGNAL(moduleDeactivated()));
+    connect(module, &ZeraModules::VirtualModule::deactivationReady, module, &ZeraModules::VirtualModule::moduleDeactivated);
     if (!module->m_DeactivationMachine.isRunning())
         module->m_DeactivationMachine.start();
 }
