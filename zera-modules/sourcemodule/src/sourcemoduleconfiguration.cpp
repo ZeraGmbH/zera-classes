2d6b625de8b2c348b9db9592f8f8a969c21cf434
Andreas Müller <schnitzeltony@gmail.com>
2021-09-16 19:59:07 +0200

    sourcemodule: Introduce automatic code generation for configuration data
    Let xsdcxx create the XML-Wrapper code for us and simplify XML content
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/src/sourcemoduleconfiguration.cpp
+++ zera-modules/sourcemodule/src/sourcemoduleconfiguration.cpp
@@ -3,7 +3,6 @@
 #include <xmlconfigreader.h>
 
 #include "sourcemoduleconfiguration.h"
-#include "sourcemoduleconfigdata.h"
 #include "socket.h"
 
 namespace SOURCEMODULE
@@ -11,15 +10,6 @@ namespace SOURCEMODULE
 
 cSourceModuleConfiguration::cSourceModuleConfiguration()
 {
-    m_pStatusModulConfigData = 0;
-    connect(m_pXMLReader, SIGNAL(valueChanged(const QString&)), this, SLOT(configXMLInfo(const QString&)));
-    connect(m_pXMLReader, SIGNAL(finishedParsingXML(bool)), this, SLOT(completeConfiguration(bool)));
-}
-
-
-cSourceModuleConfiguration::~cSourceModuleConfiguration()
-{
-    if (m_pStatusModulConfigData) delete m_pStatusModulConfigData;
 }
 
 
@@ -27,59 +17,30 @@ void cSourceModuleConfiguration::setConfiguration(QByteArray xmlString)
 {
     m_bConfigured = m_bConfigError = false;
 
-    if (m_pStatusModulConfigData) delete m_pStatusModulConfigData;
-    m_pStatusModulConfigData = new cSourceModuleConfigData();
-
-    m_ConfigXMLMap.clear(); // in case of new configuration we completely set up
-
-    // so now we can set up
-    // initializing hash table for xml configuration
-
-    m_ConfigXMLMap["statusmodconfpar:configuration:connectivity:debuglevel"] = setDebugLevel;
-
-    if (m_pXMLReader->loadSchema(defaultXSDFile))
-        m_pXMLReader->loadXMLFromString(QString::fromUtf8(xmlString.data(), xmlString.size()));
-    else
+    std::istringstream stdStream(xmlString.toStdString());
+    try {
+        m_xsdGeneratedConfig = configuration_(stdStream, xml_schema::flags::dont_validate);
+        m_bConfigured = true;
+        emit configXMLDone();
+    }
+    catch (const xml_schema::exception& e) {
         m_bConfigError = true;
+        qWarning("XML: %s\n%s", e.what(), qPrintable(xmlString));
+    }
 }
 
 
 QByteArray cSourceModuleConfiguration::exportConfiguration()
 {
-    // nothing to set befor export
-    return m_pXMLReader->getXMLConfig().toUtf8();
+    std::stringstream oStream;
+    configuration_(oStream, *m_xsdGeneratedConfig);
+    return QByteArray::fromStdString(oStream.str());
 }
 
 
-cSourceModuleConfigData *cSourceModuleConfiguration::getConfigurationData()
+std::unique_ptr<configuration> &cSourceModuleConfiguration::GetConfig()
 {
-    return m_pStatusModulConfigData;
-}
-
-
-void cSourceModuleConfiguration::configXMLInfo(QString key)
-{
-    if (m_ConfigXMLMap.contains(key)) {
-        int cmd = m_ConfigXMLMap[key];
-        bool valueOK = false;
-        switch (cmd)
-        {
-        case setDebugLevel:
-            m_pStatusModulConfigData->m_nDebugLevel = m_pXMLReader->getValue(key).toInt(&valueOK);
-            break;
-        }
-        m_bConfigError |= !valueOK;
-    }
-    else {
-        m_bConfigError = true;
-    }
-}
-
-
-void cSourceModuleConfiguration::completeConfiguration(bool ok)
-{
-    m_bConfigured = (ok && !m_bConfigError);
-    emit configXMLDone();
+    return m_xsdGeneratedConfig;
 }
 
 }
