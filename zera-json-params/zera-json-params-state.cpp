15f1a73797c29aa762e007902fbbe456ab1b0650
Andreas Müller <schnitzeltony@gmail.com>
2021-10-02 22:57:28 +0200

    zera-json-params-state: Stip down to parameter keeper
    * Forget about validation - this is cZeraJsonParamsStructure' job
    * Adjust zera-json-params-cli to changed design
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-json-params/zera-json-params-state.cpp
+++ zera-json-params/zera-json-params-state.cpp
@@ -1,5 +1,6 @@
 #include "zera-json-params-state.h"
 #include "zera-json-params-structure.h"
+#include "zera-json-merge.h"
 
 cZeraJsonParamsState::cZeraJsonParamsState()
 {
@@ -7,60 +8,21 @@ cZeraJsonParamsState::cZeraJsonParamsState()
 
 bool cZeraJsonParamsState::isValid()
 {
-    return !m_jsonObjParamState.isEmpty();
+    return !m_jsonObjState.isEmpty();
 }
 
-cZeraJsonParamsState::ErrList cZeraJsonParamsState::loadState(QJsonObject& jsonState, cZeraJsonParamsStructure* paramStructure)
+void cZeraJsonParamsState::setState(const QJsonObject& jsonState)
 {
-    ErrList errList;
-    if(paramStructure && paramStructure->isValid()) {
-        // TODO: let structure validate state data
-    }
-    else {
-        errEntry error(ERR_INVALID_STRUCTURE, QString());
-        errList.push_back(error);
-    }
-    return errList;
+    m_jsonObjState = jsonState;
 }
 
-const QJsonObject &cZeraJsonParamsState::jsonState()
+void cZeraJsonParamsState::mergeState(const QJsonObject &jsonState)
 {
-
+    cJSONMerge::mergeJson(m_jsonObjState, jsonState);
 }
 
-cZeraJsonParamsState::ErrList cZeraJsonParamsState::param(const QStringList &paramPath, QVariant& value)
+const QJsonObject& cZeraJsonParamsState::state()
 {
-    ErrList errList;
-    // TODO
-    return errList;
+    return m_jsonObjState;
 }
 
-cZeraJsonParamsState::ErrList cZeraJsonParamsState::setParam(const QStringList &paramPath, QVariant value)
-{
-    ErrList errList;
-    // TODO
-    return errList;
-}
-
-
-cZeraJsonParamsState::errEntry::errEntry(cZeraJsonParamsState::errorTypes errType, QString strInfo) :
-    m_errType(errType),
-    m_strInfo(strInfo)
-{
-}
-
-QString cZeraJsonParamsState::errEntry::strID()
-{
-    QString str;
-    switch(m_errType) {
-    case ERR_INVALID_STRUCTURE:
-        str = "Invalid parameter structure";
-    case ERR_INVALID_PARAM_PATH:
-        str = "Invalid parameter path";
-    case ERR_PARAM_DOES_NOT_EXIST:
-        str = "Parameter does not exist";
-    case ERR_INVALID_VALUE:
-        str = "Invalid parameter value";
-    }
-    return str;
-}
