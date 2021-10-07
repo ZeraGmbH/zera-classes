91e6356410db78298faf16d578d000ea13492c2e
Andreas Müller <schnitzeltony@gmail.com>
2021-10-07 13:58:16 +0200

    sourcemodule: Add cSourceDeviceStatus - a simple JSON status keeper
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/sourcemodule/src/sourcedevicestatus.h
@@ -0,0 +1,27 @@
+#ifndef CSOURCEDEVICESTATUS_H
+#define CSOURCEDEVICESTATUS_H
+
+#include <QJsonObject>
+
+namespace SOURCEMODULE
+{
+
+class cSourceDeviceStatus
+{
+public:
+    cSourceDeviceStatus();
+
+    const QJsonObject &jsonStatus();
+    void reset();
+
+    void setBusy(bool busy);
+    void addError(const QString error);
+    void addWarning(const QString warning);
+
+private:
+    QJsonObject m_jsonStatus;
+};
+
+} //namespace SOURCEMODULE
+
+#endif // CSOURCEDEVICESTATUS_H
