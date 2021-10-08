c1422d2ba1b5d1b2cba77bfcafd2fc1dc6b9eed1
Andreas Müller <schnitzeltony@gmail.com>
2021-10-08 10:14:48 +0200

    sourcemodule: Replace MT-Single-Phase by MT507
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/src/sourcedevice.h
+++ zera-modules/sourcemodule/src/sourcedevice.h
@@ -18,11 +18,11 @@ class cSourceDevice : public QObject
 public:
     enum SourceType { // TODO replace by protocol
         SOURCE_DEMO = 0,
-        SOURCE_MT3000,
-        SOURCE_MT400_20,
-        SOURCE_MT_SINGLE_PHASE,
+        SOURCE_MT3000,   // basic
+        SOURCE_MT400_20, // current only
+        SOURCE_MT507,    // single phase
 
-        SOURCE_DEMO_FG_4PHASE,
+        SOURCE_DEMO_FG_4PHASE, // AUX/harmonics
 
         SOURCE_TYPE_COUNT
     };
