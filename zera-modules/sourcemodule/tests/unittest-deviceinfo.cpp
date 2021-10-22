0bbbbc064482952e84ed0233900b5fc159a19afd
Andreas Müller <schnitzeltony@gmail.com>
2021-10-22 09:09:55 +0200

    sourcemodule: Test all our devicefiles for validity
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/sourcemodule/tests/unittest-deviceinfo.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <QDir>
+#include <QJsonDocument>
+#include <zera-json-params-structure.h>
+
+// Check all deviceinfos for validity by loading them into cZeraJsonParamsStructure
+TEST(TEST_DEVICEINFO,ALL_VALID) {
+    QDir dir(QStringLiteral("://deviceinfo"));
+    QFileInfoList fileInfoList = dir.entryInfoList();
+    for(auto fileInfo : fileInfoList) {
+        QString diFileName = fileInfo.absoluteFilePath();
+        QFile diFile(diFileName);
+        bool diOpened = diFile.open(QIODevice::Unbuffered | QIODevice::ReadOnly);
+        EXPECT_EQ(diOpened, true) << "Deviceinfo could not be opened:" << qPrintable(diFileName);
+        if(diOpened) {
+            QJsonObject diStructure = QJsonDocument::fromJson(diFile.readAll()).object();
+            cZeraJsonParamsStructure jsonParamStructure;
+            cZeraJsonParamsStructure::ErrList errListStructure = jsonParamStructure.loadStructure(diStructure);
+            QString errorInfoTxt;
+            for(auto errInfo: errListStructure) {
+                errorInfoTxt += "\n" + errInfo.strID() + ": " + errInfo.m_strInfo;
+            }
+            EXPECT_EQ(errListStructure.length(), 0) << "Deviceinfo invalid: " <<
+                                                       qPrintable(diFileName) << qPrintable(errorInfoTxt) << "\n";
+        }
+    }
+}
