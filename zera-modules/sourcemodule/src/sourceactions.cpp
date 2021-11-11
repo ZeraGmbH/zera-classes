c7de6879d0a96ac42d6c52a9b106b302ef0581e5
Andreas Müller <schnitzeltony@gmail.com>
2021-11-11 14:12:03 +0100

    Do a complete overhaul of source actions
    * Now we started using it it does not seem to make sense to keep whole json
      on each action
    * Make our sorter generic
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/src/sourceactions.cpp
+++ zera-modules/sourcemodule/src/sourceactions.cpp
@@ -1,62 +1,26 @@
 #include "sourceactions.h"
 
-cSourceAction::cSourceAction()
+tSourceActionTypeList cSourceActionGenerator::GenerateLoadActionList(const QJsonObject jsonSourceParamState)
 {
-}
-
-cSourceAction::cSourceAction(QJsonObject jsonSourceParamStructure, QJsonObject jsonSourceParamState) :
-    m_jsonSourceParamStructure(jsonSourceParamStructure),
-    m_jsonSourceParamState(jsonSourceParamState)
-{
-}
-
-tSourceActionMap cSourceActionGenerator::GenerateLoadActionMap(const QJsonObject jsonSourceParamStructure, const QJsonObject jsonSourceParamState)
-{
-    tSourceActionMap map;
+    tSourceActionTypeList list;
     // 1st guess: switch off just switch phases
     if(jsonSourceParamState.contains("on") && !jsonSourceParamState["on"].toBool()) {
-        cSourceAction action(jsonSourceParamStructure, jsonSourceParamState);
-        map.insert(cSourceActionTypes::ActionTypes::SWITCH_PHASES, action);
+        list.append(cSourceActionTypes::ActionTypes::SWITCH_PHASES);
     }
     else {
         for(int type=0; type<cSourceActionTypes::getLoadActionTypeCount(); ++type) {
-            cSourceAction action(jsonSourceParamStructure, jsonSourceParamState);
-            map.insert(cSourceActionTypes::ActionTypes(type), action);
+            list.append(cSourceActionTypes::ActionTypes(type));
         }
     }
-    return map;
+    return list;
 }
 
-tSourceActionMap cSourceActionGenerator::GeneratePeriodicActionMap(const QJsonObject jsonSourceParamStructure)
+tSourceActionTypeList cSourceActionGenerator::GeneratePeriodicActionList()
 {
-    tSourceActionMap map;
+    tSourceActionTypeList list;
     for(int type=cSourceActionTypes::PERIODIC_FIRST; type<int(cSourceActionTypes::PERIODIC_LAST); ++type) {
-        cSourceAction action(jsonSourceParamStructure, QJsonObject());
-        map.insert(cSourceActionTypes::ActionTypes(type), action);
+        list.append(cSourceActionTypes::ActionTypes(type));
     }
-    return map;
+    return list;
 }
 
-static void appendToActionList(tSourceActionList& list, tSourceActionMap::Iterator iter)
-{
-    tSourceListEntry listEntry;
-    listEntry.action = iter.value();
-    listEntry.actionType = iter.key();
-    list.append(listEntry);
-}
-
-tSourceActionList cSourceActionSorter::SortActionMap(tSourceActionMap sourceActionMap, const QList<cSourceActionTypes::ActionTypes> sortToFrontList)
-{
-    tSourceActionList sortedList;
-    for(auto sortFrontEntry : sortToFrontList) { // append front sort
-        auto iter = sourceActionMap.find(sortFrontEntry);
-        if(iter != sourceActionMap.end()) {
-            appendToActionList(sortedList, iter);
-            sourceActionMap.erase(iter);
-        }
-    }
-    for(auto iter = sourceActionMap.begin(); iter != sourceActionMap.end(); ++iter) { // append remaining
-        appendToActionList(sortedList, iter);
-    }
-    return sortedList;
-}
