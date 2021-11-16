dc4e5378826e1d4d7d34ca8a05b1eae5a39325f8
Andreas Müller <schnitzeltony@gmail.com>
2021-11-16 16:21:54 +0100

    sourceiotransactiongenerator: Rework
    * add type to transactions and their lists
    * do some plausibility tests on each transaction
    * prepare for periodic state query
    * prettify enums & names
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/tests/unittest-sourceiotransactiongenerator.cpp
+++ zera-modules/sourcemodule/tests/unittest-sourceiotransactiongenerator.cpp
@@ -20,3 +20,45 @@ TEST(TEST_SOURCE_IO_TRANSACTION, VALUE_CONVERSION) {
     EXPECT_EQ(valGenerated, valExpected);
 }
 
+TEST(TEST_SOURCE_IO_TRANSACTION, TRANSACTION_TYPE_SET) {
+    for(int action = 0; action<cSourceActionTypes::ACTION_LIMIT; action++) {
+        cSourceSwitchIoTransactionGenerator transactionGenerator = cSourceSwitchIoTransactionGenerator(QJsonObject());
+        tSourceIoTransactionList transactionList = transactionGenerator.generateListForAction(cSourceActionTypes::ActionTypes(action));
+        for(auto transaction : transactionList) {
+            EXPECT_EQ(transaction.m_actionType, action);
+        }
+    }
+}
+
+TEST(TEST_SOURCE_IO_TRANSACTION, TRANSACTION_SEND_NOT_EMPTY) {
+    for(int action = 0; action<cSourceActionTypes::ACTION_LIMIT; action++) {
+        cSourceSwitchIoTransactionGenerator transactionGenerator = cSourceSwitchIoTransactionGenerator(QJsonObject());
+        tSourceIoTransactionList transactionList = transactionGenerator.generateListForAction(cSourceActionTypes::ActionTypes(action));
+        for(auto transaction : transactionList) {
+            EXPECT_FALSE(transaction.m_dataSend.isEmpty());
+        }
+    }
+}
+
+TEST(TEST_SOURCE_IO_TRANSACTION, TRANSACTION_RESPONSE_TYPE_SET) {
+    for(int action = 0; action<cSourceActionTypes::ACTION_LIMIT; action++) {
+        cSourceSwitchIoTransactionGenerator transactionGenerator = cSourceSwitchIoTransactionGenerator(QJsonObject());
+        tSourceIoTransactionList transactionList = transactionGenerator.generateListForAction(cSourceActionTypes::ActionTypes(action));
+        for(auto transaction : transactionList) {
+            EXPECT_FALSE(transaction.m_ioResponseType == RESP_UNDEFINED);
+            EXPECT_FALSE(transaction.m_ioResponseType >= RESP_LIMIT);
+        }
+    }
+}
+
+TEST(TEST_SOURCE_IO_TRANSACTION, TRANSACTION_EXPECT_RESPONSE_SET) {
+    for(int action = 0; action<cSourceActionTypes::ACTION_LIMIT; action++) {
+        cSourceSwitchIoTransactionGenerator transactionGenerator = cSourceSwitchIoTransactionGenerator(QJsonObject());
+        tSourceIoTransactionList transactionList = transactionGenerator.generateListForAction(cSourceActionTypes::ActionTypes(action));
+        for(auto transaction : transactionList) {
+            if(transaction.m_ioResponseType == RESP_FULL_DATA_SEQUENCE || transaction.m_ioResponseType == RESP_PART_DATA_SEQUENCE) {
+                EXPECT_FALSE(transaction.m_dataExpectedResponse.isEmpty());
+            }
+        }
+    }
+}
