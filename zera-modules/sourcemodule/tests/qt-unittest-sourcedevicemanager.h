e7cbf7e69d32277535fb40e8c5efb0d2840d2e6b
Andreas Müller <schnitzeltony@gmail.com>
2021-11-19 19:30:06 +0100

    qt-unittest-sourcedevicemanager: Although in trouble with crasher detection add
    We have to investigate further why second ioFinished is not received on crasher
    unittest.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/sourcemodule/tests/qt-unittest-sourcedevicemanager.h
@@ -0,0 +1,50 @@
+#ifndef SOURCEDEVICEMANAGER_H
+#define SOURCEDEVICEMANAGER_H
+
+#include <QObject>
+#include "sourcedevice.h"
+
+using SOURCEMODULE::cSourceDevice;
+
+struct FinishEntry
+{
+    FinishEntry(int slotNo, cSourceDevice* device, QUuid uuid, QString errMsg);
+    int slotNo;
+    cSourceDevice* device;
+    QUuid uuid;
+    QString errMsg;
+};
+
+class SourceDeviceManagerTest : public QObject
+{
+    Q_OBJECT
+public slots:
+    void onSourceScanFinished(int slotNo, cSourceDevice* device, QUuid uuid, QString errMsg);
+    void onSlotRemoved(int slotNo);
+
+private slots:
+    void init();
+
+    void testInitState();
+    void testAddTooMany();
+    void testRemoveOnEmpty();
+    void testRemoveInvalidBelow();
+    void testRemoveInvalidAbove();
+    void testRemoveTooMany();
+
+    void testAvailAndDemoCountAdd();
+    void testAvailAndDemoCountAddRemove();
+
+    void testAddNotification();
+    void testAddNotificationTooMany();
+//    void testRemoveNotification();
+//    void testRemoveNotificationTooMany();
+
+    void testNoCrashOnManagerDeadBeforeScanFinished();
+
+private:
+    QList<FinishEntry> m_listSourcesAdded;
+    QList<int> m_socketsRemoved;
+};
+
+#endif // SOURCEDEVICEMANAGER_H
