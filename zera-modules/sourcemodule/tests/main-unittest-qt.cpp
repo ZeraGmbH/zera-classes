860c9076b66dc94ce09151ce3a293c22a0708105
Andreas Müller <schnitzeltony@gmail.com>
2021-11-22 09:32:57 +0100

    main-unittest-qt: Show failing test with an extra line
    In debug-builds this line is red to not miss a failing test
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/tests/main-unittest-qt.cpp
+++ zera-modules/sourcemodule/tests/main-unittest-qt.cpp
@@ -17,6 +17,13 @@ int main(int argc, char *argv[])
         status |= QTest::qExec(test, argc, argv);
         delete test;
     }
+    if(status) {
+#ifdef QT_DEBUG
+        qWarning("\n\033[1;31m%s\033[0m","One or more tests failed!!!");
+#else
+        qWarning("\nOne or more tests failed!!!");
+#endif
+    }
     return status;
 }
 
