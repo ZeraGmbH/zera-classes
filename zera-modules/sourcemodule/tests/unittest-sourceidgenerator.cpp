17dc5a3bf44dd1fb55474630755ede4609ce3cb4
Andreas Müller <schnitzeltony@gmail.com>
2021-11-23 21:38:02 +0100

    unittest-sourceidgenerator: Adjust to new behavior: zero is a valid id
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/tests/unittest-sourceidgenerator.cpp
+++ zera-modules/sourcemodule/tests/unittest-sourceidgenerator.cpp
@@ -1,13 +1,13 @@
 #include <gtest/gtest.h>
 #include "sourceidgenerator.h"
 
-TEST(TEST_ID_GENERATOR, NOT_ZERO) {
+TEST(TEST_ID_GENERATOR, INCREMENT) {
     cSourceIdGenerator generator;
     int id;
+    id = generator.nextID();
+    EXPECT_EQ(id, 0);
     id = generator.nextID();
     EXPECT_EQ(id, 1);
     id = generator.nextID();
     EXPECT_EQ(id, 2);
-    id = generator.nextID();
-    EXPECT_EQ(id, 3);
 }
