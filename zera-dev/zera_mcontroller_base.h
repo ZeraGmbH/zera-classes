83dc187ca3279909893d508ff84ad4de43e0a49e
Andreas Müller <schnitzeltony@gmail.com>
2021-11-27 15:39:34 +0100

    zera-dev/zera_mcontroller_base: Split out error flags and message generation
    * By doing so we can test them
    * Switch back to using sprintf because asprintf does not work. Unfortunately the
      returned error texts are very important because our firmware update scripts
      decide upon their content how to go on - yes very bad.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-dev/zera_mcontroller_base.h
+++ zera-dev/zera_mcontroller_base.h
@@ -13,19 +13,8 @@
 #include "dev_global.h"
 #include "protocol_zera_bootloader.h"
 #include "protocol_zera_hard.h"
+#include "zera_mcontroller_errorflags.h"
 
-/** @brief Master-side error flags
- */
-constexpr quint32 MASTER_ERR_FLAG_I2C_TRANSFER = 1<<16;
-constexpr quint32 MASTER_ERR_FLAG_CRC = 1<<17;
-constexpr quint32 MASTER_ERR_FLAG_LENGTH = 1<<18;
-constexpr quint32 MASTER_ERR_FLAG_VERIFY_BLOCK = 1<<19;
-constexpr quint32 MASTER_ERR_FLAG_FLASH_WRITE = 1<<20;
-constexpr quint32 MASTER_ERR_FLAG_EERPOM_WRITE = 1<<21;
-constexpr quint32 MASTER_ERR_FLAG_FLASH_VERIFY = 1<<22;
-constexpr quint32 MASTER_ERR_FLAG_EERPOM_VERIFY = 1<<23;
-
-constexpr quint32 MASTER_ERR_FLAG_CODER_IS_AN_IDIOT = 1<<31;
 
 /**
  * @brief ZeraMcontrollerBase implements basic functionality for hardware-/bootloader-protocol
@@ -133,14 +122,11 @@ private:
      * @return done or error type information
      */
     atmelRM loadOrVerifyMemory(quint8 blCmd, cIntelHexFileIO& ihxFIO, bool verify);
-    void appendMasterErrorFlags(QHash<quint32, QString>& errorFlagsText);
 
     QString m_sI2CDevNode;
     quint8 m_nI2CAdr;
     quint8 m_nDebugLevel;
     quint32 m_nLastErrorFlags;
-    static QHash<quint32, QString> m_errorFlagsText;
-    static QHash<quint32, QString> m_errorFlagsBootText;
     bool m_bBootCmd;
     quint8 maxBlockWriteTries;
 };
