c1a238771df12d8c1e5e92d547e08a791054bc54
Andreas Müller <schnitzeltony@gmail.com>
2022-01-18 00:43:38 +0100

    Work on SourceDeviceStatusWatcherJson
    It is not done: Commented tests fail. Investigations point to IoWorker. It seems
    we missed tests for:
    
    * two command packages
    * first package fails for wrong or missing response
    
    => Currently all remaining packages are killed then
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/src/device/sourcedevicestatuswatcherjson.cpp
+++ zera-modules/sourcemodule/src/device/sourcedevicestatuswatcherjson.cpp
@@ -1,12 +1,108 @@
 #include "sourcedevicestatuswatcherjson.h"
 
 SourceDeviceStatusWatcherJson::SourceDeviceStatusWatcherJson(SourceDevice *sourceDevice) :
-    SourceDeviceObserver(sourceDevice)
+    SourceDeviceObserver(sourceDevice),
+    m_sourceDevice(sourceDevice),
+    m_stateEnum(SOURCE_STATE::UNDEFINED)
 {
+    m_pollTimer.setSingleShot(false);
+    m_pollTimer.setInterval(500);
+    enablePolling();
+    connect(&m_pollTimer, &QTimer::timeout,
+            this, &SourceDeviceStatusWatcherJson::onPollTimer);
+    connect(m_sourceDevice, &SourceDevice::sigSwitchTransationStarted,
+            this, &SourceDeviceStatusWatcherJson::onSwitchBusyChanged);
+}
 
+void SourceDeviceStatusWatcherJson::enablePolling()
+{
+    m_pollTimer.start();
+}
+
+void SourceDeviceStatusWatcherJson::disablePolling()
+{
+    m_pollTimer.stop();
+}
+
+void SourceDeviceStatusWatcherJson::setPollTime(int ms)
+{
+    bool active = m_pollTimer.isActive();
+    if(active) {
+        disablePolling();
+    }
+    m_pollTimer.setInterval(ms);
+    if(active) {
+        enablePolling();
+    }
+}
+
+bool SourceDeviceStatusWatcherJson::isErrorActive()
+{
+    return m_stateEnum == SOURCE_STATE::ERROR_SWITCH ||
+            m_stateEnum == SOURCE_STATE::ERROR_POLL;
+}
+
+void SourceDeviceStatusWatcherJson::onSwitchBusyChanged()
+{
+    // switch is the only way out of error -> set busy unconditional
+    setState(SOURCE_STATE::SWITCH_BUSY);
 }
 
 void SourceDeviceStatusWatcherJson::updateResponse(IoWorkerCmdPack cmdPack)
 {
+    if(cmdPack.isSwitchPack()) {
+        handleSwitchResponse(cmdPack);
+    }
 
+    if(cmdPack.isStateQuery() && m_ioIdKeeper.isCurrAndDeactivateIf(cmdPack.m_workerId)) {
+        handleStateResponse(cmdPack);
+    }
+}
+
+void SourceDeviceStatusWatcherJson::onPollTimer()
+{
+    if(!m_ioIdKeeper.isActive()) {
+        IoCommandPacket cmdPack = m_sourceDevice->getIoPacketGenerator().generateStatusPollPacket();
+        IoWorkerCmdPack workerPack = IoWorkerConverter::commandPackToWorkerPack(cmdPack);
+        m_ioIdKeeper.setCurrent(m_sourceDevice->startTransaction(workerPack));
+    }
+}
+
+void SourceDeviceStatusWatcherJson::setState(SOURCE_STATE state)
+{
+    if(state != m_stateEnum) {
+        m_stateEnum = state;
+        emit sigStateChanged(m_stateEnum);
+        setPollingOnStateChange();
+    }
+}
+
+void SourceDeviceStatusWatcherJson::setPollingOnStateChange()
+{
+    if(!isErrorActive()) {
+        enablePolling();
+    }
+    else {
+        disablePolling();
+    }
+}
+
+void SourceDeviceStatusWatcherJson::handleSwitchResponse(IoWorkerCmdPack &cmdPack)
+{
+    if(!cmdPack.passedAll() && !isErrorActive()) {
+        setState(SOURCE_STATE::ERROR_SWITCH);
+    }
+    else if(m_stateEnum == SOURCE_STATE::SWITCH_BUSY) {
+        setState(SOURCE_STATE::IDLE);
+    }
+}
+
+void SourceDeviceStatusWatcherJson::handleStateResponse(IoWorkerCmdPack &cmdPack)
+{
+    if(!cmdPack.passedAll() && !isErrorActive()) {
+        setState(SOURCE_STATE::ERROR_POLL);
+    }
+    else if(m_stateEnum == SOURCE_STATE::UNDEFINED) {
+        setState(SOURCE_STATE::IDLE);
+    }
 }
--- zera-modules/sourcemodule/src/device/sourcedevicestatuswatcherjson.h
+++ zera-modules/sourcemodule/src/device/sourcedevicestatuswatcherjson.h
@@ -6,6 +6,22 @@
 #include "json/jsondevicestatusapi.h"
 
 #include <QObject>
+#include <QTimer>
+
+/*
+ * Periodic actor / observer on source device
+ * Poll status
+ * notify current state changed
+ */
+
+enum class SOURCE_STATE
+{
+    UNDEFINED,
+    IDLE,
+    SWITCH_BUSY,
+    ERROR_SWITCH,
+    ERROR_POLL
+};
 
 class SourceDeviceStatusWatcherJson : public SourceDeviceObserver
 {
@@ -13,13 +29,31 @@ class SourceDeviceStatusWatcherJson : public SourceDeviceObserver
 public:
     SourceDeviceStatusWatcherJson(SourceDevice* sourceDevice);
 
+    void setPollTime(int ms);
+
 signals:
+    void sigStateChanged(SOURCE_STATE state);
 
 protected:
     virtual void updateResponse(IoWorkerCmdPack cmdPack);
 
+private slots:
+    void onPollTimer();
+    void onSwitchBusyChanged();
 private:
-    JsonDeviceStatusApi m_deviceStatus;
+    bool isErrorActive();
+    void enablePolling();
+    void disablePolling();
+    void setState(SOURCE_STATE state);
+    void setPollingOnStateChange();
+    void handleSwitchResponse(IoWorkerCmdPack &cmdPack);
+    void handleStateResponse(IoWorkerCmdPack &cmdPack);
+
+    SourceDevice* m_sourceDevice;
+    SOURCE_STATE m_stateEnum;
+    IoIdKeeper m_ioIdKeeper;
+
+    QTimer m_pollTimer;
 };
 
 #endif // SOURCEDEVICESTATUSWATCHER_H
