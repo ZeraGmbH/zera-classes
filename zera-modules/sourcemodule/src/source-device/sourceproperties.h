e94c636c4aaf4d4313ae3648cf2f84a8d1b95572
Andreas Müller <schnitzeltony@gmail.com>
2022-01-28 22:46:13 +0100

    sourceproperties: Add protocol type
    As long as we just have SourceProtocols::ZERA_SERIAL this is not exciting but
    as soon as we get further scanner strategies they can drop a useful information
    which protocol to use for sources found.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/src/source-device/sourceproperties.h
+++ zera-modules/sourcemodule/src/source-device/sourceproperties.h
@@ -2,24 +2,29 @@
 #define SOURCEPROPERTIES_H
 
 #include "supportedsources.h"
+#include "source-protocols/sourceprotocols.h"
 #include <QString>
 
 class SourceProperties
 {
 public:
     SourceProperties();
-    SourceProperties(SupportedSourceTypes type, QString name, QString version);
+    SourceProperties(SupportedSourceTypes type,
+                     QString name,
+                     QString version,
+                     SourceProtocols protocol = SourceProtocols::ZERA_SERIAL);
 
     bool isValid() const;
     SupportedSourceTypes getType() const;
     QString getName() const;
     QString getVersion() const;
+    SourceProtocols getProtocol() const;
 private:
     bool m_isValid = false;
     SupportedSourceTypes m_type = SOURCE_MT_COMMON;
     QString m_name;
     QString m_version;
-
+    SourceProtocols m_protocol;
 };
 
 #endif // SOURCEPROPERTIES_H
