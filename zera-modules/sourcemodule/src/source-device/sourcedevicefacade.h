8cbc5f7c96b92022b832eb12f318a61672b6e614
Andreas Müller <schnitzeltony@gmail.com>
2022-02-07 13:51:52 +0100

    sourcemodule: Introduce SourceDeviceFacade
    A working wrapper for all objects required to control a source
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/sourcemodule/src/source-device/sourcedevicefacade.h
@@ -0,0 +1,71 @@
+#ifndef SOURCEDEVICEOBJCONTAINER_H
+#define SOURCEDEVICEOBJCONTAINER_H
+
+#include "io-device/iodevicebase.h"
+#include "sourceproperties.h"
+#include "sourceio.h"
+#include "sourcetransactionstartnotifier.h"
+#include "sourcestatecontroller.h"
+#include "sourceswitchjson.h"
+#include "json/jsondevicestatusapi.h"
+#include "json/persistentjsonstate.h"
+
+#include <QObject>
+#include <QUuid>
+#include <QJsonObject>
+
+class SourceVeinInterface;
+
+class SourceDeviceFacade : public QObject
+{
+    Q_OBJECT
+public:
+    SourceDeviceFacade(IoDeviceBase::Ptr ioDevice, SourceProperties properties);
+
+    void setVeinInterface(SourceVeinInterface* veinInterface);
+    void setStatusPollTime(int ms);
+    void switchLoad(QJsonObject params);
+
+    QString getIoDeviceInfo() const;
+    bool hasDemoIo() const;
+    QStringList getLastErrors() const;
+
+    bool close(QUuid uuid);
+
+signals:
+    void sigClosed(SourceDeviceFacade* source, QUuid uuid);
+
+
+private slots:
+    void onSourceStateChanged(SourceStateController::States state);
+    void onSourceSwitchFinished();
+    void onIoDeviceClosed();
+
+private:
+    void handleErrorState(SourceStateController::States state);
+    void doFinalCloseActivities();
+    void setVeinParamStructure(QJsonObject paramStruct);
+    void setVeinParamState(QJsonObject paramState);
+    void setVeinDeviceState(QJsonObject deviceState);
+    void resetVeinComponents();
+    void enableCloseRequested(QUuid uuid);
+    bool tryDemoCloseByUsbDisconnect(QUuid uuid);
+
+    SourceVeinInterface* m_veinInterface = nullptr;
+    IoDeviceBase::Ptr m_ioDevice;
+    ISourceIo::Ptr m_sourceIo;
+
+    SourceTransactionStartNotifier m_transactionNotifierStatus;
+    SourceTransactionStartNotifier m_transactionNotifierSwitch;
+    SourceStateController m_stateController;
+    SourceSwitchJson m_switcher;
+
+    QUuid m_closeUuid;
+    bool m_closeRequested = false;
+
+    JsonDeviceStatusApi m_deviceStatus;
+
+    static bool m_demoCloseByUsbDisconnect;
+};
+
+#endif // SOURCEDEVICEOBJCONTAINER_H
