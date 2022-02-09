a2b8118d277b05c8d94fe5577fca4914ceeb1039
Andreas Müller <schnitzeltony@gmail.com>
2022-02-09 19:37:02 +0100

    sourcestatecontroller: Tiny tidy
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/src/source-device/sourcestatecontroller.h
+++ zera-modules/sourcemodule/src/source-device/sourcestatecontroller.h
@@ -25,7 +25,6 @@ public:
         ERROR_SWITCH,
         ERROR_POLL
     };
-    bool isErrorState() const;
 signals:
     void sigStateChanged(States state);
 
@@ -34,10 +33,12 @@ private slots:
     void onStateQueryTransationStarted(int dataGroupId);
     void onResponseReceived(const IoQueueEntry::Ptr transferGroup);
 private:
+    bool isErrorState() const;
     void setState(States state);
     void setPollingOnStateChange();
     void handleSwitchResponse(const IoQueueEntry::Ptr transferGroup);
     void handleStateResponse(const IoQueueEntry::Ptr transferGroup);
+    bool isNewError(const IoQueueEntry::Ptr transferGroup);
     ISourceIo::Ptr m_sourceIo;
     SourceTransactionStartNotifier::Ptr m_sourceNotificationSwitch;
     SourceTransactionStartNotifier::Ptr m_sourceNotificationStateQuery;
