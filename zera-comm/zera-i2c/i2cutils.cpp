ec89df51d80db3f7a8114e008c94fb70ac13dc5b
Andreas Müller <schnitzeltony@gmail.com>
2022-04-13 17:38:34 +0200

    zera-i2c: Introduce I2cPing
    We used this code when implementing EMOB firmware update by using i2c-tools
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-comm/zera-i2c/i2cutils.cpp
+++ zera-comm/zera-i2c/i2cutils.cpp
@@ -4,6 +4,7 @@
 #include <sys/ioctl.h>
 #include <QByteArray>
 #include <errno.h>
+#include <linux/i2c.h>
 
 #include "i2cutils.h"
 
@@ -36,4 +37,50 @@ int I2CTransfer(QString deviceNode, int i2cadr, i2c_rdwr_ioctl_data* iodata)
 
     close(fd);
     return(0); // acknowledge
+}
+
+// stolen from https://github.com/mozilla-b2g/i2c-tools
+__s32 i2c_smbus_access(int file, char read_write, __u8 command,
+                       int size, union i2c_smbus_data *data)
+{
+    struct i2c_smbus_ioctl_data args;
+    __s32 err;
+
+    args.read_write = read_write;
+    args.command = command;
+    args.size = size;
+    args.data = data;
+
+    err = ioctl(file, I2C_SMBUS, &args);
+    if (err == -1)
+        err = -errno;
+    return err;
+}
+
+__s32 i2c_smbus_read_byte(int file)
+{
+    union i2c_smbus_data data;
+    int err;
+
+    err = i2c_smbus_access(file, I2C_SMBUS_READ, 0, I2C_SMBUS_BYTE, &data);
+    if (err < 0)
+        return err;
+
+    return 0x0FF & data.byte;
+}
+
+bool I2cPing(QString deviceNode, int i2cadr)
+{
+    bool bDevConnected = false;
+    int file = open(deviceNode.toLatin1().data(), O_RDWR);
+    if(file >=0) {
+        unsigned long funcs;
+        if (ioctl(file, I2C_FUNCS, &funcs) >= 0) {
+            if (ioctl(file, I2C_SLAVE, i2cadr) >= 0) {
+                bDevConnected = i2c_smbus_read_byte(file) >= 0;
+            }
+        }
+        close(file);
+    }
+    return bDevConnected;
 }
