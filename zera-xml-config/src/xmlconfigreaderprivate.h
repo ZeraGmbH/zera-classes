2c44824697abeeabde57f50243ec66d1c0965c1c
Andreas Müller <schnitzeltony@gmail.com>
2022-04-27 18:15:47 +0200

    xmlconfigreaderprivate: Remove doxygen dust
    Comments are obvious and make reading a pain
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-xml-config/src/xmlconfigreaderprivate.h
+++ zera-xml-config/src/xmlconfigreaderprivate.h
@@ -14,97 +14,74 @@
 template<class T_key, class T_value> class FCKDUPHash {
 
 public:
-  void clear()
-  {
-    m_keyList.clear();
-    m_valueList.clear();
-  }
-
-  void insert(T_key key, T_value value)
-  {
-    if(contains(key) == false)
+    void clear()
     {
-      m_keyList.append(key);
-      m_valueList.append(value);
+        m_keyList.clear();
+        m_valueList.clear();
     }
-    else
+
+    void insert(T_key key, T_value value)
     {
-      m_valueList.replace(m_keyList.indexOf(key), value);
+        if(contains(key) == false)
+        {
+            m_keyList.append(key);
+            m_valueList.append(value);
+        }
+        else
+        {
+            m_valueList.replace(m_keyList.indexOf(key), value);
+        }
     }
-  }
 
 
-  T_value value(T_key key) const
-  {
-    T_value retVal;
-    if(contains(key))
+    T_value value(T_key key) const
     {
-      retVal = m_valueList.at(m_keyList.indexOf(key));
+        T_value retVal;
+        if(contains(key))
+        {
+            retVal = m_valueList.at(m_keyList.indexOf(key));
+        }
+        return retVal;
     }
-    return retVal;
-  }
 
-  bool contains(T_key key) const
-  {
-    return m_keyList.contains(key);
-  }
+    bool contains(T_key key) const
+    {
+        return m_keyList.contains(key);
+    }
 
-  QList<T_key> keys() const
-  {
-    return m_keyList;
-  }
+    QList<T_key> keys() const
+    {
+        return m_keyList;
+    }
 
-  QList<T_value> values() const
-  {
-    return m_valueList;
-  }
+    QList<T_value> values() const
+    {
+        return m_valueList;
+    }
 
 private:
 
-  QList<T_key> m_keyList;
-  QList<T_value> m_valueList;
+    QList<T_key> m_keyList;
+    QList<T_value> m_valueList;
 };
 
 
 namespace Zera
 {
-  namespace XMLConfig
-  {
-    class cReader;
-
-    /**
-     * @brief Private data of Zera::XMLConfig::Reader for ABI safety
-     */
-    class cReaderPrivate
+    namespace XMLConfig
     {
-    public:
-      /**
-       * @brief cReaderPrivate Default constructor
-       * @param parent the q_ptr of this instance
-       */
-      cReaderPrivate(cReader *parent);
-
-    private:
-
-      /**
-       * @brief data Data member holding all config values
-       */
-       FCKDUPHash<QString, QString>data;
-
-      /**
-       * @brief schemaFilePath cached path to the schema file
-       */
-      QString schemaFilePath;
-
-      MessageHandler messageHandler;
-
-      /**
-       * @brief q_ptr See Q_DECLARE_PUBLIC
-       */
-      Zera::XMLConfig::cReader *q_ptr;
-
-      Q_DECLARE_PUBLIC(cReader)
-    };
-  }
+        class cReader;
+        class cReaderPrivate
+        {
+        public:
+            cReaderPrivate(cReader *parent);
+        private:
+            FCKDUPHash<QString, QString>data;
+            QString schemaFilePath;
+            MessageHandler messageHandler;
+            Zera::XMLConfig::cReader *q_ptr;
+            Q_DECLARE_PUBLIC(cReader)
+        };
+    }
 }
 #endif // XMLCONFIGREADER_PRIVATE_H
