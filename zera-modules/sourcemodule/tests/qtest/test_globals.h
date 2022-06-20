98fbd28ce557fb5dbca8355a34fc7dca3d48fc6c
Andreas Müller <schnitzeltony@gmail.com>
2022-06-21 01:39:25 +0200

    test_globals.h: Customize QTEST_MAIN
    Hmm we missed warnings - another revisit is necessary
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/tests/qtest/test_globals.h
+++ zera-modules/sourcemodule/tests/qtest/test_globals.h
@@ -5,6 +5,17 @@
 #include "io-device/iodevicebase.h"
 #include "io-device/iodevicedemo.h"
 
+#undef QTEST_MAIN
+#define QTEST_MAIN(TestObject) \
+int main(int argc, char *argv[]) \
+{ \
+    Q_INIT_RESOURCE(resource); \
+    /* Make qCritical in tests for re-used IoTransferDataSingle objects - see IoDeviceBase::prepareSendAndReceive */ \
+    qputenv("QT_FATAL_CRITICALS", "1"); \
+    QTEST_MAIN_IMPL(TestObject) \
+}
+
+
 static constexpr int shortQtEventTimeout = 5;
 
 IoDeviceBase::Ptr createOpenDemoIoDevice(QString deviceInfo = QString());
