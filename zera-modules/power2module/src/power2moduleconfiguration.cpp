1c3234cfbed13cae5de8864fa2a21bd1e6ec3a51
Andreas Müller <schnitzeltony@gmail.com>
2022-07-30 13:03:39 +0200

    power2moduleconfiguration: Prettify
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/power2module/src/power2moduleconfiguration.cpp
+++ zera-modules/power2module/src/power2moduleconfiguration.cpp
@@ -1,10 +1,6 @@
-#include <QPoint>
-#include <QString>
-#include <xmlconfigreader.h>
-
 #include "power2moduleconfiguration.h"
 #include "power2moduleconfigdata.h"
-#include "socket.h"
+#include <xmlconfigreader.h>
 
 namespace POWER2MODULE
 {
@@ -16,18 +12,17 @@ cPower2ModuleConfiguration::cPower2ModuleConfiguration()
     connect(m_pXMLReader, SIGNAL(finishedParsingXML(bool)), this, SLOT(completeConfiguration(bool)));
 }
 
-
 cPower2ModuleConfiguration::~cPower2ModuleConfiguration()
 {
     if (m_pPower2ModulConfigData) delete m_pPower2ModulConfigData;
 }
 
-
 void cPower2ModuleConfiguration::setConfiguration(QByteArray xmlString)
 {
     m_bConfigured = m_bConfigError = false;
 
-    if (m_pPower2ModulConfigData) delete m_pPower2ModulConfigData;
+    if (m_pPower2ModulConfigData)
+        delete m_pPower2ModulConfigData;
     m_pPower2ModulConfigData = new cPower2ModuleConfigData();
 
     QString s = "m0,m1";
@@ -38,7 +33,6 @@ void cPower2ModuleConfiguration::setConfiguration(QByteArray xmlString)
 
     // so now we can set up
     // initializing hash table for xml configuration
-
     m_ConfigXMLMap["pow2modconfpar:configuration:connectivity:debuglevel"] = setDebugLevel;
     m_ConfigXMLMap["pow2modconfpar:configuration:connectivity:ethernet:resourcemanager:ip"] = setRMIp;
     m_ConfigXMLMap["pow2modconfpar:configuration:connectivity:ethernet:resourcemanager:port"] = setRMPort;
@@ -65,13 +59,11 @@ void cPower2ModuleConfiguration::setConfiguration(QByteArray xmlString)
     m_ConfigXMLMap["pow2modconfpar:parameter:interval:time"] = setMeasureIntervalTime;
     m_ConfigXMLMap["pow2modconfpar:parameter:interval:period"] = setMeasureIntervalPeriod;
 
-
     if (m_pXMLReader->loadSchema(defaultXSDFile))
         m_pXMLReader->loadXMLFromString(QString::fromUtf8(xmlString.data(), xmlString.size()));
     else
         m_bConfigError = true;
 }
-
 
 QByteArray cPower2ModuleConfiguration::exportConfiguration()
 {
@@ -87,20 +79,15 @@ QByteArray cPower2ModuleConfiguration::exportConfiguration()
     return m_pXMLReader->getXMLConfig().toUtf8();
 }
 
-
 cPower2ModuleConfigData *cPower2ModuleConfiguration::getConfigurationData()
 {
     return m_pPower2ModulConfigData;
 }
 
-
 void cPower2ModuleConfiguration::configXMLInfo(QString key)
 {
-    bool ok;
-
-    if (m_ConfigXMLMap.contains(key))
-    {
-        ok = true;
+    if (m_ConfigXMLMap.contains(key)) {
+        bool ok = true;
         int cmd = m_ConfigXMLMap[key];
         switch (cmd)
         {
@@ -157,17 +144,16 @@ void cPower2ModuleConfiguration::configXMLInfo(QString key)
             break;
         case setFrequencyOutputCount:
             m_pPower2ModulConfigData->m_nFreqOutputCount = m_pXMLReader->getValue(key).toInt(&ok);
-            if (m_pPower2ModulConfigData->m_nFreqOutputCount > 0)
-                for (int i = 0; i < m_pPower2ModulConfigData->m_nFreqOutputCount; i++)
-                {
+            if (m_pPower2ModulConfigData->m_nFreqOutputCount > 0) {
+                for (int i = 0; i < m_pPower2ModulConfigData->m_nFreqOutputCount; i++) {
                     m_ConfigXMLMap[QString("pow2modconfpar:configuration:frequencyoutput:output:fout%1:name").arg(i+1)] = setfreqout1Name+i;
                     m_ConfigXMLMap[QString("pow2modconfpar:configuration:frequencyoutput:output:fout%1:source").arg(i+1)] = setfreqout1Source+i;
                     m_ConfigXMLMap[QString("pow2modconfpar:configuration:frequencyoutput:output:fout%1:type").arg(i+1)] = setfreqout1Type+i;
                     freqoutconfiguration fconf;
                     m_pPower2ModulConfigData->m_FreqOutputConfList.append(fconf);
                 }
+            }
             break;
-
         case setMeasuringMode:
             m_pPower2ModulConfigData->m_sMeasuringMode.m_sKey = key;
             m_pPower2ModulConfigData->m_sMeasuringMode.m_sValue = m_pXMLReader->getValue(key);
@@ -183,30 +169,20 @@ void cPower2ModuleConfiguration::configXMLInfo(QString key)
 
         default:
             // here we decode the dyn. generated cmd's
-
-            if ((cmd >= setMeasMode1) && (cmd < setMeasMode1 + 32))
-            {
+            if ((cmd >= setMeasMode1) && (cmd < setMeasMode1 + 32)) {
                 cmd -= setMeasMode1;
                 // it is command for setting measuring mode
                 QString measMode = m_pXMLReader->getValue(key);
                 m_pPower2ModulConfigData->m_sMeasmodeList.append(measMode); // for configuration of our engine
             }
-
-            else
-
-            if ((cmd >= setfreqout1Name) && (cmd < setfreqout1Name + 8))
-            {
+            else if ((cmd >= setfreqout1Name) && (cmd < setfreqout1Name + 8)) {
                 cmd -= setfreqout1Name;
                 freqoutconfiguration fconf;
                 fconf = m_pPower2ModulConfigData->m_FreqOutputConfList.at(cmd);
                 fconf.m_sName = m_pXMLReader->getValue(key);
                 m_pPower2ModulConfigData->m_FreqOutputConfList.replace(cmd, fconf);
             }
-
-            else
-
-            if ((cmd >= setfreqout1Source) && (cmd < setfreqout1Source + 8))
-            {
+            else if ((cmd >= setfreqout1Source) && (cmd < setfreqout1Source + 8)) {
                 QString s;
                 s = m_pXMLReader->getValue(key);
                 cmd -= setfreqout1Source;
@@ -223,11 +199,7 @@ void cPower2ModuleConfiguration::configXMLInfo(QString key)
 
                 m_pPower2ModulConfigData->m_FreqOutputConfList.replace(cmd, fconf);
             }
-
-            else
-
-            if ((cmd >= setfreqout1Type) && (cmd < setfreqout1Type+ 8))
-            {
+            else if ((cmd >= setfreqout1Type) && (cmd < setfreqout1Type+ 8)) {
                 QString s;
                 cmd -= setfreqout1Type;
                 s = m_pXMLReader->getValue(key);
@@ -239,18 +211,14 @@ void cPower2ModuleConfiguration::configXMLInfo(QString key)
                     fconf.m_nFoutMode = posPower;
                 if (s == "-")
                     fconf.m_nFoutMode = negPower;
-
                 m_pPower2ModulConfigData->m_FreqOutputConfList.replace(cmd, fconf);
             }
-
         }
         m_bConfigError |= !ok;
     }
-
     else
         m_bConfigError = true;
 }
-
 
 void cPower2ModuleConfiguration::completeConfiguration(bool ok)
 {
@@ -259,4 +227,3 @@ void cPower2ModuleConfiguration::completeConfiguration(bool ok)
 }
 
 }
-
