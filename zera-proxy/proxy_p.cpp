48e3b5ebdd19114c90c92f23b04a8a59a6607028
Andreas Müller <schnitzeltony@gmail.com>
2022-11-20 00:35:13 +0100

    proxy_p.cpp: Prettify
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-proxy/proxy_p.cpp
+++ zera-proxy/proxy_p.cpp
@@ -1,13 +1,11 @@
-#include <QUuid>
-#include <QTimer>
-#include <xiqnetpeer.h>
-#include <netmessages.pb.h>
-
 #include "proxy_p.h"
 #include "proxyclient_p.h"
 #include "proxynetpeer.h"
 #include "proxyconnection.h"
-
+#include <QUuid>
+#include <QTimer>
+#include <xiqnetpeer.h>
+#include <netmessages.pb.h>
 
 namespace Zera
 {
@@ -16,7 +14,6 @@ namespace Proxy
 
 cProxy* cProxyPrivate::singletonInstance=0;
 
-
 cProxyPrivate::cProxyPrivate(cProxy *parent):
     q_ptr(parent)
 {
@@ -24,32 +21,24 @@ cProxyPrivate::cProxyPrivate(cProxy *parent):
     m_nMessageNumber = 0;
 }
 
-
 cProxyClient* cProxyPrivate::getConnection(QString ipadress, quint16 port)
 {
-    cProxyNetPeer* netClient;
-    QUuid uuid;
-    QByteArray binUUid;
-
-    if (ipadress == "")
+    if(ipadress == "")
         ipadress = m_sIPAdress;
-
-    if ((netClient = searchConnection(ipadress, port)) == 0) // look for existing connection
-    {
-        // if not existing we have to create
+    cProxyNetPeer* netClient = searchConnection(ipadress, port);
+    if(!netClient)  {// look for existing connection
         netClient = new cProxyNetPeer(this);
         netClient->setWrapper(&protobufWrapper);
         connect(netClient, &cProxyNetPeer::sigMessageReceived, this, &cProxyPrivate::receiveMessage);
         connect(netClient, &cProxyNetPeer::sigSocketError, this, &cProxyPrivate::receiveTcpError);
         connect(netClient, &cProxyNetPeer::sigConnectionEstablished, this, &cProxyPrivate::registerConnection);
         connect(netClient, &cProxyNetPeer::sigConnectionClosed, this, &cProxyPrivate::registerDisConnection);
-        // netClient->startConnection(ipadress, port);
     }
 
     cProxyClientPrivate *proxyclient = new cProxyClientPrivate(this);
 
-    uuid = QUuid::createUuid(); // we use a per client uuid
-    binUUid = uuid.toRfc4122();
+    QUuid uuid = QUuid::createUuid(); // we use a per client uuid
+    QByteArray binUUid = uuid.toRfc4122();
 
     cProxyConnection *connection = new cProxyConnection(ipadress, port, binUUid, netClient);
     m_ConnectionHash[proxyclient] = connection;
@@ -58,31 +47,26 @@ cProxyClient* cProxyPrivate::getConnection(QString ipadress, quint16 port)
     return proxyclient;
 }
 
-
 cProxyClient* cProxyPrivate::getConnection(quint16 port)
 {
     return getConnection(m_sIPAdress, port);
 }
 
-
 void cProxyPrivate::startConnection(cProxyClientPrivate *client)
 {
     cProxyConnection *connection = m_ConnectionHash[client];
     cProxyNetPeer *peer = connection->m_pNetClient;
-    if (peer->isStarted())
-    {
-        if (peer->isConnected())
+    if(peer->isStarted()) {
+        if(peer->isConnected())
             client->transmitConnection();
     }
     else
         peer->startProxyConnection(connection->m_sIP, connection->m_nPort);
 }
 
-
 bool cProxyPrivate::releaseConnection(cProxyClientPrivate *client)
 {
-    if (m_ConnectionHash.contains(client))
-    {
+    if(m_ConnectionHash.contains(client)) {
         cProxyConnection *connection = m_ConnectionHash.take(client);
         QByteArray binUUid = m_ClientHash.key(client);
         m_ClientHash.remove(binUUid);
@@ -95,21 +79,18 @@ bool cProxyPrivate::releaseConnection(cProxyClientPrivate *client)
         delete connection;
         return true;
     }
-
     return false;
 }
 
 
 quint32 cProxyPrivate::transmitCommand(cProxyClientPrivate* client, ProtobufMessage::NetMessage *message)
 {
-    quint32 nr;
-
     QByteArray ba = m_ConnectionHash[client]->m_binUUID;
     message->set_clientid(ba.data(), ba.size()); // we put the client's id into message
 
-    if (m_nMessageNumber == 0)
+    if(m_nMessageNumber == 0)
         m_nMessageNumber++; // we never use 0 as message number (reserved)
-    nr = m_nMessageNumber;
+    quint32 nr = m_nMessageNumber;
     message->set_messagenr(nr);
     m_nMessageNumber++; // increment message number
 
@@ -117,70 +98,48 @@ quint32 cProxyPrivate::transmitCommand(cProxyClientPrivate* client, ProtobufMess
     return nr;
 }
 
-
 void cProxyPrivate::setIPAdress(QString ipAddress)
 {
     m_sIPAdress = ipAddress;
 }
 
-
 void cProxyPrivate::receiveMessage(std::shared_ptr<google::protobuf::Message> message)
 {
     std::shared_ptr<ProtobufMessage::NetMessage> netMessage = std::static_pointer_cast<ProtobufMessage::NetMessage>(message);
-
-    if (netMessage->has_clientid())
-    {
+    if(netMessage->has_clientid()) {
         QByteArray key(netMessage->clientid().data(), netMessage->clientid().size());
-        if (m_ClientHash.contains(key))
-        {
+        if(m_ClientHash.contains(key))
             m_ClientHash[key]->transmitAnswer(netMessage);
-        }
         else
-        {
-            qDebug() << "Unknown ClientID; " << netMessage->clientid().size();
-        }
+            qWarning() << "Unknown ClientID; " << netMessage->clientid().size();
     }
     // ? todo error handling in case of unknown clientid ?
     else
-    {
-        qDebug() << "No ClientID";
-    }
+        qWarning() << "No ClientID";
 }
-
 
 void cProxyPrivate::receiveTcpError(QAbstractSocket::SocketError errorCode)
 {
     cProxyNetPeer* netClient = qobject_cast<cProxyNetPeer*>(QObject::sender());
-
     QHashIterator<cProxyClientPrivate*, cProxyConnection*> it(m_ConnectionHash);
-
-    while (it.hasNext())
-    {
-        cProxyConnection *pC;
+    while(it.hasNext()) {
         it.next();
-        pC = it.value();
-        if (pC->m_pNetClient == netClient) // we found a client that was connected to netclient
-        {
+        cProxyConnection *pC = it.value();
+        if(pC->m_pNetClient == netClient) { // we found a client that was connected to netclient
             cProxyClientPrivate* client = it.key();
             client->transmitError(errorCode); // so this client will forward error
         }
     }
 }
 
-
 void cProxyPrivate::registerConnection()
 {
     cProxyNetPeer* netClient = qobject_cast<cProxyNetPeer*>(QObject::sender());
-
     QHashIterator<cProxyClientPrivate*, cProxyConnection*> it(m_ConnectionHash);
-
-    while (it.hasNext())
-    {
-        cProxyConnection *pC;
+    while(it.hasNext()) {
         it.next();
-        pC = it.value();
-        if (pC->m_pNetClient == netClient) // we found a client that tried to connected to netclient
-        {
+        cProxyConnection *pC = it.value();
+        if(pC->m_pNetClient == netClient) { // we found a client that tried to connected to netclient
             cProxyClientPrivate* client = it.key();
             client->transmitConnection();
         }
@@ -190,40 +149,29 @@ void cProxyPrivate::registerConnection()
 void cProxyPrivate::registerDisConnection()
 {
     cProxyNetPeer* netClient = qobject_cast<cProxyNetPeer*>(QObject::sender());
-
     QHashIterator<cProxyClientPrivate*, cProxyConnection*> it(m_ConnectionHash);
-
-    while (it.hasNext())
-    {
-        cProxyConnection *pC;
+    while(it.hasNext()) {
         it.next();
-        pC = it.value();
-        if (pC->m_pNetClient == netClient) // we found a client that tried to connected to netclient
-        {
+        cProxyConnection *pC = it.value();
+        if(pC->m_pNetClient == netClient) { // we found a client that tried to connected to netclient
             cProxyClientPrivate* client = it.key();
             client->transmitDisConnection(); // so this client will be forwarded connection
         }
     }
 }
 
-
 cProxyNetPeer* cProxyPrivate::searchConnection(QString ip, quint16 port)
 {
-    cProxyNetPeer* lnetClient = 0;
+    cProxyNetPeer* lnetClient = nullptr;
     QHashIterator<cProxyClientPrivate*, cProxyConnection*> it(m_ConnectionHash);
-
-    while (it.hasNext())
-    {
-        cProxyConnection *pC;
+    while(it.hasNext()) {
         it.next();
-        pC = it.value();
-        if ( (pC->m_sIP == ip) && (pC->m_nPort == port))
-        {
+        cProxyConnection *pC = it.value();
+        if( pC->m_sIP == ip && pC->m_nPort == port) {
              lnetClient = pC->m_pNetClient;
              break;
         }
     }
-
     return lnetClient;
 }
 
