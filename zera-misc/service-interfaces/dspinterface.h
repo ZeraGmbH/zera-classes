35cb630dbeae10f8fe55898625eb29927a8a37bf
Andreas Müller <schnitzeltony@gmail.com>
2022-11-23 14:41:11 +0100

    service-interfaces: Make methods virtual again for tests
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-misc/service-interfaces/dspinterface.h
+++ zera-misc/service-interfaces/dspinterface.h
@@ -27,42 +27,42 @@ public:
     cDSPInterface();
     virtual ~cDSPInterface();
     void setClient(Zera::Proxy::cProxyClient *client);
-    quint32 bootDsp();
-    quint32 resetDsp();
-    quint32 setDSPBootPath(QString path);
-    quint32 setSamplingSystem(int chncount, int samp_per, int samp_mper); // nmuber of channels, samples/signalperiod, samples/measperiod
-    quint32 varList2Dsp(); // send the var-list to dsp server
-    quint32 cmdList2Dsp(); // send cyclic command list to the dsp server
-    quint32 intList2Dsp(); // send interrupt command list to the dsp server
-    int cmdListCount(); // returns the number of command in cyclist program list
-    int intListCount(); // returns the number of command in intlist program list
-    void clearCmdList(); // clears all cmd lists
-    void clearMemLists(); // clears all memory lists (memhandles)
-    quint32 setSignalRouting(tRouting* routingtab); // set signal routing
-    quint32 setDsp61850SourceAdr(cETHAdress& ethadr); // set eth source adr
-    quint32 setDsp61850DestAdr(cETHAdress& ethadr); // set eth dest adr
-    quint32 setDsp61850PriorityTagged(quint32 priotag); // set entries for eth frame decoder
-    quint32 setDsp61850EthTypeAppId(quint32 typAppid); // same
-    quint32 setDsp61850EthSynchronisation(quint32 syncdata); // setting strong sync. and dataset
-    quint32 resetMaximum(); // clear maximum memory
-    quint32 triggerIntHKSK(quint32 hksk); // trigger start hksk in intlist
-    void addCycListItem(QString cmd); // appends new command to cyclic list
-    void addIntListItem(QString cmd); // same for interrupt list
-    cDspMeasData* getMemHandle(QString name); // init a new memory group and return handle
-    void deleteMemHandle(cDspMeasData* memhandle);
-    quint32 activateInterface(); // load var- and cmdlists to dsp (starts theprogram on dsp)
-    quint32 deactivateInterface(); // unload ...
-    quint32 dataAcquisition(cDspMeasData* memgroup); // reads all vars of this memorygroup that are of type vapplication
-    quint32 dspMemoryRead(cDspMeasData* memgroup, DSPDATA::dType type = DSPDATA::dFloat); // reads all vars of this memorygroup
-    void setVarData(cDspMeasData* memgroup, QString datalist, DSPDATA::dType type = DSPDATA::dFloat); //set the values of memgroup from qstring
-    quint32 dspMemoryWrite(cDspMeasData* memgroup); // writes all vars of this memorygroup with type
-    float* data(cDspMeasData* memgroup, QString name); // returns a pointer to the vars read from dsp
-    void getData(cDspMeasData* memgroup, QVector<float>& vector); // copy the memgroups data into qvector
-    quint32 setGainCorrection(int chn, float val); // sets gaincorrection for 1 channel
-    quint32 setPhaseCorrection(int chn, float val); // sets phase correction for 1 channel
-    quint32 setOffsetCorrection(int chn, float val); // sets offset correction for 1 channel
-    quint32 readDeviceVersion(); // reads the dsp devices version
-    quint32 readServerVersion(); // what do you think ?
+    virtual quint32 bootDsp();
+    virtual quint32 resetDsp();
+    virtual quint32 setDSPBootPath(QString path);
+    virtual quint32 setSamplingSystem(int chncount, int samp_per, int samp_mper); // nmuber of channels, samples/signalperiod, samples/measperiod
+    virtual quint32 varList2Dsp(); // send the var-list to dsp server
+    virtual quint32 cmdList2Dsp(); // send cyclic command list to the dsp server
+    virtual quint32 intList2Dsp(); // send interrupt command list to the dsp server
+    virtual int cmdListCount(); // returns the number of command in cyclist program list
+    virtual int intListCount(); // returns the number of command in intlist program list
+    virtual void clearCmdList(); // clears all cmd lists
+    virtual void clearMemLists(); // clears all memory lists (memhandles)
+    virtual quint32 setSignalRouting(tRouting* routingtab); // set signal routing
+    virtual quint32 setDsp61850SourceAdr(cETHAdress& ethadr); // set eth source adr
+    virtual quint32 setDsp61850DestAdr(cETHAdress& ethadr); // set eth dest adr
+    virtual quint32 setDsp61850PriorityTagged(quint32 priotag); // set entries for eth frame decoder
+    virtual quint32 setDsp61850EthTypeAppId(quint32 typAppid); // same
+    virtual quint32 setDsp61850EthSynchronisation(quint32 syncdata); // setting strong sync. and dataset
+    virtual quint32 resetMaximum(); // clear maximum memory
+    virtual quint32 triggerIntHKSK(quint32 hksk); // trigger start hksk in intlist
+    virtual void addCycListItem(QString cmd); // appends new command to cyclic list
+    virtual void addIntListItem(QString cmd); // same for interrupt list
+    virtual cDspMeasData* getMemHandle(QString name); // init a new memory group and return handle
+    virtual void deleteMemHandle(cDspMeasData* memhandle);
+    virtual quint32 activateInterface(); // load var- and cmdlists to dsp (starts theprogram on dsp)
+    virtual quint32 deactivateInterface(); // unload ...
+    virtual quint32 dataAcquisition(cDspMeasData* memgroup); // reads all vars of this memorygroup that are of type vapplication
+    virtual quint32 dspMemoryRead(cDspMeasData* memgroup, DSPDATA::dType type = DSPDATA::dFloat); // reads all vars of this memorygroup
+    virtual void setVarData(cDspMeasData* memgroup, QString datalist, DSPDATA::dType type = DSPDATA::dFloat); //set the values of memgroup from qstring
+    virtual quint32 dspMemoryWrite(cDspMeasData* memgroup); // writes all vars of this memorygroup with type
+    virtual float* data(cDspMeasData* memgroup, QString name); // returns a pointer to the vars read from dsp
+    virtual void getData(cDspMeasData* memgroup, QVector<float>& vector); // copy the memgroups data into qvector
+    virtual quint32 setGainCorrection(int chn, float val); // sets gaincorrection for 1 channel
+    virtual quint32 setPhaseCorrection(int chn, float val); // sets phase correction for 1 channel
+    virtual quint32 setOffsetCorrection(int chn, float val); // sets offset correction for 1 channel
+    virtual quint32 readDeviceVersion(); // reads the dsp devices version
+    virtual quint32 readServerVersion(); // what do you think ?
 
 signals:
     void tcpError(QAbstractSocket::SocketError errorCode);
--- zera-misc/service-interfaces/pcbinterface.h
+++ zera-misc/service-interfaces/pcbinterface.h
@@ -22,89 +22,89 @@ public:
     virtual ~cPCBInterface();
     void setClient(Zera::Proxy::cProxyClient *client);
     // all commands to sense interface
-    quint32 getDSPChannel(QString chnName); // int
-    quint32 getStatus(QString chnName); // int
-    quint32 resetStatus(QString chnName); // reply (ack, nak..)
-    quint32 getAlias(QString chnName); // qstring
-    quint32 getType(QString chnName); // qstring
-    quint32 getUnit(QString chnName); // qstring
-    quint32 getRange(QString chnName); // qstring
-    quint32 getRangeList(QString chnName); // qstringlist
-    quint32 getAlias(QString chnName, QString rngName); // qstring
-    quint32 getType(QString chnName, QString rngName); // int
-    quint32 getUrvalue(QString chnName, QString rngName); // double
-    quint32 getUrvalue(QString chnName); // double
-    quint32 getRejection(QString chnName, QString rngName); // double
-    quint32 getOVRejection(QString chnName, QString rngName); // double
-    quint32 getADCRejection(QString chnName, QString rngName); // double
-    quint32 isAvail(QString chnName, QString rngName); // bool
-    quint32 getGainCorrection(QString chnName, QString rngName, double at); // double
-    quint32 getAdjGainCorrection(QString chnName, QString rngName, double at); // double
-    quint32 getOffsetCorrection(QString chnName, QString rngName, double at); // double
-    quint32 getAdjOffsetCorrection(QString chnName, QString rngName, double at); // double
-    quint32 getPhaseCorrection(QString chnName, QString rngName, double at); // double
-    quint32 getAdjPhaseCorrection(QString chnName, QString rngName, double at); // double
-    quint32 setRange(QString chnName, QString rngName); // reply (ack, nak..)
-    quint32 setMMode(QString mmode); // we set the sense mode we get  reply (ack, nak..)
-    quint32 adjustComputation(); // all correction coefficients will be computed
-    quint32 adjustStorage(); // all correction data will be stored
-    quint32 adjustStorageClamp(); // all clamp correction data will be stored
-    quint32 adjustInit(QString chnName, QString rngName);
-    quint32 setAdjustGainStatus(QString chnName, QString rngName, int stat);
-    quint32 setAdjustPhaseStatus(QString chnName, QString rngName, int stat);
-    quint32 setAdjustOffsetStatus(QString chnName, QString rngName, int stat);
-    quint32 setGainNode(QString chnName, QString rngName, int nr, double corr, double at); // node nr, correction, loadpoint
-    quint32 setPhaseNode(QString chnName, QString rngName, int nr, double corr, double at); // node nr, correction, loadpoint = frequency
-    quint32 setOffsetNode(QString chnName, QString rngName, int nr, double corr, double at); // node nr, correction, loadpoint
+    virtual quint32 getDSPChannel(QString chnName); // int
+    virtual quint32 getStatus(QString chnName); // int
+    virtual quint32 resetStatus(QString chnName); // reply (ack, nak..)
+    virtual quint32 getAlias(QString chnName); // qstring
+    virtual quint32 getType(QString chnName); // qstring
+    virtual quint32 getUnit(QString chnName); // qstring
+    virtual quint32 getRange(QString chnName); // qstring
+    virtual quint32 getRangeList(QString chnName); // qstringlist
+    virtual quint32 getAlias(QString chnName, QString rngName); // qstring
+    virtual quint32 getType(QString chnName, QString rngName); // int
+    virtual quint32 getUrvalue(QString chnName, QString rngName); // double
+    virtual quint32 getUrvalue(QString chnName); // double
+    virtual quint32 getRejection(QString chnName, QString rngName); // double
+    virtual quint32 getOVRejection(QString chnName, QString rngName); // double
+    virtual quint32 getADCRejection(QString chnName, QString rngName); // double
+    virtual quint32 isAvail(QString chnName, QString rngName); // bool
+    virtual quint32 getGainCorrection(QString chnName, QString rngName, double at); // double
+    virtual quint32 getAdjGainCorrection(QString chnName, QString rngName, double at); // double
+    virtual quint32 getOffsetCorrection(QString chnName, QString rngName, double at); // double
+    virtual quint32 getAdjOffsetCorrection(QString chnName, QString rngName, double at); // double
+    virtual quint32 getPhaseCorrection(QString chnName, QString rngName, double at); // double
+    virtual quint32 getAdjPhaseCorrection(QString chnName, QString rngName, double at); // double
+    virtual quint32 setRange(QString chnName, QString rngName); // reply (ack, nak..)
+    virtual quint32 setMMode(QString mmode); // we set the sense mode we get  reply (ack, nak..)
+    virtual quint32 adjustComputation(); // all correction coefficients will be computed
+    virtual quint32 adjustStorage(); // all correction data will be stored
+    virtual quint32 adjustStorageClamp(); // all clamp correction data will be stored
+    virtual quint32 adjustInit(QString chnName, QString rngName);
+    virtual quint32 setAdjustGainStatus(QString chnName, QString rngName, int stat);
+    virtual quint32 setAdjustPhaseStatus(QString chnName, QString rngName, int stat);
+    virtual quint32 setAdjustOffsetStatus(QString chnName, QString rngName, int stat);
+    virtual quint32 setGainNode(QString chnName, QString rngName, int nr, double corr, double at); // node nr, correction, loadpoint
+    virtual quint32 setPhaseNode(QString chnName, QString rngName, int nr, double corr, double at); // node nr, correction, loadpoint = frequency
+    virtual quint32 setOffsetNode(QString chnName, QString rngName, int nr, double corr, double at); // node nr, correction, loadpoint
 
     // all commands to sample interface
-    quint32 getAliasSample(QString chnName); // qstring
-    quint32 getSampleRate(); // int the actual set sampling rate
-    quint32 getRangeListSample(QString chnName); // qstringlist
-    quint32 setRangeSample(QString chnName, QString rngName);
-    quint32 setPLLChannel(QString samplechnName, QString pllchnName);
+    virtual quint32 getAliasSample(QString chnName); // qstring
+    virtual quint32 getSampleRate(); // int the actual set sampling rate
+    virtual quint32 getRangeListSample(QString chnName); // qstringlist
+    virtual quint32 setRangeSample(QString chnName, QString rngName);
+    virtual quint32 setPLLChannel(QString samplechnName, QString pllchnName);
 
     // all commands to source interface
-    quint32 getAliasSource(QString chnName); // qstring
-    quint32 getDSPChannelSource(QString chnName); // int
-    quint32 getFormFactorSource(QString chnName); // double
-    quint32 getConstantSource(QString chnName); // double
-    quint32 setConstantSource(QString chnName, double constant); // double
-    quint32 getPowTypeSource(QString chnName); // qstring
-    quint32 setPowTypeSource(QString chnName, QString ptype);
+    virtual quint32 getAliasSource(QString chnName); // qstring
+    virtual quint32 getDSPChannelSource(QString chnName); // int
+    virtual quint32 getFormFactorSource(QString chnName); // double
+    virtual quint32 getConstantSource(QString chnName); // double
+    virtual quint32 setConstantSource(QString chnName, double constant); // double
+    virtual quint32 getPowTypeSource(QString chnName); // qstring
+    virtual quint32 setPowTypeSource(QString chnName, QString ptype);
 
     // all commands to schead interface
-    quint32 getAliasSchead(QString chnName); // qstring
+    virtual quint32 getAliasSchead(QString chnName); // qstring
 
     // all commands to frqinput interface
-    quint32 getAliasFrqinput(QString chnName); // qstring
+    virtual quint32 getAliasFrqinput(QString chnName); // qstring
 
     // all commands to status interface
-    quint32 getAdjustmentStatus(); // int
-    quint32 getAuthorizationStatus();
-    quint32 getPCBErrorStatus();
+    virtual quint32 getAdjustmentStatus(); // int
+    virtual quint32 getAuthorizationStatus();
+    virtual quint32 getPCBErrorStatus();
 
     // resource query more generell
-    quint32 resourceAliasQuery(QString resourceType, QString resourceName);
+    virtual quint32 resourceAliasQuery(QString resourceType, QString resourceName);
 
-    quint32 registerNotifier(QString query, QString notifier); // register for notification on change
-    quint32 unregisterNotifiers(); // unregister from all notifications
+    virtual quint32 registerNotifier(QString query, QString notifier); // register for notification on change
+    virtual quint32 unregisterNotifiers(); // unregister from all notifications
 
     // all commands to system interface
-    quint32 readServerVersion();
-    quint32 readPCBVersion();
-    quint32 readFPGAVersion();
-    quint32 readCTRLVersion();
-    quint32 readSerialNr();
-    quint32 writeSerialNr(QString serNr);
-    quint32 getAdjustmentChksum();
-    quint32 getPCBAdjustmentData();
-    quint32 setPCBAdjustmentData(QString xmlpcb);
-    quint32 getClampAdjustmentData();
-    quint32 setClampAdjustmentData(QString xmlclamp);
+    virtual quint32 readServerVersion();
+    virtual quint32 readPCBVersion();
+    virtual quint32 readFPGAVersion();
+    virtual quint32 readCTRLVersion();
+    virtual quint32 readSerialNr();
+    virtual quint32 writeSerialNr(QString serNr);
+    virtual quint32 getAdjustmentChksum();
+    virtual quint32 getPCBAdjustmentData();
+    virtual quint32 setPCBAdjustmentData(QString xmlpcb);
+    virtual quint32 getClampAdjustmentData();
+    virtual quint32 setClampAdjustmentData(QString xmlclamp);
 
     // sending a transparent command
-    quint32 transparentCommand(QString cmd);
+    virtual quint32 transparentCommand(QString cmd);
 
 signals:
     void tcpError(QAbstractSocket::SocketError errorCode);
