9f02f8c030ba23366381d8660a7058162516751e
Andreas Müller <schnitzeltony@gmail.com>
2022-11-24 10:04:07 +0100

    SignalWaiter: Split up setup and wait for more flexibility
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules-common/tests/signalwaiter.h
+++ zera-modules-common/tests/signalwaiter.h
@@ -9,54 +9,58 @@ class SignalWaiter : public QObject
 {
     Q_OBJECT
 public:
-    enum WaitResult {
-        WAIT_OK_SIG,
-        WAIT_ERR_SIG,
-        WAIT_ABORT,
-        WAIT_TIMEOUT,
-        WAIT_UNDEF
-    };
+    enum WaitResult { WAIT_OK_SIG, WAIT_ERR_SIG, WAIT_ABORT, WAIT_TIMEOUT, WAIT_UNDEF };
+
     template <typename Func1>
-    WaitResult WaitForSignal(const typename QtPrivate::FunctionPointer<Func1>::Object *doneSender, Func1 doneSignal,
-                       int timeout = 0)
-    {
-        return WaitForSignal(doneSender, doneSignal, this, &SignalWaiter::sigDefaultNoError, timeout);
+    SignalWaiter(const typename QtPrivate::FunctionPointer<Func1>::Object *doneSender, Func1 doneSignal,
+                 int timeout = 0) {
+        setup(doneSender, doneSignal, this, &SignalWaiter::sigDefaultNoError, timeout);
     }
     template <typename Func1, typename Func2>
-    WaitResult WaitForSignal(const typename QtPrivate::FunctionPointer<Func1>::Object *doneSender, Func1 doneSignal,
-                             const typename QtPrivate::FunctionPointer<Func2>::Object *errorSender, Func2 errorSignal,
-                             int timeout = 0)
-    {
-        WaitResult result = WAIT_UNDEF;
-        connect(doneSender, doneSignal, [&]() {
-            result = WAIT_OK_SIG;
-            m_eventLoop.quit();
-        });
-        connect(errorSender, errorSignal, [&]() {
-            result = WAIT_ERR_SIG;
-            m_eventLoop.quit();
-        });
-        connect(this, &SignalWaiter::sigAbort, [&]() {
-            result = WAIT_ABORT;
-            m_eventLoop.quit();
-        });
-        QTimer timer;
-        connect(&timer, &QTimer::timeout, this, [&]() {
-            result = WAIT_TIMEOUT;
-            m_eventLoop.quit();
-        });
-        if(timeout > 0) {
-            timer.start(timeout);
-        }
+    SignalWaiter(const typename QtPrivate::FunctionPointer<Func1>::Object *doneSender, Func1 doneSignal,
+                 const typename QtPrivate::FunctionPointer<Func2>::Object *errorSender, Func2 errorSignal,
+                 int timeout = 0) {
+        setup(doneSender, doneSignal, errorSender, errorSignal, timeout);
+    }
+
+    WaitResult wait() {
         m_eventLoop.exec();
-        return result;
-    };
+        return m_waitResult;
+    }
     void abort() { emit sigAbort(); }
 signals:
     void sigDefaultNoError();
     void sigAbort();
 private:
+    template <typename Func1, typename Func2>
+    void setup(const typename QtPrivate::FunctionPointer<Func1>::Object *doneSender, Func1 doneSignal,
+                             const typename QtPrivate::FunctionPointer<Func2>::Object *errorSender, Func2 errorSignal,
+                             int timeout = 0)
+    {
+        connect(doneSender, doneSignal, [&]() {
+            m_waitResult = WAIT_OK_SIG;
+            m_eventLoop.quit();
+        });
+        connect(errorSender, errorSignal, [&]() {
+            m_waitResult = WAIT_ERR_SIG;
+            m_eventLoop.quit();
+        });
+        connect(this, &SignalWaiter::sigAbort, [&]() {
+            m_waitResult = WAIT_ABORT;
+            m_eventLoop.quit();
+        });
+        connect(&m_timeoutTimer, &QTimer::timeout, this, [&]() {
+            m_waitResult = WAIT_TIMEOUT;
+            m_eventLoop.quit();
+        });
+        if(timeout > 0) {
+            m_timeoutTimer.start(timeout);
+        }
+    };
+
     QEventLoop m_eventLoop;
+    QTimer m_timeoutTimer;
+    WaitResult m_waitResult = WAIT_UNDEF;
 };
 
 #endif // SIGNALWAITER_H
--- zera-modules-common/tests/test_blockunblockwrapper.cpp
+++ zera-modules-common/tests/test_blockunblockwrapper.cpp
@@ -7,44 +7,44 @@ QTEST_MAIN(test_blockunblockwrapper)
 
 void test_blockunblockwrapper::detectSignal()
 {
-    SignalWaiter waiter;
     QTimer timer;
+    SignalWaiter waiter(&timer, &QTimer::timeout);
     timer.start(1);
-    QCOMPARE(SignalWaiter::WAIT_OK_SIG, waiter.WaitForSignal(&timer, &QTimer::timeout));
+    QCOMPARE(SignalWaiter::WAIT_OK_SIG, waiter.wait());
 }
 
 void test_blockunblockwrapper::detectSignalWithNoneErrSig()
 {
-    SignalWaiter waiter;
     QTimer timer;
+    SignalWaiter waiter(&timer, &QTimer::timeout,
+                        this, &test_blockunblockwrapper::sigNone);
     timer.start(1);
-    QCOMPARE(SignalWaiter::WAIT_OK_SIG, waiter.WaitForSignal(&timer, &QTimer::timeout,
-                                                             this, &test_blockunblockwrapper::sigNone));
+    QCOMPARE(SignalWaiter::WAIT_OK_SIG, waiter.wait());
 }
 
 void test_blockunblockwrapper::handleAbort()
 {
-    SignalWaiter waiter;
     QTimer timer;
+    SignalWaiter waiter(this, &test_blockunblockwrapper::sigNone);
     timer.start(1);
     connect(&timer, &QTimer::timeout, this, [&]() {
         waiter.abort();
     });
-    QCOMPARE(SignalWaiter::WAIT_ABORT, waiter.WaitForSignal(this, &test_blockunblockwrapper::sigNone));
+    QCOMPARE(SignalWaiter::WAIT_ABORT, waiter.wait());
 }
 
 void test_blockunblockwrapper::detectError()
 {
-    SignalWaiter waiter;
     QTimer timer;
+    SignalWaiter waiter(this, &test_blockunblockwrapper::sigNone,
+                        &timer, &QTimer::timeout);
     timer.start(1);
-    QCOMPARE(SignalWaiter::WAIT_ERR_SIG, waiter.WaitForSignal(this, &test_blockunblockwrapper::sigNone,
-                                                              &timer, &QTimer::timeout));
+    QCOMPARE(SignalWaiter::WAIT_ERR_SIG, waiter.wait());
 }
 
 void test_blockunblockwrapper::detectTimeout()
 {
-    SignalWaiter waiter;
-    QCOMPARE(SignalWaiter::WAIT_TIMEOUT, waiter.WaitForSignal(this, &test_blockunblockwrapper::sigNone,
-                                                              this, &test_blockunblockwrapper::sigNone, 1));
+    SignalWaiter waiter(this, &test_blockunblockwrapper::sigNone,
+                        this, &test_blockunblockwrapper::sigNone, 1);
+    QCOMPARE(SignalWaiter::WAIT_TIMEOUT, waiter.wait());
 }
