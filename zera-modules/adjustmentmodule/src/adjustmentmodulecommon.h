3fe3d1c1de606d6b742998c1b87a38fb990bbf73
Andreas Müller <schnitzeltony@gmail.com>
2022-11-26 00:02:00 +0100

    adjustmentmodule: Move port hash from common to activation only
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/adjustmentmodule/src/adjustmentmodulecommon.h
+++ zera-modules/adjustmentmodule/src/adjustmentmodulecommon.h
@@ -15,7 +15,6 @@ class cAdjustChannelInfo;
 struct AdjustmentModuleActivateData
 {
     QList<Zera::Proxy::cProxyClient*> m_pcbClientList; // a list with our pcb clients ... for clean up
-    QHash<QString,int> m_chnPortHash; // a hash for our channels ethernet ports
     QHash<QString, cAdjustChannelInfo*> m_adjustChannelInfoHash;
     QHash<int, QString> m_portChannelHash; // a list of ports for which we have established connection
     QList<Zera::Server::cPCBInterface*> m_pcbInterfaceList; // a list of pcbinterfaces ... for clean up
