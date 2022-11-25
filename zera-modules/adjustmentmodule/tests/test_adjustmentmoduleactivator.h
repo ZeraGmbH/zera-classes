a41d8960fd6e38117aa3a5e78973fe16442e53ca
Asmae Kaouissi <a.kaouissi@zera.de>
2022-11-25 16:37:20 +0100

    Initial test for adjustmentmodule
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

---
+++ zera-modules/adjustmentmodule/tests/test_adjustmentmoduleactivator.h
@@ -0,0 +1,13 @@
+#ifndef TEST_ADJUSTMENTMODULEACTIVATOR_H
+#define TEST_ADJUSTMENTMODULEACTIVATOR_H
+
+#include <QObject>
+
+class test_adjustmentmoduleactivator : public QObject
+{
+    Q_OBJECT
+private slots:
+    void instanciate();
+};
+
+#endif // TEST_ADJUSTMENTMODULEACTIVATOR_H
