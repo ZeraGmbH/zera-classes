f6ff9008760b52c6a64147ed6762d238a9b47ca1
Andreas Müller <schnitzeltony@gmail.com>
2022-11-25 18:51:49 +0100

    Get more objects into adjustment module test
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/adjustmentmodule/tests/test_adjustmentmoduleactivator.cpp
+++ zera-modules/adjustmentmodule/tests/test_adjustmentmoduleactivator.cpp
@@ -1,7 +1,10 @@
 #include "adjustmentmoduleactivator.h"
+#include "adjustmentmoduleconfiguration.h"
 #include "test_adjustmentmoduleactivator.h"
 #include "vs_veinhash.h"
 #include <QTest>
+#include <QFile>
+#include <QByteArray>
 
 QTEST_MAIN(test_adjustmentmoduleactivator)
 
@@ -10,7 +13,15 @@ void test_adjustmentmoduleactivator::instanciate()
     using namespace ADJUSTMENTMODULE;
     AdjustmentModuleActivateData activationData;
     Zera::Proxy::cProxy* proxy= Zera::Proxy::cProxy::getInstance();
-    std::shared_ptr<cBaseModuleConfiguration> pConfiguration;
+
+    std::shared_ptr<cBaseModuleConfiguration> pConfiguration = std::make_shared<cAdjustmentModuleConfiguration>();
+
+    QString configPath = QString(MODMAN_CONFIG_PATH);
+    QFile xmlFile(configPath + "/mt310s2-adjustmentmodule.xml");
+    xmlFile.open(QIODevice::Unbuffered | QIODevice::ReadOnly);
+    QByteArray xmlConfigData = xmlFile.readAll();
+    pConfiguration->setConfiguration(xmlConfigData); // trouble loading xsd
+
     VeinStorage::VeinHash storagesystem;
     cAdjustmentModule module(1, proxy, 3, &storagesystem);
 
