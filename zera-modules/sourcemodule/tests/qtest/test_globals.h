757bcc5187692d52cf7f12b6296e21f055046957
Andreas Müller <schnitzeltony@gmail.com>
2022-12-02 20:40:37 +0100

    sourcemodule/tests: Increase timeout - we have seen it failing
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/sourcemodule/tests/qtest/test_globals.h
+++ zera-modules/sourcemodule/tests/qtest/test_globals.h
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) \
 }
 
 
-static constexpr int shortQtEventTimeout = 5;
+static constexpr int shortQtEventTimeout = 6;
 
 IoDeviceBase::Ptr createOpenDemoIoDevice(QString deviceInfo = QString());
 void setDemoIoFixedTimeout(IoDeviceBase::Ptr ioDevice, int timeoutMs);
