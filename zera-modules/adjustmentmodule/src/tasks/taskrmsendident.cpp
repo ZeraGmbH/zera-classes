36017ff76a78a6b40715944771ee49cad7b39a17
Andreas Müller <schnitzeltony@gmail.com>
2022-12-05 20:38:43 +0100

    adjustmentmodule: Add tasks for rm connection and rm ident
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/adjustmentmodule/src/tasks/taskrmsendident.cpp
@@ -0,0 +1,34 @@
+#include "taskrmsendident.h"
+#include "reply.h"
+#include "errormessages.h"
+
+namespace ADJUSTMENTMODULE
+{
+
+std::unique_ptr<TaskRmSendIdent> TaskRmSendIdent::create(Zera::Server::cRMInterface *rmInterface, std::function<void (QVariant)> errMsgFunction)
+{
+    return std::make_unique<TaskRmSendIdent>(rmInterface, errMsgFunction);
+}
+
+TaskRmSendIdent::TaskRmSendIdent(Zera::Server::cRMInterface *rmInterface, std::function<void (QVariant)> errMsgFunction) :
+    m_rmInterface(rmInterface),
+    m_errMsgFunction(errMsgFunction)
+{
+}
+
+void TaskRmSendIdent::start()
+{
+    connect(m_rmInterface, &Zera::Server::cRMInterface::serverAnswer, this, &TaskRmSendIdent::onRmAnswer);
+    m_msgnr = m_rmInterface->rmIdent(QString("Adjustment"));
+}
+
+void TaskRmSendIdent::onRmAnswer(quint32 msgnr, quint8 reply)
+{
+    if(msgnr == m_msgnr) {
+        if(reply != ack)
+            m_errMsgFunction(rmidentErrMSG);
+        emit sigFinish(reply == ack, getTaskId());
+    }
+}
+
+}
