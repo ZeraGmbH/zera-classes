3d06b210fdb1a4e8e51da731100beb0d1d291546
Andreas Müller <schnitzeltony@gmail.com>
2022-12-06 20:46:28 +0100

    More on tasks
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/adjustmentmodule/src/tasks/taskrmcheckchannelsavail.h
@@ -0,0 +1,23 @@
+#ifndef TASKRMCHECKCHANNELSAVAIL_H
+#define TASKRMCHECKCHANNELSAVAIL_H
+
+#include "taskcomposit.h"
+#include "rminterface.h"
+#include <QStringList>
+
+class TaskRmCheckChannelsAvail : public TaskComposite
+{
+    Q_OBJECT
+public:
+    static std::unique_ptr<TaskRmCheckChannelsAvail> create(Zera::Server::RMInterfacePtr rmInterface, QStringList expectedChannels);
+    TaskRmCheckChannelsAvail(Zera::Server::RMInterfacePtr rmInterface, QStringList expectedChannels);
+    void start() override;
+private slots:
+    void onRmAnswer(quint32 msgnr, quint8 reply, QVariant answer);
+private:
+    Zera::Server::RMInterfacePtr m_rmInterface;
+    QStringList m_expectedChannels;
+    quint32 m_msgnr;
+};
+
+#endif // TASKRMCHECKCHANNELSAVAIL_H
