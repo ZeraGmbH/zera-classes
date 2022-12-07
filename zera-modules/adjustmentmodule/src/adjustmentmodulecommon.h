d62500235d840e3eb4beb893ba6ba14e76fa5705
Andreas Müller <schnitzeltony@gmail.com>
2022-12-07 23:35:49 +0100

    adjustmentmodule: Move pcb connetion-start to tasks
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/adjustmentmodule/src/adjustmentmodulecommon.h
+++ zera-modules/adjustmentmodule/src/adjustmentmodulecommon.h
@@ -15,11 +15,12 @@ class cAdjustChannelInfo;
 
 struct AdjustmentModuleActivateData
 {
-    QList<Zera::Proxy::cProxyClient*> m_pcbClientList; // a list with our pcb clients ... for clean up
+    QList<Zera::Proxy::ProxyClientPtr> m_pcbClientList; // a list with our pcb clients ... for clean up
     QHash<QString, cAdjustChannelInfo*> m_adjustChannelInfoHash;
     QHash<int, QString> m_portChannelHash; // a list of ports for which we have established connection
     QList<Zera::Server::cPCBInterface*> m_pcbInterfaceList; // a list of pcbinterfaces ... for clean up
     QHash<QString, QString> m_AliasChannelHash; // we use this hash for easier access
+    QHash<QString,int> m_chnPortHash; // a hash for our channels ethernet ports
 };
 
 typedef std::shared_ptr<AdjustmentModuleActivateData> AdjustmentModuleActivateDataPtr;
---
+++ zera-modules/adjustmentmodule/src/tasks/taskchannelpcbconnectionsstart.h
@@ -0,0 +1,34 @@
+#ifndef TASKCHANNELPCBCONNECTIONSSTART_H
+#define TASKCHANNELPCBCONNECTIONSSTART_H
+
+#include <adjustmentmodulecommon.h>
+#include <taskparallel.h>
+#include <QStringList>
+
+namespace ADJUSTMENTMODULE {
+
+class TaskChannelPcbConnectionsStart : public TaskComposite
+{
+    Q_OBJECT
+public:
+    static std::unique_ptr<TaskComposite> create(
+            AdjustmentModuleActivateDataPtr activateData,
+            QStringList channels,
+            QString pcbServerIp,
+            int connectTimeout);
+    TaskChannelPcbConnectionsStart(
+            AdjustmentModuleActivateDataPtr activateData,
+            QStringList channels,
+            QString pcbServerIp,
+            int connectTimeout);
+    void start() override;
+private:
+    AdjustmentModuleActivateDataPtr m_activationData;
+    QStringList m_channels;
+    QString m_pcbServerIp;
+    int m_connectTimeout;
+    TaskParallel m_pcbActivationTasks;
+};
+
+}
+#endif // TASKCHANNELPCBCONNECTIONSSTART_H
