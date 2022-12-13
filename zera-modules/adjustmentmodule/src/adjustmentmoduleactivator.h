af1b091822a3f13854ea8bc74c158ff09f7c5bba
Andreas Müller <schnitzeltony@gmail.com>
2022-12-13 22:42:07 +0100

    AdjustmentModuleActivator: Tidy up a bit more
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/adjustmentmodule/src/adjustmentmoduleactivator.h
+++ zera-modules/adjustmentmodule/src/adjustmentmoduleactivator.h
@@ -37,6 +37,7 @@ private:
     TaskCompositePtr getChannelsReadTasks();
     TaskCompositePtr getChannelsRegisterNotifyTasks();
     TaskCompositePtr getDeactivationTasks();
+    void initChannelInfoHash();
     void fillChannelAliasHash();
 
     QStringList m_configuredChannels;
