ea037cd180c55fa4a81959671e09d431ef465993
Andreas Müller <schnitzeltony@gmail.com>
2022-12-14 23:52:45 +0100

    TaskChannelReadAlias: Prettify & simplify
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-misc/tasks-common/taskchannelreadalias.cpp
+++ zera-misc/tasks-common/taskchannelreadalias.cpp
@@ -2,24 +2,26 @@
 #include "tasktimeoutdecorator.h"
 #include "reply.h"
 
-TaskChannelReadAlias::TaskChannelReadAlias(Zera::Server::PcbInterfacePtr pcbInterface, QString channelName, QString &targetAlias) :
+TaskCompositePtr TaskChannelReadAlias::create(Zera::Server::PcbInterfacePtr pcbInterface,
+                                              QString channelName,
+                                              QString &valueReceived,
+                                              int timeout, std::function<void ()> additionalErrorHandler)
+{
+    return TaskTimeoutDecorator::wrapTimeout(timeout,
+                                             std::make_unique<TaskChannelReadAlias>(
+                                                 pcbInterface,
+                                                 channelName,
+                                                 valueReceived),
+                                             additionalErrorHandler);
+}
+
+TaskChannelReadAlias::TaskChannelReadAlias(Zera::Server::PcbInterfacePtr pcbInterface,
+                                           QString channelName,
+                                           QString &valueReceived) :
     m_pcbInterface(pcbInterface),
     m_channelName(channelName),
-    m_targetAlias(targetAlias)
+    m_valueReceived(valueReceived)
 {
-}
-
-std::unique_ptr<TaskComposite> TaskChannelReadAlias::create(Zera::Server::PcbInterfacePtr pcbInterface, QString channelName,
-                                                              QString &targetAlias)
-{
-    return std::make_unique<TaskChannelReadAlias>(pcbInterface, channelName, targetAlias);
-}
-
-std::unique_ptr<TaskComposite> TaskChannelReadAlias::create(Zera::Server::PcbInterfacePtr pcbInterface, QString channelName,
-                                                              QString &targetAlias,
-                                                              int timeout, std::function<void ()> additionalErrorHandler)
-{
-    return TaskTimeoutDecorator::wrapTimeout(timeout, create(pcbInterface, channelName, targetAlias), additionalErrorHandler);
 }
 
 void TaskChannelReadAlias::start()
@@ -32,7 +34,7 @@ void TaskChannelReadAlias::onRmAnswer(quint32 msgnr, quint8 reply, QVariant answ
 {
     if(m_msgnr == msgnr) {
         if (reply == ack)
-            m_targetAlias = answer.toString();
+            m_valueReceived = answer.toString();
         finishTask(reply == ack);
     }
 }
--- zera-misc/tasks-common/taskchannelreadalias.h
+++ zera-misc/tasks-common/taskchannelreadalias.h
@@ -8,19 +8,19 @@ class TaskChannelReadAlias : public TaskComposite
 {
     Q_OBJECT
 public:
-    static std::unique_ptr<TaskComposite> create(Zera::Server::PcbInterfacePtr pcbInterface, QString channelName,
-                                                 QString& targetAlias);
-    static std::unique_ptr<TaskComposite> create(Zera::Server::PcbInterfacePtr pcbInterface, QString channelName,
-                                                 QString& targetAlias,
-                                                 int timeout, std::function<void()> additionalErrorHandler = []{});
-    TaskChannelReadAlias(Zera::Server::PcbInterfacePtr pcbInterface, QString channelName, QString& targetAlias);
+    static TaskCompositePtr create(Zera::Server::PcbInterfacePtr pcbInterface, QString channelName,
+                                   QString& valueReceived,
+                                   int timeout, std::function<void()> additionalErrorHandler = []{});
+    TaskChannelReadAlias(Zera::Server::PcbInterfacePtr pcbInterface,
+                         QString channelName,
+                         QString& valueReceived);
     void start() override;
 private slots:
     void onRmAnswer(quint32 msgnr, quint8 reply, QVariant answer);
 private:
     Zera::Server::PcbInterfacePtr m_pcbInterface;
     QString m_channelName;
-    QString& m_targetAlias;
+    QString& m_valueReceived;
     quint32 m_msgnr;
 };
 
