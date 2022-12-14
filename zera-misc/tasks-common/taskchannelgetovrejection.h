f59150dfc16b9e08143173759b82c74b52a99a57
Andreas Müller <schnitzeltony@gmail.com>
2022-12-14 23:19:04 +0100

    Add TaskChannelGetOvRejection - not running yet
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-misc/tasks-common/taskchannelgetovrejection.h
@@ -0,0 +1,28 @@
+#ifndef TASKCHANNELGETOVREJECTION_H
+#define TASKCHANNELGETOVREJECTION_H
+
+#include "taskcomposit.h"
+#include "pcbinterface.h"
+
+class TaskChannelGetOvRejection : public TaskComposite
+{
+    Q_OBJECT
+public:
+    static std::unique_ptr<TaskComposite> create(Zera::Server::PcbInterfacePtr pcbInterface,
+                                                 QString channelSysName, QString rangeName,
+                                                 double &valueReceived,
+                                                 int timeout, std::function<void()> additionalErrorHandler = []{});
+    TaskChannelGetOvRejection(Zera::Server::PcbInterfacePtr pcbInterface,
+                              QString channelSysName, QString rangeName, double &valueReceived);
+    void start() override;
+private slots:
+    void onRmAnswer(quint32 msgnr, quint8 reply, QVariant answer);
+private:
+    Zera::Server::PcbInterfacePtr m_pcbInterface;
+    QString m_channelSysName;
+    QString m_rangeName;
+    double &m_valueReceived;
+    quint32 m_msgnr;
+};
+
+#endif // TASKCHANNELGETOVREJECTION_H
