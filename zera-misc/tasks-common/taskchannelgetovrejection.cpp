eb46614f1e56eea9675e37c99219f062ffbebc71
Andreas Müller <schnitzeltony@gmail.com>
2022-12-14 23:19:04 +0100

    Add TaskChannelGetOvRejection - not running yet
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-misc/tasks-common/taskchannelgetovrejection.cpp
@@ -0,0 +1,40 @@
+#include "taskchannelgetovrejection.h"
+#include "tasktimeoutdecorator.h"
+#include "reply.h"
+
+std::unique_ptr<TaskComposite> TaskChannelGetOvRejection::create(Zera::Server::PcbInterfacePtr pcbInterface,
+                                                                 QString channelSysName, QString rangeName,
+                                                                 double &valueReceived,
+                                                                 int timeout, std::function<void ()> additionalErrorHandler)
+{
+    return TaskTimeoutDecorator::wrapTimeout(timeout,
+                                             std::make_unique<TaskChannelGetOvRejection>(pcbInterface,
+                                                                                         channelSysName, rangeName,
+                                                                                         valueReceived),
+                                             additionalErrorHandler);
+}
+
+TaskChannelGetOvRejection::TaskChannelGetOvRejection(Zera::Server::PcbInterfacePtr pcbInterface,
+                                                     QString channelSysName, QString rangeName,
+                                                     double &valueReceived) :
+    m_pcbInterface(pcbInterface),
+    m_channelSysName(channelSysName),
+    m_rangeName(rangeName),
+    m_valueReceived(valueReceived)
+{
+}
+
+void TaskChannelGetOvRejection::start()
+{
+    connect(m_pcbInterface.get(), &Zera::Server::cPCBInterface::serverAnswer, this, &TaskChannelGetOvRejection::onRmAnswer);
+    m_msgnr = m_pcbInterface->getOVRejection(m_channelSysName, m_rangeName);
+}
+
+void TaskChannelGetOvRejection::onRmAnswer(quint32 msgnr, quint8 reply, QVariant answer)
+{
+    if(m_msgnr == msgnr) {
+        if (reply == ack)
+            m_valueReceived = answer.toDouble();
+        finishTask(reply == ack);
+    }
+}
