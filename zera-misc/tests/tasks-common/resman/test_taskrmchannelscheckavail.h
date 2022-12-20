98a0bc3bfd8dd9e0d3401a05d025fb5b9664dce1
Andreas Müller <schnitzeltony@gmail.com>
2022-12-20 23:28:32 +0100

    TaskRmChannelsCheckAvail: rework
    * reuse TaskRmChannelsGetAvail
    * test
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-misc/tests/tasks-common/resman/test_taskrmchannelscheckavail.h
@@ -0,0 +1,15 @@
+#ifndef TEST_TASKRMCHANNELSCHECKAVAIL_H
+#define TEST_TASKRMCHANNELSCHECKAVAIL_H
+
+#include <QObject>
+
+class test_taskrmchannelscheckavail : public QObject
+{
+    Q_OBJECT
+private slots:
+    void okOnExpectedEqualGet();
+    void okOnExpectedPartOfGet();
+    void errOnExpectedNotPartOfGet();
+};
+
+#endif // TEST_TASKRMCHANNELSCHECKAVAIL_H
