8dcc6fe496bb5db22624120c6dc6e9602c646d0d
Andreas Müller <schnitzeltony@gmail.com>
2022-12-21 00:38:52 +0100

    TaskRmSendIdent: Re-add proper timeout handling
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-misc/tasks-common/resman/taskrmsendident.cpp
+++ zera-misc/tasks-common/resman/taskrmsendident.cpp
@@ -1,20 +1,19 @@
 #include "taskrmsendident.h"
 #include "tasktimeoutdecorator.h"
-#include "reply.h"
 
 TaskCompositePtr TaskRmSendIdent::create(Zera::Server::RMInterfacePtr rmInterface,
                                          QString ident,
                                          int timeout, std::function<void ()> additionalErrorHandler)
 {
-    return std::make_unique<TaskRmSendIdent>(
-                rmInterface,
-                ident,
-                timeout, additionalErrorHandler);
+    return TaskTimeoutDecorator::wrapTimeout(timeout,
+                                             std::make_unique<TaskRmSendIdent>(
+                                                 rmInterface,
+                                                 ident),
+                                             additionalErrorHandler);
 }
 
 TaskRmSendIdent::TaskRmSendIdent(Zera::Server::RMInterfacePtr rmInterface,
-                                 QString ident,
-                                 int timeout, std::function<void ()> additionalErrorHandler) :
+                                 QString ident) :
     TaskServerTransactionTemplate(rmInterface),
     m_rmInterface(rmInterface),
     m_ident(ident)
