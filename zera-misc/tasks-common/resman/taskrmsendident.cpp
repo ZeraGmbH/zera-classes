847d595ce9d48b638fe27a870c623958dca936fc
Andreas Müller <schnitzeltony@gmail.com>
2022-12-21 00:03:37 +0100

    TaskRmSendIdent: Inherit TaskServerTransactionTemplate to simplify
    * This adds tcp-error handling
    * Let's consider TaskRmSendIdent tested by TaskServerTransactionTemplate
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-misc/tasks-common/resman/taskrmsendident.cpp
+++ zera-misc/tasks-common/resman/taskrmsendident.cpp
@@ -2,31 +2,31 @@
 #include "tasktimeoutdecorator.h"
 #include "reply.h"
 
-TaskCompositePtr TaskRmSendIdent::create(Zera::Server::RMInterfacePtr rmInterface, QString ident,
+TaskCompositePtr TaskRmSendIdent::create(Zera::Server::RMInterfacePtr rmInterface,
+                                         QString ident,
                                          int timeout, std::function<void ()> additionalErrorHandler)
 {
-    return TaskTimeoutDecorator::wrapTimeout(timeout,
-                                             std::make_unique<TaskRmSendIdent>(
-                                                 rmInterface,
-                                                 ident),
-                                             additionalErrorHandler);
+    return std::make_unique<TaskRmSendIdent>(
+                rmInterface,
+                ident,
+                timeout, additionalErrorHandler);
 }
 
-TaskRmSendIdent::TaskRmSendIdent(Zera::Server::RMInterfacePtr rmInterface, QString ident) :
+TaskRmSendIdent::TaskRmSendIdent(Zera::Server::RMInterfacePtr rmInterface,
+                                 QString ident,
+                                 int timeout, std::function<void ()> additionalErrorHandler) :
+    TaskServerTransactionTemplate(rmInterface),
     m_rmInterface(rmInterface),
     m_ident(ident)
 {
 }
 
-void TaskRmSendIdent::start()
+quint32 TaskRmSendIdent::sendToServer()
 {
-    connect(m_rmInterface.get(), &Zera::Server::cRMInterface::serverAnswer,
-            this, &TaskRmSendIdent::onServerAnswer);
-    m_msgnr = m_rmInterface->rmIdent(m_ident);
+    return m_rmInterface->rmIdent(m_ident);
 }
 
-void TaskRmSendIdent::onServerAnswer(quint32 msgnr, quint8 reply)
+bool TaskRmSendIdent::handleCheckedServerAnswer(QVariant answer)
 {
-    if(msgnr == m_msgnr)
-        finishTask(reply == ack);
+    return true;
 }
