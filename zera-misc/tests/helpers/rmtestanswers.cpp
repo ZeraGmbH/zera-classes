1030e915f3992fc015499ddf492df1dd175440f3
Andreas Müller <schnitzeltony@gmail.com>
2022-12-20 12:11:22 +0100

    Initial draft on rm-interface to run under test
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-misc/tests/helpers/rmtestanswers.cpp
+++ zera-misc/tests/helpers/rmtestanswers.cpp
@@ -1,13 +1,19 @@
 #include "rmtestanswers.h"
 
-RmTestAnswer::RmTestAnswer(quint8 reply, QVariant answer)
+RmTestAnswer::RmTestAnswer(quint8 reply, QVariant answer, MsgIdType msgIdType)
 {
     this->reply = reply;
     this->answer = answer;
+    this->msgIdType = msgIdType;
 }
 
 RmTestAnswers::RmTestAnswers(QList<RmTestAnswer> answers) :
     m_answers(answers)
 {
+}
+
+RmTestAnswer RmTestAnswers::take()
+{
+    return m_answers.takeFirst();
 }
 
