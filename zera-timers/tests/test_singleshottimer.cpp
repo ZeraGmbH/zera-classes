e44e7aedd16ee065d7756af6ae747a2ab000ab52
Andreas Müller <schnitzeltony@gmail.com>
2022-12-26 17:01:30 +0100

    zera-timers: More test enhancements
    * Increase delays and their limits
    * Add tests for infinite expire
    
    Yes it's time to get rid of delay measurements at other places than zera-timers
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-timers/tests/test_singleshottimer.cpp
+++ zera-timers/tests/test_singleshottimer.cpp
@@ -1,10 +1,21 @@
 #include "test_singleshottimer.h"
 #include "singleshottimerqt.h"
+#include "timertestdefaults.h"
 #include <QTest>
 
 QTEST_MAIN(test_singleshottimer)
 
-constexpr int defaultExpireMs = 10;
+static bool isExpireTimeWithinLimits(int measuredTime, int expectedTime)
+{
+    int minAllowed = expectedTime;
+    int maxAllowed = expectedTime * 1.2;
+    bool ok = measuredTime>=minAllowed && measuredTime<=maxAllowed;
+    if(!ok) {
+        qWarning("Measured: %i / Expected: %i", measuredTime, expectedTime);
+        qWarning("Min allowed: %i / Max allowed: %i", minAllowed, maxAllowed);
+    }
+    return ok;
+}
 
 void test_singleshottimer::init()
 {
@@ -34,68 +45,68 @@ void test_singleshottimer::inspectTimerByRunner(SingleShotTimerTest *timer)
 
 void test_singleshottimer::signalOnExpireTiming()
 {
-    SingleShotTimerQt timer(defaultExpireMs);
+    SingleShotTimerQt timer(DEFAULT_EXPIRE);
     timer.setHighAccuracy(true);
     inspectTimerByDelay(&timer);
 
     timer.start();
-    QTest::qWait(defaultExpireMs+1);
+    QTest::qWait(DEFAULT_EXPIRE_WAIT);
 
     QCOMPARE(m_expireCount, 1);
-    QCOMPARE(m_expireTime, defaultExpireMs);
+    QVERIFY(isExpireTimeWithinLimits(m_expireTime, DEFAULT_EXPIRE)); // fuzzy
 }
 
 void test_singleshottimer::signalOnExpireTimingTest()
 {
-    SingleShotTimerTest timer(defaultExpireMs);
+    SingleShotTimerTest timer(DEFAULT_EXPIRE);
     inspectTimerByRunner(&timer);
 
     timer.start();
-    m_elapsedTestTimerRunner->processTimers(defaultExpireMs+1);
+    m_elapsedTestTimerRunner->processTimers(DEFAULT_EXPIRE_WAIT);
 
     QCOMPARE(m_expireCount, 1);
-    QCOMPARE(m_expireTime, defaultExpireMs);
+    QCOMPARE(m_expireTime, DEFAULT_EXPIRE); // on point
 }
 
 void test_singleshottimer::restartTiming()
 {
-    SingleShotTimerQt timer(defaultExpireMs);
+    SingleShotTimerQt timer(DEFAULT_EXPIRE);
     timer.setHighAccuracy(true);
     inspectTimerByDelay(&timer);
 
     timer.start();
-    QTest::qWait(defaultExpireMs/2);
+    QTest::qWait(DEFAULT_EXPIRE/2);
     timer.start();
-    QTest::qWait(defaultExpireMs+1);
+    QTest::qWait(DEFAULT_EXPIRE_WAIT);
 
     QCOMPARE(m_expireCount, 1);
-    QCOMPARE(m_expireTime, defaultExpireMs*1.5);
+    QVERIFY(isExpireTimeWithinLimits(m_expireTime, DEFAULT_EXPIRE*1.5)); // fuzzy
 }
 
 void test_singleshottimer::restartTimingTest()
 {
-    SingleShotTimerTest timer(defaultExpireMs);
+    SingleShotTimerTest timer(DEFAULT_EXPIRE);
     inspectTimerByRunner(&timer);
 
     timer.start();
-    m_elapsedTestTimerRunner->processTimers(defaultExpireMs/2);
+    m_elapsedTestTimerRunner->processTimers(DEFAULT_EXPIRE/2);
     timer.start();
-    m_elapsedTestTimerRunner->processTimers(defaultExpireMs+1);
+    m_elapsedTestTimerRunner->processTimers(DEFAULT_EXPIRE_WAIT);
 
     QCOMPARE(m_expireCount, 1);
-    QCOMPARE(m_expireTime, defaultExpireMs*1.5);
+    QCOMPARE(m_expireTime, DEFAULT_EXPIRE*1.5); // on point
 }
 
 void test_singleshottimer::stopWhilePending()
 {
-    SingleShotTimerQt timer(defaultExpireMs);
+    SingleShotTimerQt timer(DEFAULT_EXPIRE);
     timer.setHighAccuracy(true);
     inspectTimerByDelay(&timer);
 
     timer.start();
-    QTest::qWait(defaultExpireMs/2);
+    QTest::qWait(DEFAULT_EXPIRE/2);
     timer.stop();
-    QTest::qWait(defaultExpireMs+1);
+    QTest::qWait(DEFAULT_EXPIRE_WAIT);
 
     QCOMPARE(m_expireCount, 0);
     QCOMPARE(m_expireTime, 0);
@@ -103,13 +114,13 @@ void test_singleshottimer::stopWhilePending()
 
 void test_singleshottimer::stopWhilePendingTest()
 {
-    SingleShotTimerTest timer(defaultExpireMs);
+    SingleShotTimerTest timer(DEFAULT_EXPIRE);
     inspectTimerByRunner(&timer);
 
     timer.start();
-    m_elapsedTestTimerRunner->processTimers(defaultExpireMs/2);
+    m_elapsedTestTimerRunner->processTimers(DEFAULT_EXPIRE/2);
     timer.stop();
-    m_elapsedTestTimerRunner->processTimers(defaultExpireMs+1);
+    m_elapsedTestTimerRunner->processTimers(DEFAULT_EXPIRE_WAIT);
 
     QCOMPARE(m_expireCount, 0);
     QCOMPARE(m_expireTime, 0);
@@ -117,7 +128,7 @@ void test_singleshottimer::stopWhilePendingTest()
 
 void test_singleshottimer::queuedConnectetionsOnExpire()
 {
-    SingleShotTimerQt timer(defaultExpireMs);
+    SingleShotTimerQt timer(DEFAULT_EXPIRE);
     timer.setHighAccuracy(true);
     inspectTimerByDelay(&timer);
     TestEventLoop evTest(&timer);
@@ -127,14 +138,14 @@ void test_singleshottimer::queuedConnectetionsOnExpire()
     });
 
     timer.start();
-    QTest::qWait(defaultExpireMs+1);
+    QTest::qWait(DEFAULT_EXPIRE_WAIT);
 
     QCOMPARE(expireReceived, 1);
 }
 
 void test_singleshottimer::queuedConnectetionsOnExpireTest()
 {
-    SingleShotTimerTest timer(defaultExpireMs);
+    SingleShotTimerTest timer(DEFAULT_EXPIRE);
     inspectTimerByRunner(&timer);
     TestEventLoop evTest(&timer);
     int expireReceived = 0;
@@ -143,7 +154,32 @@ void test_singleshottimer::queuedConnectetionsOnExpireTest()
     });
 
     timer.start();
-    m_elapsedTestTimerRunner->processTimers(defaultExpireMs+1);
+    m_elapsedTestTimerRunner->processTimers(DEFAULT_EXPIRE_WAIT);
 
     QCOMPARE(expireReceived, 1);
+}
+
+void test_singleshottimer::infiniteExpire()
+{
+    SingleShotTimerQt timer(EXPIRE_INFINITE);
+    timer.setHighAccuracy(true);
+    inspectTimerByDelay(&timer);
+
+    timer.start();
+    QTest::qWait(DEFAULT_EXPIRE_WAIT);
+
+    QCOMPARE(m_expireCount, 0);
+    QCOMPARE(m_expireTime, 0);
+}
+
+void test_singleshottimer::infiniteExpireTest()
+{
+    SingleShotTimerTest timer(EXPIRE_INFINITE);
+    inspectTimerByRunner(&timer);
+
+    timer.start();
+    m_elapsedTestTimerRunner->processTimers(DEFAULT_EXPIRE_WAIT);
+
+    QCOMPARE(m_expireCount, 0);
+    QCOMPARE(m_expireTime, 0);
 }
