2569fc753fc671742bf70dc2f863514411a5a54c
Andreas Müller <schnitzeltony@gmail.com>
2022-12-27 22:39:51 +0100

    Remove TaskTestHelper
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-tasks/test-helpers/lib/tasktesthelper.cpp
+++ zera-tasks/test-helpers/lib/tasktesthelper.cpp
@@ -1,22 +1,6 @@
 #include "tasktesthelper.h"
 #include <timerrunnerfortest.h>
 
-TaskTestHelper::TaskTestHelper(TaskComposite *taskUnderTest)
-{
-    connect(taskUnderTest, &TaskComposite::sigFinish, this, &TaskTestHelper::onTaskFinish);
-    m_elapsedTimer.start();
-}
-
-void TaskTestHelper::onTaskFinish(bool ok, int taskId)
-{
-    if(ok)
-        m_okCount++;
-    else
-        m_errCount++;
-    m_signalDelayMs = m_elapsedTimer.elapsed();
-    m_taskIdsReceived.append(taskId);
-}
-
 TaskTestHelperNew::TaskTestHelperNew(TaskComposite *taskUnderTest)
 {
     connect(taskUnderTest, &TaskComposite::sigFinish, this, &TaskTestHelperNew::onTaskFinish);
--- zera-tasks/test-helpers/lib/tasktesthelper.h
+++ zera-tasks/test-helpers/lib/tasktesthelper.h
@@ -7,25 +7,6 @@
 #include <QElapsedTimer>
 #include <QList>
 
-class TaskTestHelper : public QObject
-{
-    Q_OBJECT
-public:
-    explicit TaskTestHelper(TaskComposite *taskUnderTest);
-    int okCount() const { return m_okCount; }
-    int errCount()  const { return m_errCount; }
-    int signalDelayMs() const { return m_signalDelayMs; }
-    int lastTaskIdReceived() const { return m_taskIdsReceived.last(); }
-private slots:
-    void onTaskFinish(bool ok, int taskId);
-private:
-    int m_okCount = 0;
-    int m_errCount = 0;
-    int m_signalDelayMs = 0;
-    QElapsedTimer m_elapsedTimer;
-    QList<int> m_taskIdsReceived;
-};
-
 class TaskTestHelperNew : public QObject
 {
     Q_OBJECT
