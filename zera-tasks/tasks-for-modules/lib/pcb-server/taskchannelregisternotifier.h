dde076fd933b83bbd95f9dd763317771a54b0951
Andreas Müller <schnitzeltony@gmail.com>
2022-12-29 23:49:56 +0100

    TaskChannelRegisterNotifier: Add tests and use TaskServerTransactionTemplate
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-tasks/tasks-for-modules/lib/pcb-server/taskchannelregisternotifier.h
+++ zera-tasks/tasks-for-modules/lib/pcb-server/taskchannelregisternotifier.h
@@ -1,10 +1,10 @@
 #ifndef TASKCHANNELREGISTERNOTIFIER_H
 #define TASKCHANNELREGISTERNOTIFIER_H
 
-#include "taskcomposit.h"
+#include "taskservertransactiontemplate.h"
 #include <pcbinterface.h>
 
-class TaskChannelRegisterNotifier : public TaskComposite
+class TaskChannelRegisterNotifier : public TaskServerTransactionTemplate
 {
     Q_OBJECT
 public:
@@ -12,10 +12,9 @@ public:
                                    QString channelName,
                                    int timeout, std::function<void()> additionalErrorHandler = []{});
     TaskChannelRegisterNotifier(Zera::Server::PcbInterfacePtr pcbInterface, QString channelName);
-    void start() override;
-private slots:
-    void onServerAnswer(quint32 msgnr, quint8 reply, QVariant answer);
 private:
+    quint32 sendToServer() override;
+    bool handleCheckedServerAnswer(QVariant answer) override;
     Zera::Server::PcbInterfacePtr m_pcbInterface;
     QString m_channelName;
     quint32 m_msgnr;
