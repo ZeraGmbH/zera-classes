e7397bce0e005c63f2b93e6b0d974e715cc8c3ce
Andreas Müller <schnitzeltony@gmail.com>
2022-12-29 16:06:26 +0100

    TaskChannelGetOvRejection: Add tests and use TaskServerTransactionTemplate
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-tasks/tasks-for-modules/lib/pcb-server/taskchannelgetovrejection.cpp
+++ zera-tasks/tasks-for-modules/lib/pcb-server/taskchannelgetovrejection.cpp
@@ -18,24 +18,20 @@ TaskCompositePtr TaskChannelGetOvRejection::create(Zera::Server::PcbInterfacePtr
 TaskChannelGetOvRejection::TaskChannelGetOvRejection(Zera::Server::PcbInterfacePtr pcbInterface,
                                                      QString channelSysName, QString rangeName,
                                                      double &valueReceived) :
+    TaskServerTransactionTemplate(pcbInterface),
     m_pcbInterface(pcbInterface),
     m_channelSysName(channelSysName), m_rangeName(rangeName),
     m_valueReceived(valueReceived)
 {
 }
 
-void TaskChannelGetOvRejection::start()
+quint32 TaskChannelGetOvRejection::sendToServer()
 {
-    connect(m_pcbInterface.get(), &Zera::Server::cPCBInterface::serverAnswer,
-            this, &TaskChannelGetOvRejection::onServerAnswer);
-    m_msgnr = m_pcbInterface->getOVRejection(m_channelSysName, m_rangeName);
+    return m_pcbInterface->getOVRejection(m_channelSysName, m_rangeName);
 }
 
-void TaskChannelGetOvRejection::onServerAnswer(quint32 msgnr, quint8 reply, QVariant answer)
+bool TaskChannelGetOvRejection::handleCheckedServerAnswer(QVariant answer)
 {
-    if(m_msgnr == msgnr) {
-        if (reply == ack)
-            m_valueReceived = answer.toDouble();
-        finishTask(reply == ack);
-    }
+    m_valueReceived = answer.toDouble();
+    return true;
 }
