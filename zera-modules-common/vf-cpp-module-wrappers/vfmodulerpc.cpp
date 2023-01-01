b1126ab45ec7096dbae20830cd3dcb6b280d8dd2
Andreas Müller <schnitzeltony@gmail.com>
2023-01-01 23:55:24 +0100

    Introduce library vf-cpp-module-wrappers
    * start to get rid of adding vf-cpp dependency on all modules
    * rpc calling is currently in ModuleValidator which is not the first place to
      search for
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules-common/vf-cpp-module-wrappers/vfmodulerpc.cpp
@@ -0,0 +1,43 @@
+#include "vfmodulerpc.h"
+
+VfModuleRpc::VfModuleRpc(int entityId) :
+    VeinCommandFilterEventSystem(VeinEvent::CommandEvent::EventSubtype::TRANSACTION),
+    m_entityId(entityId)
+{
+}
+
+void VfModuleRpc::processCommandEvent(VeinEvent::CommandEvent *t_cEvent)
+{
+    if(t_cEvent->eventData()->entityId() == m_entityId &&
+            t_cEvent->eventData()->type() == VeinComponent::RemoteProcedureData::dataType()) {
+        VeinComponent::RemoteProcedureData *rpcData = static_cast<VeinComponent::RemoteProcedureData *>(t_cEvent->eventData());
+        if(rpcData->command() == VeinComponent::RemoteProcedureData::Command::RPCMD_CALL) {
+            if(m_rpcHash.contains(rpcData->procedureName())) {
+                const QUuid callId = rpcData->invokationData().value(VeinComponent::RemoteProcedureData::s_callIdString).toUuid();
+                Q_ASSERT(!callId.isNull());
+                m_rpcHash[rpcData->procedureName()]->callFunction(callId,
+                                                                  t_cEvent->peerId(),
+                                                                  rpcData->invokationData());
+                t_cEvent->accept();
+            }
+            else {
+                VF_ASSERT(false, QStringC(QString("No remote procedure with entityId: %1 name: %2").arg(m_entityId).arg(rpcData->procedureName())));
+                VeinComponent::ErrorData *eData = new VeinComponent::ErrorData();
+                eData->setEntityId(m_entityId);
+                eData->setErrorDescription(QString("No remote procedure with name: %1").arg(rpcData->procedureName()));
+                eData->setOriginalData(rpcData);
+                eData->setEventOrigin(VeinEvent::EventData::EventOrigin::EO_LOCAL);
+                eData->setEventTarget(VeinEvent::EventData::EventTarget::ET_ALL);
+                VeinEvent::CommandEvent *errorEvent = new VeinEvent::CommandEvent(VeinEvent::CommandEvent::EventSubtype::NOTIFICATION, eData);
+                errorEvent->setPeerId(t_cEvent->peerId());
+                t_cEvent->accept();
+                emit sigSendEvent(errorEvent);
+            }
+        }
+    }
+}
+
+void VfModuleRpc::addRpc(VfCpp::cVeinModuleRpc::Ptr rpc)
+{
+    m_rpcHash[rpc->rpcName()] = rpc;
+}
---
+++ zera-modules-common/vf-cpp-module-wrappers/vfmodulerpc.h
@@ -0,0 +1,20 @@
+#ifndef VFMODULERPC_H
+#define VFMODULERPC_H
+
+#include <veincommandfiltereventsystem.h>
+#include <vf-cpp-rpc.h>
+#include <QHash>
+
+class VfModuleRpc : public VeinCommandFilterEventSystem
+{
+    Q_OBJECT
+public:
+    VfModuleRpc(int entityId);
+    virtual void processCommandEvent(VeinEvent::CommandEvent *t_cEvent);
+    void addRpc(VfCpp::cVeinModuleRpc::Ptr rpc);
+private:
+    int m_entityId;
+    QHash<QString, VfCpp::cVeinModuleRpc::Ptr> m_rpcHash;
+};
+
+#endif // VFMODULERPC_H
