a54f76e7840053cbd9ae592592b0a23fb492d1ad
Andreas Müller <schnitzeltony@gmail.com>
2023-01-01 21:22:08 +0100

    Make cBaseModuleEventSystem a VeinCommandFilterEventSystem to simpify code
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules-common/zera-basemodule/basemoduleeventsystem.cpp
+++ zera-modules-common/zera-basemodule/basemoduleeventsystem.cpp
@@ -1,60 +1,31 @@
 #include "basemoduleeventsystem.h"
 #include "veinmodulecomponent.h"
 
-bool cBaseModuleEventSystem::processEvent(QEvent *t_event)
+cBaseModuleEventSystem::cBaseModuleEventSystem() :
+    VeinCommandFilterEventSystem(VeinEvent::CommandEvent::EventSubtype::NOTIFICATION)
 {
-    bool retVal = false;
-
-    if(t_event->type() == VeinEvent::CommandEvent::eventType())
-    {
-        VeinEvent::CommandEvent *cEvent = 0;
-        cEvent = static_cast<VeinEvent::CommandEvent *>(t_event);
-        if(cEvent != 0)
-        {
-            switch(cEvent->eventSubtype())
-            {
-            case VeinEvent::CommandEvent::EventSubtype::NOTIFICATION:
-                retVal = true;
-                processCommandEvent(cEvent);
-                break;
-              default:
-                break;
-            }
-        }
-    }
-    return retVal;
 }
-
 
 void cBaseModuleEventSystem::setInputList(QList<cVeinModuleComponentInput *> &inputlist)
 {
     m_InputList = inputlist;
 }
 
-
-void cBaseModuleEventSystem::processCommandEvent(VeinEvent::CommandEvent *t_cEvent)
+void cBaseModuleEventSystem::processCommandEvent(VeinEvent::CommandEvent *commandEvent)
 {
     // is it a command event for setting component data
-    if (t_cEvent->eventData()->type() == VeinComponent::ComponentData::dataType()) {
-        // only notifications will be handled
-        if (t_cEvent->eventSubtype() == VeinEvent::CommandEvent::EventSubtype::NOTIFICATION) {
-            QString cName;
-            int entityId;
-            VeinComponent::ComponentData* cData = static_cast<VeinComponent::ComponentData*> (t_cEvent->eventData());
-            cName = cData->componentName();
-            entityId = cData->entityId();
-
-            // now let's look if we need this input
-            for (int i = 0; i < m_InputList.count(); i++) {
-                cVeinModuleComponentInput *vmci = m_InputList.at(i);
-                if ( (vmci->m_sName == cName) && (vmci->m_nEntityId == entityId) ) {
-                    vmci->setValue(cData->newValue());
-                    emit vmci->sigValueChanged(cData->newValue());
-                    // break; // we don't break here -> so a module can use an input several times
-                }
+    if (commandEvent->eventData()->type() == VeinComponent::ComponentData::dataType()) {
+        VeinComponent::ComponentData* componentData = static_cast<VeinComponent::ComponentData*> (commandEvent->eventData());
+        QString componentName = componentData->componentName();
+        int entityId = componentData->entityId();
+        // now let's look if we need this input
+        for (int i = 0; i < m_InputList.count(); i++) {
+            cVeinModuleComponentInput *vmci = m_InputList.at(i);
+            if ( (vmci->m_sName == componentName) && (vmci->m_nEntityId == entityId) ) {
+                vmci->setValue(componentData->newValue());
+                emit vmci->sigValueChanged(componentData->newValue());
+                // break; // we don't break here -> so a module can use an input several times
             }
         }
     }
 }
-
-
