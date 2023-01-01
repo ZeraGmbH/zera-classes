eb83ff59c69cbfcf74ad84fab861dee7e140b918
Andreas Müller <schnitzeltony@gmail.com>
2023-01-01 21:14:43 +0100

    zera-vein-moduleinterface: Introduce VeinCommandFilterEventSystem
    We have seen these few lines of code so often - so make a class out of it
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules-common/zera-vein-moduleinterface/veincommandfiltereventsystem.cpp
@@ -0,0 +1,19 @@
+#include "veincommandfiltereventsystem.h"
+
+VeinCommandFilterEventSystem::VeinCommandFilterEventSystem(VeinEvent::CommandEvent::EventSubtype subtypeToFilter) :
+    m_subtypeToFilter(subtypeToFilter)
+{
+}
+
+bool VeinCommandFilterEventSystem::processEvent(QEvent *event)
+{
+    bool retVal = false;
+    if(event->type() == VeinEvent::CommandEvent::eventType()) {
+        VeinEvent::CommandEvent *commandEvent = static_cast<VeinEvent::CommandEvent *>(event);
+        if(commandEvent && commandEvent->eventSubtype() == m_subtypeToFilter) {
+            retVal = true;
+            processCommandEvent(commandEvent);
+        }
+    }
+    return retVal;
+}
