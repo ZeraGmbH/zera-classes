2402ca9bbf1a22db9339e633f20916f4ccf96b15
Andreas Müller <schnitzeltony@gmail.com>
2023-01-02 01:46:10 +0100

    modulevalidator: Do not check for transactions twice
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules-common/zera-validator/modulevalidator.cpp
+++ zera-modules-common/zera-validator/modulevalidator.cpp
@@ -15,19 +15,16 @@ void ModuleValidator::processCommandEvent(VeinEvent::CommandEvent *t_cEvent)
     if (t_cEvent->eventData()->entityId() == m_entityId) {
         // is it a command event for setting component data
         if (t_cEvent->eventData()->type() == VeinComponent::ComponentData::dataType()) {
-            // only transactions will be handled
             VeinComponent::ComponentData* cData = static_cast<VeinComponent::ComponentData*> (t_cEvent->eventData());
-            if (t_cEvent->eventSubtype() == VeinEvent::CommandEvent::EventSubtype::TRANSACTION) {
-                QString cName = cData->componentName();
-                // does this component data belong to our module
-                auto hashIter = m_Parameter2ValidateHash.find(cName);
-                if(hashIter != m_Parameter2ValidateHash.end()) {
-                    // we only take new values if the old values are equal
-                    if (cData->oldValue() == m_storageSystem->getStoredValue(m_entityId, cName)) {
-                        cVeinModuleParameter *param = hashIter.value();
-                        param->transaction(t_cEvent->peerId(), cData->newValue(), cData->oldValue(), cData->eventCommand());
-                        t_cEvent->accept(); // it is an event for us ... the parameter will do the rest
-                    }
+            QString cName = cData->componentName();
+            // does this component data belong to our module
+            auto hashIter = m_Parameter2ValidateHash.find(cName);
+            if(hashIter != m_Parameter2ValidateHash.end()) {
+                // we only take new values if the old values are equal
+                if (cData->oldValue() == m_storageSystem->getStoredValue(m_entityId, cName)) {
+                    cVeinModuleParameter *param = hashIter.value();
+                    param->transaction(t_cEvent->peerId(), cData->newValue(), cData->oldValue(), cData->eventCommand());
+                    t_cEvent->accept(); // it is an event for us ... the parameter will do the rest
                 }
             }
         }
