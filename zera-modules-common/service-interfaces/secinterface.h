13c4c9bcaf7467fa9287f1725fc46b6e29b79327
Andreas Müller <schnitzeltony@gmail.com>
2023-01-03 13:00:59 +0100

    CMake: Simplify significantly
    Create one target export only to keep CMake-files as simple as possible here
    and for our consumers.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules-common/service-interfaces/secinterface.h
+++ zera-modules-common/service-interfaces/secinterface.h
@@ -1,7 +1,7 @@
 #ifndef SECINTERFACE_H
 #define SECINTERFACE_H
 
-#include "zera-service-interfaces_export.h"
+#include "service-interfaces_export.h"
 #include <proxyclient.h>
 #include <QObject>
 #include <QAbstractSocket>
@@ -47,7 +47,7 @@ namespace Server
 {
 class cSECInterfacePrivate;
 
-class ZERA_SERVICE_INTERFACES_EXPORT cSECInterface: public QObject
+class SERVICE_INTERFACES_EXPORT cSECInterface: public QObject
 {
     Q_OBJECT
 public:
