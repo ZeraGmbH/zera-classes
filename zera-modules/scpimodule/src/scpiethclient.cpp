115895350aede9a3e8e8952d0d46201d16b5021a
Andreas Müller <schnitzeltony@gmail.com>
2023-01-12 11:49:31 +0100

    scpimodule: Add log messages for SCPI socket connection/disconnection
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/scpimodule/src/scpiethclient.cpp
+++ zera-modules/scpimodule/src/scpiethclient.cpp
@@ -1,8 +1,8 @@
-#include <QTcpSocket>
-
 #include "scpiinterface.h"
 #include "ieee488-2.h"
 #include "scpiethclient.h"
+#include <QTcpSocket>
+#include <QHostAddress>
 
 namespace SCPIMODULE
 
@@ -11,6 +11,8 @@ namespace SCPIMODULE
 cSCPIEthClient::cSCPIEthClient(QTcpSocket *socket, cSCPIModule *module, cSCPIModuleConfigData &configdata, cSCPIInterface *iface)
     :m_pSocket(socket), cSCPIClient(module, configdata, iface)
 {
+    m_peerAddress = getPeerAddress();
+    qInfo("Incoming SCPI socket from %s", qPrintable(m_peerAddress));
     // so now we can start our connection
     connect(m_pSocket, &QTcpSocket::readyRead, this, &cSCPIEthClient::cmdInput);
     connect(m_pSocket, &QTcpSocket::disconnected, this, &cSCPIEthClient::deleteLater);
@@ -19,6 +21,7 @@ cSCPIEthClient::cSCPIEthClient(QTcpSocket *socket, cSCPIModule *module, cSCPIMod
 
 cSCPIEthClient::~cSCPIEthClient()
 {
+    qInfo("SCPI socket %s closed", qPrintable(m_peerAddress));
     m_pSocket->abort();
     delete m_pSocket;
 }
@@ -31,6 +34,12 @@ void cSCPIEthClient::receiveAnswer(QString answ)
     answer = answ +"\n";
     ba = answer.toLatin1();
     m_pSocket->write(ba);
+}
+
+QString cSCPIEthClient::getPeerAddress()
+{
+    QHostAddress addr = m_pSocket->peerAddress();
+    return addr.toString();
 }
 
 
