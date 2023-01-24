7996c9e091bea7df737414d0f64ec6d581cc9870
Anuja Mutalik <a.mutalik@zera.de>
2023-01-24 15:10:15 +0100

    Introduce PeriodicTimerTest
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

---
+++ zera-timers/tests/test_periodictimerqt.h
@@ -0,0 +1,25 @@
+#ifndef TEST_PERIODICTIMERQT_H
+#define TEST_PERIODICTIMERQT_H
+
+#include "zeratimertemplate.h"
+#include <QElapsedTimer>
+#include <QList>
+#include <memory>
+
+class test_periodictimerqt : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init();
+
+    void oneIntervalTest();
+    void threeIntervalTest();
+
+private:
+    void inspectTimerByDelay(ZeraTimerTemplate* timer);
+    void inspectTimerByRunner(ZeraTimerTemplate *timer);
+    QList<int> m_expireTimes;
+    std::unique_ptr<QElapsedTimer> m_elapsedTimer;
+};
+
+#endif // TEST_PERIODICTIMERQT_H
