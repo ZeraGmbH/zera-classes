1110f530d899143d5ba83614afe9f22aa981581b
Andreas Müller <schnitzeltony@gmail.com>
2023-03-06 00:10:41 +0100

    power1/2moduleconfigdata: Try to document nastiness better
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/power1module/lib/power1moduleconfigdata.h
+++ zera-modules/power1module/lib/power1moduleconfigdata.h
@@ -61,8 +61,8 @@ public:
 
     quint8 m_nMeasModeCount; // how many measurement modes do we support
     QStringList m_sMeasmodeList; // a list of our measurement modes
-    QStringList m_sMeasSystemList; // our measuring systems "m0,m1" ....
-    int m_measSystemCount = 0; // count of measSysstem is 3 always!!
+    QStringList m_sMeasSystemList; // our measuring systems "m0,m1" ALWAYS 3 ENTRIES!!!
+    int m_measSystemCount = 0; // count of measSysstem
     QString m_sM2WSystem; // the system on which 2wire measuring mode shall work on
     QString m_sIntegrationMode; // we integrate over time or periods
     quint32 m_nNominalFrequency; // our nominal frequency output for full range power
