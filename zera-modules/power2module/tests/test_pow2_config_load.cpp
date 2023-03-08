20c6bee362277752df8964d6fd629ec095fcce8a
Andreas Müller <schnitzeltony@gmail.com>
2023-03-08 17:24:30 +0100

    power2module: Give up ultra confusing m_sMeasSystemList fixed size 3
    Whole configuration system relies on XML read in certain sequence so why this
    quirk?
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/power2module/tests/test_pow2_config_load.cpp
+++ zera-modules/power2module/tests/test_pow2_config_load.cpp
@@ -12,13 +12,15 @@ void test_pow2_config_load::fileFound()
     QVERIFY(tmpXmlConfigFile.exists());
 }
 
-void test_pow2_config_load::fileLoaded()
+void test_pow2_config_load::allFilesLoaded()
 {
-    QFile tmpXmlConfigFile(QStringLiteral(CONFIG_PATH) + "/" + "com5003-power2moduleCED.xml");
-    QVERIFY(tmpXmlConfigFile.open(QIODevice::Unbuffered | QIODevice::ReadOnly));
-    std::unique_ptr<POWER2MODULE::cPower2ModuleConfiguration> conf = std::make_unique<POWER2MODULE::cPower2ModuleConfiguration>();
-
-    QSignalSpy spy(conf.get(), &cBaseModuleConfiguration::configXMLDone);
-    conf->setConfiguration(tmpXmlConfigFile.readAll());
-    QCOMPARE(spy.count(), 1);
+    QFileInfoList fileList = QDir(QStringLiteral(CONFIG_PATH)).entryInfoList(QStringList() << "*.xml");
+    for(const auto &fileInfo : fileList) {
+        QFile configFile(fileInfo.absoluteFilePath());
+        qInfo("Load %s...", qPrintable(configFile.fileName()));
+        QVERIFY(configFile.open(QIODevice::Unbuffered | QIODevice::ReadOnly));
+        POWER2MODULE::cPower2ModuleConfiguration conf;
+        conf.setConfiguration(configFile.readAll());
+        QVERIFY(conf.isConfigured());
+    }
 }
