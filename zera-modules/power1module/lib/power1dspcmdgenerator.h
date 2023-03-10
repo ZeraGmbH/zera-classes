02c03fc7dc531450d7731e1e621be2d3d2eea9d4
Andreas Müller <schnitzeltony@gmail.com>
2023-03-10 19:05:08 +0100

    power1module: Remove now obsolete 4LW & 2LW chain
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/power1module/lib/power1dspcmdgenerator.h
+++ zera-modules/power1module/lib/power1dspcmdgenerator.h
@@ -8,14 +8,12 @@
 class Power1DspCmdGenerator
 {
 public:
-    static QStringList getCmdsMMode4LW(int dspSelectCode, MeasSystemChannels measChannelPairList);
     static QStringList getCmdsMMode4LB(int dspSelectCode, MeasSystemChannels measChannelPairList);
     static QStringList getCmdsMMode4LBK(int dspSelectCode, MeasSystemChannels measChannelPairList);
     static QStringList getCmdsMMode4LS(int dspSelectCode, MeasSystemChannels measChannelPairList);
     static QStringList getCmdsMMode4LSg(int dspSelectCode, MeasSystemChannels measChannelPairList);
     static QStringList getCmdsMMode3LW(int dspSelectCode, MeasSystemChannels measChannelPairList);
     static QStringList getCmdsMMode3LB(int dspSelectCode, MeasSystemChannels measChannelPairList);
-    static QStringList getCmdsMMode2LW(int dspSelectCode, MeasSystemChannels measChannelPairList, int idx2WireMeasSystem);
     static QStringList getCmdsMMode2LB(int dspSelectCode, MeasSystemChannels measChannelPairList, int idx2WireMeasSystem);
     static QStringList getCmdsMMode2LS(int dspSelectCode, MeasSystemChannels measChannelPairList, int idx2WireMeasSystem);
     static QStringList getCmdsMMode2LSg(int dspSelectCode, MeasSystemChannels measChannelPairList, int idx2WireMeasSystem);
