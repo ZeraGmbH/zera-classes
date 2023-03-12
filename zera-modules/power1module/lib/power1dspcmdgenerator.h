7c844762cd1fdc04bc53136165aeb65a2ce9e112
Andreas Müller <schnitzeltony@gmail.com>
2023-03-12 15:48:11 +0100

    power1module: Introduce XLSg
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/power1module/lib/power1dspcmdgenerator.h
+++ zera-modules/power1module/lib/power1dspcmdgenerator.h
@@ -21,6 +21,7 @@ public:
     static QStringList getCmdsMModeXLW(int dspSelectCode, MeasSystemChannels measChannelPairList);
     static QStringList getCmdsMModeXLB(int dspSelectCode, MeasSystemChannels measChannelPairList);
     static QStringList getCmdsMModeXLS(int dspSelectCode, MeasSystemChannels measChannelPairList);
+    static QStringList getCmdsMModeXLSg(int dspSelectCode, MeasSystemChannels measChannelPairList);
     static QStringList getCmdsSumAndAverage();
     static QStringList getCmdsFreqOutput(const POWER1MODULE::cPower1ModuleConfigData *configData,
                                          const QHash<QString, cFoutInfo> foutInfoHash,
--- zera-modules/power1module/lib/power1modulemeasprogram.cpp
+++ zera-modules/power1module/lib/power1modulemeasprogram.cpp
@@ -510,7 +510,9 @@ void cPower1ModuleMeasProgram::setDspCmdList()
 
         case mXlb:
 
-        case mXls: {
+        case mXls:
+
+        case mXlsg: {
             brokerReturn = measBroker.getMeasMode(mInfo.getName(), measChannelPairList);
             dspMModesCommandList.append(brokerReturn.dspCmdList);
             std::shared_ptr<MeasMode> mode = std::make_shared<MeasMode>(mInfo.getName(),
