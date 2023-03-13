702bf6195300788faedfcd08f819d7cfc3d5b546
Andreas Müller <schnitzeltony@gmail.com>
2023-03-13 19:14:23 +0100

    Create library dsp-helpers to avoid dependency on zera-basemodule
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules-common/dsp-helpers/tests/test_dspchainidgen.h
@@ -0,0 +1,14 @@
+#ifndef TEST_DSPCHAINIDGEN_H
+#define TEST_DSPCHAINIDGEN_H
+
+#include <QObject>
+
+class test_dspchainidgen : public QObject
+{
+    Q_OBJECT
+private slots:
+    void main1();
+    void main2();
+};
+
+#endif // TEST_DSPCHAINIDGEN_H
