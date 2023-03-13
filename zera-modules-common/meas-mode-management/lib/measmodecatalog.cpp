0e62484378b433e943d7c6dbe325968e177d0447
Andreas Müller <schnitzeltony@gmail.com>
2023-03-13 11:55:02 +0100

    MeasModeCatalog: Add getInfo(measmodes modeId)
    Currently it is just used by tests but I bet it opens other options
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules-common/meas-mode-management/lib/measmodecatalog.cpp
+++ zera-modules-common/meas-mode-management/lib/measmodecatalog.cpp
@@ -1,33 +1,54 @@
 #include "measmodecatalog.h"
 
-QHash<QString,cMeasModeInfo> MeasModeCatalog::m_modeInfoHash;
+QHash<QString,cMeasModeInfo> MeasModeCatalog::m_modeInfoHashByString;
+QHash<measmodes,cMeasModeInfo> MeasModeCatalog::m_modeInfoHashById;
+
 std::function<void(QString mode)> MeasModeCatalog::m_warningHandler = [](QString name) {
     qWarning("Unknown measurement mode %s", qPrintable(name));
 };
 
+void MeasModeCatalog::setupHashes()
+{
+    addInfoToHashes(cMeasModeInfo("4LW", "P", "W", m4lw));
+    addInfoToHashes(cMeasModeInfo("4LB", "Q", "Var", m4lb));
+    addInfoToHashes(cMeasModeInfo("4LBK", "Q", "Var", m4lbk));
+    addInfoToHashes(cMeasModeInfo("4LS", "S", "VA", m4ls));
+    addInfoToHashes(cMeasModeInfo("4LSg", "S", "W", m4lsg));
+    addInfoToHashes(cMeasModeInfo("3LW", "P", "W", m3lw));
+    addInfoToHashes(cMeasModeInfo("3LB", "Q", "Var", m3lb));
+    addInfoToHashes(cMeasModeInfo("2LW", "P", "W", m2lw));
+    addInfoToHashes(cMeasModeInfo("2LB", "Q", "Var", m2lb));
+    addInfoToHashes(cMeasModeInfo("2LS", "S", "VA", m2ls));
+    addInfoToHashes(cMeasModeInfo("2LSg", "S", "VA", m2lsg));
+    addInfoToHashes(cMeasModeInfo("XLW", "P", "W", mXlw));
+    addInfoToHashes(cMeasModeInfo("XLB", "Q", "Var", mXlb));
+    addInfoToHashes(cMeasModeInfo("XLS", "S", "VA", mXls));
+    addInfoToHashes(cMeasModeInfo("XLSg", "S", "VA", mXlsg));
+    addInfoToHashes(cMeasModeInfo("QREF", "P", "W", mqref));
+}
+
+void MeasModeCatalog::addInfoToHashes(cMeasModeInfo info)
+{
+    m_modeInfoHashByString[info.getName()] = info;
+    m_modeInfoHashById[info.getCode()] = info;
+}
+
 cMeasModeInfo MeasModeCatalog::getInfo(QString name)
 {
-    if(m_modeInfoHash.isEmpty()) {
-        m_modeInfoHash["4LW"] = cMeasModeInfo("4LW", "P", "W", m4lw);
-        m_modeInfoHash["4LB"] = cMeasModeInfo("4LB", "Q", "Var", m4lb);
-        m_modeInfoHash["4LBK"] = cMeasModeInfo("4LBK", "Q", "Var", m4lbk);
-        m_modeInfoHash["4LS"] = cMeasModeInfo("4LS", "S", "VA", m4ls);
-        m_modeInfoHash["4LSg"] = cMeasModeInfo("4LSg", "S", "W", m4lsg);
-        m_modeInfoHash["3LW"] = cMeasModeInfo("3LW", "P", "W", m3lw);
-        m_modeInfoHash["3LB"] = cMeasModeInfo("3LB", "Q", "Var", m3lb);
-        m_modeInfoHash["2LW"] = cMeasModeInfo("2LW", "P", "W", m2lw);
-        m_modeInfoHash["2LB"] = cMeasModeInfo("2LB", "Q", "Var", m2lb);
-        m_modeInfoHash["2LS"] = cMeasModeInfo("2LS", "S", "VA", m2ls);
-        m_modeInfoHash["2LSg"] = cMeasModeInfo("2LSg", "S", "VA", m2lsg);
-        m_modeInfoHash["XLW"] = cMeasModeInfo("XLW", "P", "W", mXlw);
-        m_modeInfoHash["XLB"] = cMeasModeInfo("XLB", "Q", "Var", mXlb);
-        m_modeInfoHash["XLS"] = cMeasModeInfo("XLS", "S", "VA", mXls);
-        m_modeInfoHash["XLSg"] = cMeasModeInfo("XLSg", "S", "VA", mXlsg);
-        m_modeInfoHash["QREF"] = cMeasModeInfo("QREF", "P", "W", mqref);
-    }
-    if(!m_modeInfoHash.contains(name)) {
+    if(m_modeInfoHashByString.isEmpty())
+        setupHashes();
+    if(!m_modeInfoHashByString.contains(name)) {
         m_warningHandler(name);
         return cMeasModeInfo();
     }
-    return m_modeInfoHash[name];
+    return m_modeInfoHashByString[name];
+}
+
+cMeasModeInfo MeasModeCatalog::getInfo(measmodes modeId)
+{
+    if(m_modeInfoHashById.isEmpty())
+        setupHashes();
+    if(!m_modeInfoHashById.contains(modeId))
+        return cMeasModeInfo(); // no warning - we test this
+    return m_modeInfoHashById[modeId];
 }
--- zera-modules-common/meas-mode-management/lib/measmodecatalog.h
+++ zera-modules-common/meas-mode-management/lib/measmodecatalog.h
@@ -9,10 +9,14 @@ class MeasModeCatalog
 {
 public:
     static cMeasModeInfo getInfo(QString name);
+    static cMeasModeInfo getInfo(measmodes modeId);
 protected:
     static std::function<void(QString mode)> m_warningHandler;
 private:
-    static QHash<QString,cMeasModeInfo> m_modeInfoHash;
+    static QHash<QString,cMeasModeInfo> m_modeInfoHashByString;
+    static QHash<measmodes,cMeasModeInfo> m_modeInfoHashById;
+    static void setupHashes();
+    static void addInfoToHashes(cMeasModeInfo info);
 };
 
 #endif // MEASMODECATALOG_H
