5664002833d9961b57d2be7fa679f5bf7ae4467d
Andreas Müller <schnitzeltony@gmail.com>
2023-03-15 19:10:05 +0100

    sec1module: Restructure folder layouts for tests as done at other modules
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/sec1module/tests/test_sec1_config_load.cpp
@@ -0,0 +1,61 @@
+#include "test_sec1_config_load.h"
+#include "sec1moduleconfiguration.h"
+#include "sec1moduleconfigdata.h"
+#include <memory>
+#include <QSignalSpy>
+#include <QTest>
+
+QTEST_MAIN(test_sec1_config_load)
+
+void test_sec1_config_load::fileFound()
+{
+    QFile tmpXmlConfigFile(QStringLiteral(CONFIG_PATH) + "/" + "com5003-sec1module.xml");
+    QVERIFY(tmpXmlConfigFile.exists());
+}
+
+void test_sec1_config_load::allFilesLoaded()
+{
+    // Note: Some checks are done within cPower1ModuleConfiguration and failures fire Q_ASSERT
+    QFileInfoList fileList = QDir(QStringLiteral(CONFIG_PATH)).entryInfoList(QStringList() << "*.xml");
+    for(const auto &fileInfo : fileList) {
+        QFile configFile(fileInfo.absoluteFilePath());
+        qInfo("Load %s...", qPrintable(configFile.fileName()));
+        QVERIFY(configFile.open(QIODevice::Unbuffered | QIODevice::ReadOnly));
+        SEC1MODULE::cSec1ModuleConfiguration conf;
+        conf.setConfiguration(configFile.readAll());
+        QVERIFY(conf.isConfigured());
+    }
+}
+
+
+// from here on basic loading / validation worka so we can load once only
+struct PowConfig
+{
+    QString filename;
+    std::shared_ptr<SEC1MODULE::cSec1ModuleConfiguration> config;
+};
+
+class ConfFileLoader
+{
+public:
+    static QVector<PowConfig> &get() {
+        if(m_confFiles.isEmpty()) {
+            QFileInfoList fileList = QDir(QStringLiteral(CONFIG_PATH)).entryInfoList(QStringList() << "*.xml");
+            m_confFiles.resize(fileList.count());
+            for(int i=0; i<fileList.count(); i++) {
+                QString filename = fileList[i].fileName();
+                m_confFiles[i].filename = filename;
+                m_confFiles[i].config = std::make_shared<SEC1MODULE::cSec1ModuleConfiguration>();
+                QFile configFile(fileList[i].absoluteFilePath());
+                configFile.open(QIODevice::Unbuffered | QIODevice::ReadOnly);
+                m_confFiles[i].config->setConfiguration(configFile.readAll());
+            }
+        }
+        return m_confFiles;
+    }
+private:
+    static QVector<PowConfig> m_confFiles;
+};
+
+QVector<PowConfig> ConfFileLoader::m_confFiles;
+
