c877699caf72f63af02d111318ec28926ce76dd3
Andreas Müller <schnitzeltony@gmail.com>
2023-03-29 19:27:57 +0200

    power1module: Make 4-wire modes capable of setting phases
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/power1module/lib/power1dspmodefunctioncatalog.cpp
+++ zera-modules/power1module/lib/power1dspmodefunctioncatalog.cpp
@@ -12,11 +12,9 @@ const ModeNameFunctionHash Power1DspModeFunctionCatalog::get(int measSytemCount)
     // uncommon first
     hash["QREF"] = { mqref, Power1DspCmdGenerator::getCmdsMModeMQREF,
                      [measSytemCount] {return std::make_unique<MeasModePhaseSetStrategyPhasesFixed>(MModePhaseMask("111"), measSytemCount);}};
-    hash["4LBK"] = { m4lbk, Power1DspCmdGenerator::getCmdsMMode4LBK,
-            []{ return std::make_unique<MeasModePhaseSetStrategy4Wire>();} };
 
     hash["4LW"] = { mXlw, Power1DspCmdGenerator::getCmdsMModeXLW,
-            []{ return std::make_unique<MeasModePhaseSetStrategy4Wire>();} };
+            [measSytemCount] {return std::make_unique<MeasModePhaseSetStrategyXWire>(MModePhaseMask("111"), measSytemCount);}};
     hash["3LW"] = { m3lw, Power1DspCmdGenerator::getCmdsMMode3LW,
             []{ return std::make_unique<MeasModePhaseSetStrategyPhasesFixed>(MModePhaseMask("101"), 3);} };
     hash["2LW"] = { mXlw, Power1DspCmdGenerator::getCmdsMModeXLW,
@@ -25,25 +23,27 @@ const ModeNameFunctionHash Power1DspModeFunctionCatalog::get(int measSytemCount)
             [measSytemCount] {return std::make_unique<MeasModePhaseSetStrategyXWire>(MModePhaseMask("111"), measSytemCount);}};
 
     hash["4LB"] = { mXlb, Power1DspCmdGenerator::getCmdsMModeXLB,
-            []{ return std::make_unique<MeasModePhaseSetStrategy4Wire>();} };
+            [measSytemCount] {return std::make_unique<MeasModePhaseSetStrategyXWire>(MModePhaseMask("111"), measSytemCount);}};
     hash["3LB"] = { m3lb, Power1DspCmdGenerator::getCmdsMMode3LB,
             []{ return std::make_unique<MeasModePhaseSetStrategyPhasesFixed>(MModePhaseMask("101"), 3);} };
     hash["2LB"] = { mXlb, Power1DspCmdGenerator::getCmdsMModeXLB,
             []{ return std::make_unique<MeasModePhaseSetStrategy2Wire>();} };
     hash["XLB"] = { mXlb, Power1DspCmdGenerator::getCmdsMModeXLB,
             [measSytemCount] {return std::make_unique<MeasModePhaseSetStrategyXWire>(MModePhaseMask("111"), measSytemCount);}};
+    hash["4LBK"] = { m4lbk, Power1DspCmdGenerator::getCmdsMMode4LBK,
+            []{ return std::make_unique<MeasModePhaseSetStrategy4Wire>();} };
 
     hash["XLS"] = { mXls, Power1DspCmdGenerator::getCmdsMModeXLS,
                      [measSytemCount] {return std::make_unique<MeasModePhaseSetStrategyXWire>(MModePhaseMask("111"), measSytemCount);}};
     hash["4LS"] = { mXls, Power1DspCmdGenerator::getCmdsMModeXLS,
-            []{ return std::make_unique<MeasModePhaseSetStrategy4Wire>();} };
+            [measSytemCount] {return std::make_unique<MeasModePhaseSetStrategyXWire>(MModePhaseMask("111"), measSytemCount);}};
     hash["2LS"] = { mXls, Power1DspCmdGenerator::getCmdsMModeXLS,
             []{ return std::make_unique<MeasModePhaseSetStrategy2Wire>();} };
 
     hash["XLSg"] = { mXlsg, Power1DspCmdGenerator::getCmdsMModeXLSg,
                      [measSytemCount] {return std::make_unique<MeasModePhaseSetStrategyXWire>(MModePhaseMask("111"), measSytemCount);}};
     hash["4LSg"] = { mXlsg, Power1DspCmdGenerator::getCmdsMModeXLSg,
-            []{ return std::make_unique<MeasModePhaseSetStrategy4Wire>();} };
+            [measSytemCount] {return std::make_unique<MeasModePhaseSetStrategyXWire>(MModePhaseMask("111"), measSytemCount);}};
     hash["2LSg"] = { mXlsg, Power1DspCmdGenerator::getCmdsMModeXLSg,
             []{ return std::make_unique<MeasModePhaseSetStrategy2Wire>();} };
     return hash;
