1485ac1963d7fcb805277114f80b2c52b16676c7
Andreas Müller <schnitzeltony@gmail.com>
2023-04-08 14:51:55 +0200

    cSCPIClient: Resort initializer list to fix clazy warning
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/scpimodule/lib/scpi-clients/scpiclient.cpp
+++ zera-modules/scpimodule/lib/scpi-clients/scpiclient.cpp
@@ -24,8 +24,10 @@ namespace SCPIMODULE
 {
 
 
-cSCPIClient::cSCPIClient(cSCPIModule* module, cSCPIModuleConfigData &configdata, cSCPIInterface* iface)
-    :m_pModule(module), m_ConfigData(configdata), m_pSCPIInterface(iface)
+cSCPIClient::cSCPIClient(cSCPIModule* module, cSCPIModuleConfigData &configdata, cSCPIInterface* iface) :
+    m_pSCPIInterface(iface),
+    m_pModule(module),
+    m_ConfigData(configdata)
 {
     mClientId = QUuid::createUuid(); // we need an unique id in case we want to send deferred error notifications events
 
