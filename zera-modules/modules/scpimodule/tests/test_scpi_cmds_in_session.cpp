1a8d48c5ff38a8f3356fc17bb91ebd2695f226a0
Andreas Müller <schnitzeltony@gmail.com>
2023-04-08 22:41:15 +0200

    zera-modules: Use ModuleManagerForTest for the first time
    It is not yet a proper test but warnings spawned look familiar...
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/modules/scpimodule/tests/test_scpi_cmds_in_session.cpp
@@ -0,0 +1,15 @@
+#include "test_scpi_cmds_in_session.h"
+#include "modulemanagerfortest.h"
+#include <scpimodule.h>
+#include <QTest>
+
+QTEST_MAIN(test_scpi_cmds_in_session)
+
+void test_scpi_cmds_in_session::initialSession()
+{
+    ModuleManagerForTest modman;
+
+    SCPIMODULE::cSCPIModule scpiModule(1, 9999, modman.getStorageSystem());
+    modman.addModule(&scpiModule, QStringLiteral(CONFIG_PATH) + "/" + "demo-scpimodule.xml");
+
+}
