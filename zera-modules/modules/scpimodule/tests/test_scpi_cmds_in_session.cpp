805343941f30ab98f823a84d81f11ee39b2d3ee2
Andreas Müller <schnitzeltony@gmail.com>
2023-04-09 22:09:56 +0200

    test_scpi_cmds_in_session::initialSession(): Add basic tests on ModuleManagerForTest
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/modules/scpimodule/tests/test_scpi_cmds_in_session.cpp
+++ zera-modules/modules/scpimodule/tests/test_scpi_cmds_in_session.cpp
@@ -6,10 +6,15 @@
 QTEST_MAIN(test_scpi_cmds_in_session)
 
 void test_scpi_cmds_in_session::initialSession()
-{
+{   // First test on ModuleManagerForTest to know it works as expected
     ModuleManagerForTest modman;
 
     SCPIMODULE::cSCPIModule scpiModule(1, 9999, modman.getStorageSystem());
     modman.addModule(&scpiModule, QStringLiteral(CONFIG_SOURCE_PATH) + "/" + "demo-scpimodule.xml");
 
+    VeinStorage::VeinHash* storageHash = modman.getStorageSystem();
+    QList<int> entityList = storageHash->getEntityList();
+    QCOMPARE(entityList.count(), 1);
+    QList<QString> componentList = storageHash->getEntityComponents(entityList[0]);
+    QCOMPARE(componentList.count(), 4); // EntitiyName / Metadata / PAR_SerialScpiActive / ACT_SerialScpiDeviceFile
 }
