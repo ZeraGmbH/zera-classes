bd131cdf114ff01ceaa9a6f0c339b23c8d782af1
Andreas Müller <schnitzeltony@gmail.com>
2023-04-28 23:17:34 +0200

    dftmodule: Move to common folder layout
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/modules/dftmodule/tests/test_dft_config_load.cpp
@@ -0,0 +1,24 @@
+#include "test_dft_config_load.h"
+#include "dftmoduleconfiguration.h"
+#include <QTest>
+
+QTEST_MAIN(test_dft_config_load)
+
+void test_dft_config_load::fileFound()
+{
+    QFile tmpXmlConfigFile(QStringLiteral(CONFIG_SOURCES_DFTMODULE) + "/" + "com5003-dftmodule.xml");
+    QVERIFY(tmpXmlConfigFile.exists());
+}
+
+void test_dft_config_load::allFilesLoaded()
+{
+    QFileInfoList fileList = QDir(QStringLiteral(CONFIG_SOURCES_DFTMODULE)).entryInfoList(QStringList() << "*.xml");
+    for(const auto &fileInfo : fileList) {
+        QFile configFile(fileInfo.absoluteFilePath());
+        qInfo("Load %s...", qPrintable(configFile.fileName()));
+        QVERIFY(configFile.open(QIODevice::Unbuffered | QIODevice::ReadOnly));
+        DFTMODULE::cDftModuleConfiguration conf;
+        conf.setConfiguration(configFile.readAll());
+        QVERIFY(conf.isConfigured());
+    }
+}
