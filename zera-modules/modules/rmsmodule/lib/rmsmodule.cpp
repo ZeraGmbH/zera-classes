2af505fc4953e0f1410ffbabf6d63781f23831b4
Andreas Müller <schnitzeltony@gmail.com>
2023-05-02 21:26:41 +0200

    rmsmodule: Move to Qt5 signal/slot syntax
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/modules/rmsmodule/lib/rmsmodule.cpp
+++ zera-modules/modules/rmsmodule/lib/rmsmodule.cpp
@@ -13,33 +13,33 @@ cRmsModule::cRmsModule(quint8 modnr, int entityId, VeinEvent::StorageSystem* sto
     m_sModuleDescription = QString("This module measures true rms values for configured channels");
     m_sSCPIModuleName = QString("%1%2").arg(BaseSCPIModuleName).arg(modnr);
 
-    m_ActivationStartState.addTransition(this, SIGNAL(activationContinue()), &m_ActivationExecState);
-    m_ActivationExecState.addTransition(this, SIGNAL(activationContinue()), &m_ActivationDoneState);
-    m_ActivationDoneState.addTransition(this, SIGNAL(activationNext()), &m_ActivationExecState);
-    m_ActivationDoneState.addTransition(this, SIGNAL(activationContinue()), &m_ActivationFinishedState);
+    m_ActivationStartState.addTransition(this, &cRmsModule::activationContinue, &m_ActivationExecState);
+    m_ActivationExecState.addTransition(this, &cRmsModule::activationContinue, &m_ActivationDoneState);
+    m_ActivationDoneState.addTransition(this, &cRmsModule::activationNext, &m_ActivationExecState);
+    m_ActivationDoneState.addTransition(this, &cRmsModule::activationContinue, &m_ActivationFinishedState);
     m_ActivationMachine.addState(&m_ActivationStartState);
     m_ActivationMachine.addState(&m_ActivationExecState);
     m_ActivationMachine.addState(&m_ActivationDoneState);
     m_ActivationMachine.addState(&m_ActivationFinishedState);
     m_ActivationMachine.setInitialState(&m_ActivationStartState);
-    connect(&m_ActivationStartState, SIGNAL(entered()), SLOT(activationStart()));
-    connect(&m_ActivationExecState, SIGNAL(entered()), SLOT(activationExec()));
-    connect(&m_ActivationDoneState, SIGNAL(entered()), SLOT(activationDone()));
-    connect(&m_ActivationFinishedState, SIGNAL(entered()), SLOT(activationFinished()));
+    connect(&m_ActivationStartState, &QAbstractState::entered, this, &cRmsModule::activationStart);
+    connect(&m_ActivationExecState, &QAbstractState::entered, this, &cRmsModule::activationExec);
+    connect(&m_ActivationDoneState, &QAbstractState::entered, this, &cRmsModule::activationDone);
+    connect(&m_ActivationFinishedState, &QAbstractState::entered, this, &cRmsModule::activationFinished);
 
-    m_DeactivationStartState.addTransition(this, SIGNAL(deactivationContinue()), &m_DeactivationExecState);
-    m_DeactivationExecState.addTransition(this, SIGNAL(deactivationContinue()), &m_DeactivationDoneState);
-    m_DeactivationDoneState.addTransition(this, SIGNAL(deactivationNext()), &m_DeactivationExecState);
-    m_DeactivationDoneState.addTransition(this, SIGNAL(deactivationContinue()), &m_DeactivationFinishedState);
+    m_DeactivationStartState.addTransition(this, &cRmsModule::deactivationContinue, &m_DeactivationExecState);
+    m_DeactivationExecState.addTransition(this, &cRmsModule::deactivationContinue, &m_DeactivationDoneState);
+    m_DeactivationDoneState.addTransition(this, &cRmsModule::deactivationNext, &m_DeactivationExecState);
+    m_DeactivationDoneState.addTransition(this, &cRmsModule::deactivationContinue, &m_DeactivationFinishedState);
     m_DeactivationMachine.addState(&m_DeactivationStartState);
     m_DeactivationMachine.addState(&m_DeactivationExecState);
     m_DeactivationMachine.addState(&m_DeactivationDoneState);
     m_DeactivationMachine.addState(&m_DeactivationFinishedState);
     m_DeactivationMachine.setInitialState(&m_DeactivationStartState);
-    connect(&m_DeactivationStartState, SIGNAL(entered()), SLOT(deactivationStart()));
-    connect(&m_DeactivationExecState, SIGNAL(entered()), SLOT(deactivationExec()));
-    connect(&m_DeactivationDoneState, SIGNAL(entered()), SLOT(deactivationDone()));
-    connect(&m_DeactivationFinishedState, SIGNAL(entered()), SLOT(deactivationFinished()));
+    connect(&m_DeactivationStartState, &QAbstractState::entered, this, &cRmsModule::deactivationStart);
+    connect(&m_DeactivationExecState, &QAbstractState::entered, this, &cRmsModule::deactivationExec);
+    connect(&m_DeactivationDoneState, &QAbstractState::entered, this, &cRmsModule::deactivationDone);
+    connect(&m_DeactivationFinishedState, &QAbstractState::entered, this, &cRmsModule::deactivationFinished);
 
 }
 
@@ -65,9 +65,9 @@ void cRmsModule::setupModule()
     // we need some program that does the measuring on dsp
     m_pMeasProgram = new cRmsModuleMeasProgram(this, m_pConfiguration);
     m_ModuleActivistList.append(m_pMeasProgram);
-    connect(m_pMeasProgram, SIGNAL(activated()), SIGNAL(activationContinue()));
-    connect(m_pMeasProgram, SIGNAL(deactivated()), this, SIGNAL(deactivationContinue()));
-    connect(m_pMeasProgram, SIGNAL(errMsg(QVariant)), m_pModuleErrorComponent, SLOT(setValue(QVariant)));
+    connect(m_pMeasProgram, &cModuleActivist::activated, this, &cRmsModule::activationContinue);
+    connect(m_pMeasProgram, &cModuleActivist::deactivated, this, &cRmsModule::deactivationContinue);
+    connect(m_pMeasProgram, &cModuleActivist::errMsg, m_pModuleErrorComponent, &VfModuleErrorComponent::setValue);
 
     for (int i = 0; i < m_ModuleActivistList.count(); i++)
         m_ModuleActivistList.at(i)->generateInterface();
