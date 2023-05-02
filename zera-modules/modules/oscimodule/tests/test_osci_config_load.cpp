362b39f8b1feb5552371ec4b1b7d7731b6d5c5a0
Andreas Müller <schnitzeltony@gmail.com>
2023-05-02 22:16:20 +0200

    oscimodule: Move to common folder layout
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/modules/oscimodule/tests/test_osci_config_load.cpp
@@ -0,0 +1,24 @@
+#include "test_osci_config_load.h"
+#include "oscimoduleconfiguration.h"
+#include <QTest>
+
+QTEST_MAIN(test_osci_config_load)
+
+void test_osci_config_load::fileFound()
+{
+    QFile tmpXmlConfigFile(QStringLiteral(CONFIG_SOURCES_OSCIMODULE) + "/" + "com5003-oscimodule.xml");
+    QVERIFY(tmpXmlConfigFile.exists());
+}
+
+void test_osci_config_load::allFilesLoaded()
+{
+    QFileInfoList fileList = QDir(QStringLiteral(CONFIG_SOURCES_OSCIMODULE)).entryInfoList(QStringList() << "*.xml");
+    for(const auto &fileInfo : fileList) {
+        QFile configFile(fileInfo.absoluteFilePath());
+        qInfo("Load %s...", qPrintable(configFile.fileName()));
+        QVERIFY(configFile.open(QIODevice::Unbuffered | QIODevice::ReadOnly));
+        OSCIMODULE::cOsciModuleConfiguration conf;
+        conf.setConfiguration(configFile.readAll());
+        QVERIFY(conf.isConfigured());
+    }
+}
