b4739c75fe0e61ad50127a75c60947b139eebb32
Anuja Mutalik <a.mutalik@zera.de>
2023-05-10 15:34:42 +0200

    ieee4882interface: Add descriptions for standard SCPI commands
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- zera-modules/modules/scpimodule/lib/ieee4882interface.cpp
+++ zera-modules/modules/scpimodule/lib/ieee4882interface.cpp
@@ -28,62 +28,62 @@ bool cIEEE4882Interface::setupInterface()
 {
     cSCPIInterfaceDelegate* delegate;
 
-    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*OPC"), SCPI::isQuery | SCPI::isCmd, operationComplete);
+    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*OPC"), SCPI::isQuery | SCPI::isCmd, operationComplete, QString("OPeration Complete"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*ESE"), SCPI::isQuery | SCPI::isCmdwP, eventstatusenable);
+    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*ESE"), SCPI::isQuery | SCPI::isCmdwP, eventstatusenable, QString("Event Status Enable"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*SRE"), SCPI::isQuery | SCPI::isCmdwP, servicerequestenable);
+    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*SRE"), SCPI::isQuery | SCPI::isCmdwP, servicerequestenable, QString("Service Request Enable"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*CLS"), SCPI::isCmd, clearstatus);
+    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*CLS"), SCPI::isCmd, clearstatus, QString("Clear Status"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*RST"), SCPI::isCmd, reset);
+    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*RST"), SCPI::isCmd, reset, QString("Reset"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*IDN"), SCPI::isQuery, identification);
+    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*IDN"), SCPI::isQuery, identification, QString("Identification query"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*ESR"), SCPI::isQuery, eventstatusregister);
+    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*ESR"), SCPI::isQuery, eventstatusregister, QString("Event Status Register"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*STB"), SCPI::isQuery, statusbyte);
+    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*STB"), SCPI::isQuery, statusbyte, QString("Status Byte query"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*TST"), SCPI::isQuery, selftest);
+    delegate = new cSCPIInterfaceDelegate(QString(""), QString("*TST"), SCPI::isQuery, selftest, QString("Self-Test query"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString("SYSTEM"), QString("ERROR"), SCPI::isNode | SCPI::isQuery, read1error);
+    delegate = new cSCPIInterfaceDelegate(QString("SYSTEM"), QString("ERROR"), SCPI::isNode | SCPI::isQuery, read1error, QString("Queries one error of internal error list"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString("SYSTEM:ERROR"), QString("COUNT"), SCPI::isQuery, readerrorcount);
+    delegate = new cSCPIInterfaceDelegate(QString("SYSTEM:ERROR"), QString("COUNT"), SCPI::isQuery, readerrorcount, QString("Queries the number of error messages of internal error list"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
 
-    delegate = new cSCPIInterfaceDelegate(QString("SYSTEM:ERROR"), QString("ALL"), SCPI::isQuery, readallerrors);
+    delegate = new cSCPIInterfaceDelegate(QString("SYSTEM:ERROR"), QString("ALL"), SCPI::isQuery, readallerrors, QString("Queries all error stored in internal error list"));
     m_IEEE4882DelegateList.append(delegate);
     m_pSCPIInterface->addSCPICommand(delegate);
     connect(delegate, &cSCPIInterfaceDelegate::signalExecuteSCPI, this, &cIEEE4882Interface::executeCmd);
