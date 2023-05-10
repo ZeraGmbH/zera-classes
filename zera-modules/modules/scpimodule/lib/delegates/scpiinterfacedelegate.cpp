4ca1c942c10aca03059844734529f7764151e653
Andreas Müller <schnitzeltony@gmail.com>
2023-05-10 15:14:55 +0200

    cSCPIInterfaceDelegate: Add optional description for XML export
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/modules/scpimodule/lib/delegates/scpiinterfacedelegate.cpp
+++ zera-modules/modules/scpimodule/lib/delegates/scpiinterfacedelegate.cpp
@@ -2,10 +2,12 @@
 
 namespace SCPIMODULE {
 
-cSCPIInterfaceDelegate::cSCPIInterfaceDelegate(QString cmdParent, QString cmd, quint8 type,  quint16 cmdCode) :
+cSCPIInterfaceDelegate::cSCPIInterfaceDelegate(QString cmdParent, QString cmd, quint8 type,  quint16 cmdCode, QString cmdDescription) :
     ScpiBaseDelegate(cmdParent, cmd, type),
     m_nCmdCode(cmdCode)
 {
+    if(!cmdDescription.isEmpty())
+        setXmlAttribute("Description", cmdDescription);
 }
 
 bool cSCPIInterfaceDelegate::executeSCPI(cSCPIClient *client, QString &sInput)
