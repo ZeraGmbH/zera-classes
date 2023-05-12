8d2239338aadd539c3c6f79221de4a80a75c57d0
Asmae Kaouissi <a.kaouissi@zera.de>
2023-05-12 10:30:52 +0200

    i2cmuxerinterface: replace QSharedPointer by std::shared_ptr
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- zera-i2c/zera-i2c-devices/lib/i2cmuxerinterface.h
+++ zera-i2c/zera-i2c-devices/lib/i2cmuxerinterface.h
@@ -1,12 +1,13 @@
 #ifndef I2CMUXERINTERFACE_H
 #define I2CMUXERINTERFACE_H
 
-#include <QSharedPointer>
+#include <QString>
+#include <memory>
 
 class I2cMuxerInterface
 {
 public:
-    typedef QSharedPointer<I2cMuxerInterface> Ptr;
+    typedef std::shared_ptr<I2cMuxerInterface> Ptr;
     virtual void enableMuxChannel() = 0;
     virtual void disableMux() = 0;
     virtual QString getDevIdString() = 0;
