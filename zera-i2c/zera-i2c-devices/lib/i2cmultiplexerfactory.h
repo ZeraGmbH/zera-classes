1339479d8e57621d7b678e1ac68e0094b1bc8d18
Asmae Kaouissi <a.kaouissi@zera.de>
2023-05-12 10:58:19 +0200

    Introduce i2cmultiplexerfactory
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

---
+++ zera-i2c/zera-i2c-devices/lib/i2cmultiplexerfactory.h
@@ -0,0 +1,13 @@
+#ifndef I2CMULTIPLEXERFACTORY_H
+#define I2CMULTIPLEXERFACTORY_H
+
+#include "i2cmuxernull.h"
+
+class I2cMultiplexerFactory
+{
+public:
+    static I2cMuxerInterface::Ptr createClampMuxer(QString deviceNode, ushort i2cMuxAdress, quint8 ctrlChannel);
+    static I2cMuxerInterface::Ptr createNullMuxer();
+};
+
+#endif // I2CMULTIPLEXERFACTORY_H
