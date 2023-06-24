c4d3c0f3ebbbc82dfd3055fbc1c25c03dc032e09
Andreas Müller <schnitzeltony@gmail.com>
2023-06-24 21:36:09 +0200

    Remove zera-i2c / zera-intel-hex
    This is a preparation to include modulemanager as it simplifies build:
    zera-i2c / zera-intel-hex were the only parts in oe which were build native to
    check hex files. Since they are gone now we can remove CMake option
    ZC_BUILD_MODULES.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-i2c/zera-i2c-devices/lib/mcontroller/zeramcontrolleriotemplate.cpp
+++
@@ -1,8 +0,0 @@
-#include "zeramcontrolleriotemplate.h"
-
-ZeraMControllerIoTemplate::ZeraMControllerIoTemplate(QString devnode, quint8 adr, quint8 debuglevel) :
-    m_sI2CDevNode(devnode),
-    m_nI2CAdr(adr),
-    m_nDebugLevel(debuglevel)
-{
-}
