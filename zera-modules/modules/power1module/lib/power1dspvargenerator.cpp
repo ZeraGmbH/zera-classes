452455c331900826e4d6a2e6e04c3ab1ab061b65
Andreas Müller <schnitzeltony@gmail.com>
2023-06-27 14:43:58 +0200

    power1module: Introduce Power1DspVarGenerator for DSP variable generation
    Not used yet
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-modules/modules/power1module/lib/power1dspvargenerator.cpp
@@ -0,0 +1,78 @@
+#include "power1dspvargenerator.h"
+#include "measmodeinfo.h"
+
+Power1DspVarGenerator::Power1DspVarGenerator()
+{
+
+}
+
+void Power1DspVarGenerator::setupVarList(Zera::cDSPInterface *pDSPInterFace, const POWER1MODULE::cPower1ModuleConfigData *confData, quint32 sampleRate)
+{
+    // we fetch a handle for sampled data and other temporary values
+    m_pTmpDataDsp = pDSPInterFace->getMemHandle("TmpData");
+    m_pTmpDataDsp->addVarItem( new cDspVar("MEASSIGNAL1", sampleRate, DSPDATA::vDspTemp)); // we need 2 signals for our computations
+    m_pTmpDataDsp->addVarItem( new cDspVar("MEASSIGNAL2", sampleRate, DSPDATA::vDspTemp));
+    m_pTmpDataDsp->addVarItem(new cDspVar("VALPQS", MeasPhaseCount+SumValueCount, DSPDATA::vDspTemp)); // here x1, x2, x3 , xs will land
+    m_pTmpDataDsp->addVarItem( new cDspVar("TEMP1", 2, DSPDATA::vDspTemp)); // we need 2 temp. vars also for complex
+    m_pTmpDataDsp->addVarItem( new cDspVar("TEMP2", 2, DSPDATA::vDspTemp));
+    m_pTmpDataDsp->addVarItem( new cDspVar("FAK", 1, DSPDATA::vDspTemp));
+    m_pTmpDataDsp->addVarItem(new cDspVar("FILTER", 2*(MeasPhaseCount+SumValueCount), DSPDATA::vDspTemp));
+    m_pTmpDataDsp->addVarItem( new cDspVar("N",1,DSPDATA::vDspTemp));
+
+    // a handle for parameter
+    m_pParameterDSP =  pDSPInterFace->getMemHandle("Parameter");
+    m_pParameterDSP->addVarItem( new cDspVar("TIPAR",1, DSPDATA::vDspParam, DSPDATA::dInt)); // integrationtime res = 1ms or period
+    // we use tistart as parameter, so we can finish actual measuring interval bei setting 0
+    m_pParameterDSP->addVarItem( new cDspVar("TISTART",1, DSPDATA::vDspParam, DSPDATA::dInt));
+    m_pParameterDSP->addVarItem( new cDspVar("MMODE",1, DSPDATA::vDspParam, DSPDATA::dInt));
+    // for meas modes with phase wise enable disable
+    for(int phase=0; phase<MeasPhaseCount; phase++) {
+        QString varName = QString("XMMODEPHASE%1").arg(phase);
+        m_pParameterDSP->addVarItem(new cDspVar(varName, 1, DSPDATA::vDspParam, DSPDATA::dInt));
+    }
+
+    // a handle for filtered actual values
+    m_pActualValuesDSP = pDSPInterFace->getMemHandle("ActualValues");
+    m_pActualValuesDSP->addVarItem(new cDspVar("VALPQSF", MeasPhaseCount+SumValueCount, DSPDATA::vDspResult));
+
+    // and one for the frequency output scale values, we need 1 value for each configured output
+    m_pfreqScaleDSP = pDSPInterFace->getMemHandle("FrequencyScale");
+    m_pfreqScaleDSP->addVarItem(new cDspVar("FREQSCALE", confData->m_nFreqOutputCount, DSPDATA::vDspParam));
+
+    // and one for nominal power in case that measuring mode is qref
+    m_pNomPower = pDSPInterFace->getMemHandle("QRefScale");
+    m_pNomPower->addVarItem(new cDspVar("NOMPOWER", 1, DSPDATA::vDspParam));
+    m_nDspMemUsed = m_pTmpDataDsp->getSize() + m_pParameterDSP->getSize() + m_pActualValuesDSP->getSize();
+}
+
+void Power1DspVarGenerator::deleteVarList(Zera::cDSPInterface *pDSPInterFace)
+{
+    pDSPInterFace->deleteMemHandle(m_pTmpDataDsp);
+    pDSPInterFace->deleteMemHandle(m_pParameterDSP);
+    pDSPInterFace->deleteMemHandle(m_pActualValuesDSP);
+}
+
+cDspMeasData *Power1DspVarGenerator::getActualValues()
+{
+    return m_pActualValuesDSP;
+}
+
+cDspMeasData *Power1DspVarGenerator::getFreqScale()
+{
+    return m_pfreqScaleDSP;
+}
+
+cDspMeasData *Power1DspVarGenerator::getNominalPower()
+{
+    return m_pNomPower;
+}
+
+cDspMeasData *Power1DspVarGenerator::getParameters()
+{
+    return m_pParameterDSP;
+}
+
+quint32 Power1DspVarGenerator::getMemUsed()
+{
+    return m_nDspMemUsed;
+}
---
+++ zera-modules/modules/power1module/lib/power1dspvargenerator.h
@@ -0,0 +1,28 @@
+#ifndef POWER1DSPVARGENERATOR_H
+#define POWER1DSPVARGENERATOR_H
+
+#include "power1moduleconfigdata.h"
+#include "dspinterface.h"
+#include "dspmeasdata.h"
+
+class Power1DspVarGenerator
+{
+public:
+    Power1DspVarGenerator();
+    void setupVarList(Zera::cDSPInterface* pDSPInterFace, const POWER1MODULE::cPower1ModuleConfigData* confData, quint32 sampleRate);
+    void deleteVarList(Zera::cDSPInterface* pDSPInterFace);
+    cDspMeasData* getActualValues();
+    cDspMeasData* getFreqScale();
+    cDspMeasData* getNominalPower();
+    cDspMeasData* getParameters();
+    quint32 getMemUsed();
+private:
+    cDspMeasData* m_pTmpDataDsp = nullptr;
+    cDspMeasData* m_pParameterDSP = nullptr;
+    cDspMeasData* m_pActualValuesDSP = nullptr;
+    cDspMeasData* m_pfreqScaleDSP = nullptr;
+    cDspMeasData* m_pNomPower = nullptr;
+    quint32 m_nDspMemUsed = 0;
+};
+
+#endif // POWER1DSPVARGENERATOR_H
