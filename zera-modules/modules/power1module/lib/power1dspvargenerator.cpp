a1d984a133f4418a5f11ed06b32cc821899093cb
Andreas Müller <schnitzeltony@gmail.com>
2023-06-28 10:46:01 +0200

    power1dspvargenerator: Rework and fix dsp-user-mem sisze calculation
    This points us to the misconception of resourcemanager: Modules must be polite
    and correct on asking for memory - book keeping is fragile.
    It's time to get rid of resourcemanager and move resource book keeping to
    services itself
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-modules/modules/power1module/lib/power1dspvargenerator.cpp
+++ zera-modules/modules/power1module/lib/power1dspvargenerator.cpp
@@ -10,14 +10,15 @@ void Power1DspVarGenerator::setupVarList(Zera::cDSPInterface *pDSPInterFace, con
 {
     // we fetch a handle for sampled data and other temporary values
     m_pTmpDataDsp = pDSPInterFace->getMemHandle("TmpData");
-    m_pTmpDataDsp->addVarItem( new cDspVar("MEASSIGNAL1", sampleRate, DSPDATA::vDspTemp)); // we need 2 signals for our computations
-    m_pTmpDataDsp->addVarItem( new cDspVar("MEASSIGNAL2", sampleRate, DSPDATA::vDspTemp));
+    m_pTmpDataDsp->addVarItem(new cDspVar("MEASSIGNAL1", sampleRate, DSPDATA::vDspTemp)); // we need 2 signals for our computations
+    m_pTmpDataDsp->addVarItem(new cDspVar("MEASSIGNAL2", sampleRate, DSPDATA::vDspTemp));
     m_pTmpDataDsp->addVarItem(new cDspVar("VALPQS", MeasPhaseCount+SumValueCount, DSPDATA::vDspTemp)); // here x1, x2, x3 , xs will land
-    m_pTmpDataDsp->addVarItem( new cDspVar("TEMP1", 2, DSPDATA::vDspTemp)); // we need 2 temp. vars also for complex
-    m_pTmpDataDsp->addVarItem( new cDspVar("TEMP2", 2, DSPDATA::vDspTemp));
-    m_pTmpDataDsp->addVarItem( new cDspVar("FAK", 1, DSPDATA::vDspTemp));
+    m_pTmpDataDsp->addVarItem(new cDspVar("TEMP1", 2, DSPDATA::vDspTemp)); // we need 2 temp. vars also for complex
+    m_pTmpDataDsp->addVarItem(new cDspVar("TEMP2", 2, DSPDATA::vDspTemp));
+    m_pTmpDataDsp->addVarItem(new cDspVar("FAK", 1, DSPDATA::vDspTemp));
     m_pTmpDataDsp->addVarItem(new cDspVar("FILTER", 2*(MeasPhaseCount+SumValueCount), DSPDATA::vDspTemp));
-    m_pTmpDataDsp->addVarItem( new cDspVar("N",1,DSPDATA::vDspTemp));
+    m_pTmpDataDsp->addVarItem(new cDspVar("N",1,DSPDATA::vDspTemp));
+    quint32 tmpDataSize = m_pTmpDataDsp->getSize();
 
     // a handle for parameter
     m_pParameterDSP =  pDSPInterFace->getMemHandle("Parameter");
@@ -30,19 +31,29 @@ void Power1DspVarGenerator::setupVarList(Zera::cDSPInterface *pDSPInterFace, con
         QString varName = QString("XMMODEPHASE%1").arg(phase);
         m_pParameterDSP->addVarItem(new cDspVar(varName, 1, DSPDATA::vDspParam, DSPDATA::dInt));
     }
+    quint32 paramDataSize = m_pParameterDSP->getSize();
 
     // a handle for filtered actual values
     m_pActualValuesDSP = pDSPInterFace->getMemHandle("ActualValues");
     m_pActualValuesDSP->addVarItem(new cDspVar("VALPQSF", MeasPhaseCount+SumValueCount, DSPDATA::vDspResult));
+    quint32 actualDataSize = m_pActualValuesDSP->getSize();
 
     // and one for the frequency output scale values, we need 1 value for each configured output
     m_pfreqScaleDSP = pDSPInterFace->getMemHandle("FrequencyScale");
     m_pfreqScaleDSP->addVarItem(new cDspVar("FREQSCALE", confData->m_nFreqOutputCount, DSPDATA::vDspParam));
+    quint32 freqScaleSize = m_pfreqScaleDSP->getSize();
 
     // and one for nominal power in case that measuring mode is qref
     m_pNomPower = pDSPInterFace->getMemHandle("QRefScale");
     m_pNomPower->addVarItem(new cDspVar("NOMPOWER", 1, DSPDATA::vDspParam));
-    m_nDspMemUsed = m_pTmpDataDsp->getSize() + m_pParameterDSP->getSize() + m_pActualValuesDSP->getSize();
+    quint32 nomPowerSize = m_pfreqScaleDSP->getSize();
+
+    m_nDspMemUsed =
+            tmpDataSize +
+            paramDataSize +
+            actualDataSize +
+            freqScaleSize +
+            nomPowerSize;
 }
 
 void Power1DspVarGenerator::deleteVarList(Zera::cDSPInterface *pDSPInterFace)
