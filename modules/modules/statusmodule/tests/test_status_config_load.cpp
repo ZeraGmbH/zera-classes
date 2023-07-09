67c53588525e371b971c6f439d06d51fc2cbe98a
Andreas Müller <schnitzeltony@gmail.com>
2023-07-10 00:22:00 +0200

    Revert "statusmodule: Move identical configuartion files into one"
    We missed the accumulator setting
    
    This reverts commit e6d2462bee1e6b023564f674bfb153fcae823370.

--- modules/modules/statusmodule/tests/test_status_config_load.cpp
+++ modules/modules/statusmodule/tests/test_status_config_load.cpp
@@ -8,13 +8,13 @@ QTEST_MAIN(test_status_config_load)
 
 void test_status_config_load::fileFound()
 {
-    QFile tmpXmlConfigFile(QStringLiteral(CONFIG_SOURCES_STATUSMODULE) + "/" + "statusmodule.xml");
+    QFile tmpXmlConfigFile(QStringLiteral(CONFIG_SOURCES_STATUSMODULE) + "/" + "com5003-statusmodule.xml");
     QVERIFY(tmpXmlConfigFile.exists());
 }
 
 void test_status_config_load::fileLoaded()
 {
-    QFile tmpXmlConfigFile(QStringLiteral(CONFIG_SOURCES_STATUSMODULE) + "/" + "statusmodule.xml");
+    QFile tmpXmlConfigFile(QStringLiteral(CONFIG_SOURCES_STATUSMODULE) + "/" + "com5003-statusmodule.xml");
     QVERIFY(tmpXmlConfigFile.open(QIODevice::Unbuffered | QIODevice::ReadOnly));
     std::unique_ptr<STATUSMODULE::cStatusModuleConfiguration> conf = std::make_unique<STATUSMODULE::cStatusModuleConfiguration>();
 
