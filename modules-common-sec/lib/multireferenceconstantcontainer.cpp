ce3467e698c014afc281ec9c944bceae446d5094
Andreas Müller <schnitzeltony@gmail.com>
2023-07-14 17:09:45 +0200

    Revert "sec1module: Carefully start linking in MultiReferenceConstantContainer"
    This reverts commit 8084d0a2be0e1e21b42f3094d6b438f845c353d4.

--- modules-common-sec/lib/multireferenceconstantcontainer.cpp
+++ modules-common-sec/lib/multireferenceconstantcontainer.cpp
@@ -2,22 +2,5 @@
 
 MultiReferenceConstantContainer::MultiReferenceConstantContainer()
 {
-}
-
-void MultiReferenceConstantContainer::initRefResources(QHash<QString, SecInputInfo *> refInputInfoHash)
-{
-    m_refInputInfoHash = refInputInfoHash;
-}
-
-void MultiReferenceConstantContainer::catchInterfaceAnswer(quint32 msgnr, quint8 reply, QVariant answer)
-{
-    if(!m_refInputInfoHash.isEmpty()) {
-
-    }
-}
-
-void MultiReferenceConstantContainer::startSetupTasks()
-{
-    emit sigSetupOk();
 
 }
