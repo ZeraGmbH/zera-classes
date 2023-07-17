479ba070866917456cb8bfbffcaee7b2975c11d2
Andreas Müller <schnitzeltony@gmail.com>
2023-07-18 00:47:02 +0200

    SecRefConstantObserver: Rename methods to enhance readability once we come back
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common-sec/lib/secrefconstantobserver.h
+++ modules-common-sec/lib/secrefconstantobserver.h
@@ -12,33 +12,32 @@ public:
     void registerNofifications(Zera::PcbInterfacePtr pcbInterface,
                                QStringList refInputNameList,
                                std::function<void ()> additionalErrorHandler = []{});
-    //void unregisterNofifications();
+    // unregister is done once per module. So it is not upon us.
     double getRefConstant(QString refChannnel);
 signals:
     void sigRegistrationFinished(bool ok);
     void sigRefConstantChanged(QString refInputName);
 
 private slots:
-    void onTaskFinish(bool ok, int taskId);
-    void onSingleFetchTaskFinish(bool ok, int taskId);
-    void onServerReceive(quint32 msgnr, quint8 reply, QVariant answer);
+    void onFinishKillTaskObject(bool ok, int taskId);
+    void onSingleConstantFetchFinish(bool ok, int taskId);
+    void onPcbServerReceive(quint32 msgnr, quint8 reply, QVariant answer);
 private:
-    void startGetContants(QStringList refChannelList);
-    void connectServerNotificationSlot();
+    void connectPcbServerNotificationSlot();
     TaskTemplatePtr createNotificationTasks(Zera::PcbInterfacePtr pcbInterface,
                                             QStringList refInputNameList,
                                             std::function<void ()> additionalErrorHandler);
     TaskTemplatePtr createRefConstantFetchTasks(QStringList refInputNameList,
                                                 std::function<void ()> additionalErrorHandler = []{});
     TaskTemplatePtr createSingleFetchTask(QString refInputName, std::function<void ()> additionalErrorHandler = []{});
-    void startSingleFetch(int notifyId);
+    void tryStartSingleConstantFetch(int notifyId);
     void startTask(TaskTemplatePtr task);
-    void initRefConstantVal(QString refInputName);
+    void makeSureSharedPtrToRefConstIsCreated(QString refInputName);
 
     Zera::PcbInterfacePtr m_pcbInterface;
     QHash<QString/* refInputName */, std::shared_ptr<double>> m_refConstants;
-    std::unordered_map<int /*taskId*/, TaskTemplatePtr> m_pendingTasks;
-    QHash<int /*taskId*/, QString /* refInputName */> m_pendingFetchTaskConstantNames;
+    std::unordered_map<int /*taskId*/, TaskTemplatePtr> m_pendingTaskIds;
+    QHash<int /*taskId*/, QString /* refInputName */> m_pendingSingleFetchTaskIds;
     QMap<int /* notifyId */, QString /* refInputName */> m_registerdNotifications;
 };
 
