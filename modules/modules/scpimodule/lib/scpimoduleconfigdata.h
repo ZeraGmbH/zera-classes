4ab72b0e28b51c03febda4a0bd43bb172d25eeed
Andreas Müller <schnitzeltony@gmail.com>
2023-09-12 15:37:21 +0200

    Scpimodule / Simplify configuration: Get rid of fallback identification
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/modules/scpimodule/lib/scpimoduleconfigdata.h
+++ modules/modules/scpimodule/lib/scpimoduleconfigdata.h
@@ -30,7 +30,6 @@ public:
     quint8 m_nClients; // the max. nr of clients accepted to connect
     cSocket m_InterfaceSocket; // we listen here ip is localhost
     QString m_sDeviceName;
-    QString m_sDeviceIdentification;
     quint8 m_nQuestonionableStatusBitCount;
     quint8 m_nOperationStatusBitCount;
     quint8 m_nOperationMeasureStatusBitCount;
--- modules/modules/scpimodule/lib/scpimoduleconfiguration.h
+++ modules/modules/scpimodule/lib/scpimoduleconfiguration.h
@@ -22,7 +22,6 @@ enum moduleconfigstate
     setSerialStopbits,
     setSerialDevice,
     setDeviceName,
-    setDeviceIdentification,
 
     setQuestionableStatusBitCount,
     setOperationStatusBitCount,
