00cc69e9606e01aa9e4c6196084282a04ebf978b
Andreas Müller <schnitzeltony@gmail.com>
2023-09-18 21:58:44 +0200

    modulemanager: Add what's necessary to get ModulemanagerConfig under test
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ modulemanager/testlib/modulemanagerconfigtest.h
@@ -0,0 +1,12 @@
+#ifndef MODULEMANAGERCONFIGTEST_H
+#define MODULEMANAGERCONFIGTEST_H
+
+#include <modulemanagerconfig.h>
+
+class ModulemanagerConfigTest : public ModulemanagerConfig
+{
+public:
+    static void enableTest();
+};
+
+#endif // MODULEMANAGERCONFIGTEST_H
