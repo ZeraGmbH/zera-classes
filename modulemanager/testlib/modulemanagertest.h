ae38282940d4096c2a2666171928c0c2e6097204
Andreas Müller <schnitzeltony@gmail.com>
2023-09-20 16:42:42 +0200

    modulemanager: Add necessary hacks to make test_modman_session pass in OE
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modulemanager/testlib/modulemanagertest.h
+++ modulemanager/testlib/modulemanagertest.h
@@ -6,7 +6,10 @@
 class ModuleManagerTest : public ZeraModules::ModuleManager
 {
 public:
+    explicit ModuleManagerTest(const QStringList &sessionList, QObject *parent = nullptr);
     static void enableTest();
+private:
+    virtual QStringList getModuleFileNames() override;
 };
 
 #endif // MODULEMANAGERTEST_H
