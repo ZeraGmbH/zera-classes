7fdffa702aaa35415bd36023ee2318e50da9279b
Andreas Müller <schnitzeltony@gmail.com>
2023-09-21 22:15:30 +0200

    zera-basemodule: Introduce ModuleConfigurationNull for modules without config
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ modules-common/zera-basemodule/lib/moduleconfigurationnull.h
@@ -0,0 +1,16 @@
+#ifndef MODULECONFIGURATIONNULL_H
+#define MODULECONFIGURATIONNULL_H
+
+#include "basemoduleconfiguration.h"
+
+class ModuleConfigurationNull : public cBaseModuleConfiguration
+{
+    Q_OBJECT
+public:
+    ModuleConfigurationNull();
+    virtual void setConfiguration(QByteArray xmlString) override;
+    virtual QByteArray exportConfiguration() override;
+    virtual void configXMLInfo(QString key) override;
+};
+
+#endif // MODULECONFIGURATIONNULL_H
