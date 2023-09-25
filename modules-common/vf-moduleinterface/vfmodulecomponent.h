1bc8ebf6fbaebd1ad5f458dad4ae2cdb2a091fe8
Andreas Müller <schnitzeltony@gmail.com>
2023-09-25 16:53:48 +0200

    cMetaData: Remove as it is not used as interface
    There are just two classes (VfModuleMetaData/VfModuleComponent) having methods
    with same name. This is not what interfaces are for.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/vf-moduleinterface/vfmodulecomponent.h
+++ modules-common/vf-moduleinterface/vfmodulecomponent.h
@@ -1,17 +1,16 @@
 #ifndef VEINMODULECOMPONENT_H
 #define VEINMODULECOMPONENT_H
 
-#include "metadata.h"
 #include <ve_eventsystem.h>
 #include <vcmp_componentdata.h>
 
-class VfModuleComponent: public cMetaData
+class VfModuleComponent: public QObject
 {
     Q_OBJECT
 public:
     VfModuleComponent(int entityId, VeinEvent::EventSystem *eventsystem, QString name, QString description, QVariant initval);
     ~VfModuleComponent();
-    void exportMetaData(QJsonObject &jsObj) override;
+    void exportMetaData(QJsonObject &jsObj);
     void setChannelName(QString name); // channel name for json export can be empty
     QString getChannelName();
     void setUnit(QString unit);
@@ -22,7 +21,7 @@ signals:
     void sigValueChanged(QVariant); // we connect here if we want to do something on changed values
     void sigValueQuery(QVariant); // we connect here if we want to read a value before returning data from storage ...perhaps with parameter
 public slots:
-    void setValue(QVariant value) override; // here we have to emit event for notification
+    void setValue(QVariant value); // here we have to emit event for notification
     void setError(); // here we have to emit event for error notification
 protected:
     void sendNotification(VeinComponent::ComponentData::Command vcmd);
