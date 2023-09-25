a94e6559d6e60161b148b32d60975608e2eb3942
Andreas Müller <schnitzeltony@gmail.com>
2023-09-25 16:53:48 +0200

    cMetaData: Remove as it is not used as interface
    There are just two classes (VfModuleMetaData/VfModuleComponent) having methods
    with same name. This is not what interfaces are for.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/vf-moduleinterface/vfmodulemetadata.h
+++ modules-common/vf-moduleinterface/vfmodulemetadata.h
@@ -1,18 +1,17 @@
 #ifndef VEINMODULEMETADATA_H
 #define VEINMODULEMETADATA_H
 
-#include "metadata.h"
 #include <QString>
 #include <QVariant>
 
-class VfModuleMetaData: public cMetaData
+class VfModuleMetaData: public QObject
 {
     Q_OBJECT
 public:
     VfModuleMetaData(QString name, QVariant value);
-    virtual void exportMetaData(QJsonObject &jsObj) override;
+    virtual void exportMetaData(QJsonObject &jsObj);
 public slots:
-    void setValue(QVariant value) override;
+    void setValue(QVariant value);
 private:
     QString m_sName;
     QVariant m_vValue;
