f3931e80e5c8c1c02ebad6df8fe5b786fd061651
Anuja Mutalik <a.mutalik@zera.de>
2023-10-11 11:21:27 +0200

    test_lambdacalcdelegate: More tests
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- modules/modules/lambdamodule/tests/test_lambdacalcdelegate.cpp
+++ modules/modules/lambdamodule/tests/test_lambdacalcdelegate.cpp
@@ -41,5 +41,45 @@ void test_lambdacalcdelegate::measModeNotAvail3LW()
     QCOMPARE(delegate.getLambdaValues().phases[0], 1.0);
     QCOMPARE(delegate.getLambdaValues().phases[1], 0.0);
     QCOMPARE(delegate.getLambdaValues().phases[2], 1.0);
+    QCOMPARE(delegate.getLambdaValues().sum, 0.5);//old computations would produce 'sum(p)/sum(s)=0.25'
+}
+
+void test_lambdacalcdelegate::measModeAvailPhaseMask110()
+{
+    LambdaCalcDelegate delegate(true);
+
+    delegate.onActivePower1Change(1.0);
+    delegate.onActivePower2Change(1.0);
+    delegate.onActivePower3Change(0.0);
+    delegate.onActivePowerSumChange(2.0);
+    delegate.onApparentPower1Change(2.0);
+    delegate.onApparentPower2Change(2.0);
+    delegate.onApparentPower3Change(4.0);
+    delegate.onApparentPowerSumChange(8.0);
+    delegate.onActivePowerPhaseMaskChange("110");
+
+    QCOMPARE(delegate.getLambdaValues().phases[0], 0.5);
+    QCOMPARE(delegate.getLambdaValues().phases[1], 0.5);
+    QCOMPARE(delegate.getLambdaValues().phases[2], 0.0);
     QCOMPARE(delegate.getLambdaValues().sum, 0.5);
+}
+
+void test_lambdacalcdelegate::measModeNotAvailPhaseMask110()
+{
+    LambdaCalcDelegate delegate(false);
+
+    delegate.onActivePower1Change(1.0);
+    delegate.onActivePower2Change(1.0);
+    delegate.onActivePower3Change(0.0);
+    delegate.onActivePowerSumChange(2.0);
+    delegate.onApparentPower1Change(2.0);
+    delegate.onApparentPower2Change(2.0);
+    delegate.onApparentPower3Change(4.0);
+    delegate.onApparentPowerSumChange(10.0);
+    delegate.onActivePowerPhaseMaskChange("110");
+
+    QCOMPARE(delegate.getLambdaValues().phases[0], 0.5);
+    QCOMPARE(delegate.getLambdaValues().phases[1], 0.5);
+    QCOMPARE(delegate.getLambdaValues().phases[2], 0.0);
+    QCOMPARE(delegate.getLambdaValues().sum, 0.25);//old computations would produce 'sum(p)/sum(s)=0.20'
 }
