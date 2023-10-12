c4d71212d7c40c99a47a9e79841ef0e082f3da24
Anuja Mutalik <a.mutalik@zera.de>
2023-10-12 12:00:01 +0200

    test_lambdacalcdelegate: Remove unnecessary comments
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- modules/modules/lambdamodule/tests/test_lambdacalcdelegate.cpp
+++ modules/modules/lambdamodule/tests/test_lambdacalcdelegate.cpp
@@ -41,7 +41,7 @@ void test_lambdacalcdelegate::measModeNotAvail3LW()
     QCOMPARE(delegate.getLambdaValues().phases[0], 1.0);
     QCOMPARE(delegate.getLambdaValues().phases[1], 0.0);
     QCOMPARE(delegate.getLambdaValues().phases[2], 1.0);
-    QCOMPARE(delegate.getLambdaValues().sum, 0.5);//old computations would produce 'sum(p)/sum(s)=0.25'
+    QCOMPARE(delegate.getLambdaValues().sum, 0.5);
 }
 
 void test_lambdacalcdelegate::measModeAvailPhaseMask110()
@@ -81,5 +81,5 @@ void test_lambdacalcdelegate::measModeNotAvailPhaseMask110()
     QCOMPARE(delegate.getLambdaValues().phases[0], 0.5);
     QCOMPARE(delegate.getLambdaValues().phases[1], 0.5);
     QCOMPARE(delegate.getLambdaValues().phases[2], 0.0);
-    QCOMPARE(delegate.getLambdaValues().sum, 0.25);//old computations would produce 'sum(p)/sum(s)=0.20'
+    QCOMPARE(delegate.getLambdaValues().sum, 0.25);
 }
