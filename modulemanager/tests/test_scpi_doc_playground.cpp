336927d6fd15ff6c5301f16002b275c0daca96a3
Anuja Mutalik <a.mutalik@zera.de>
2023-10-18 11:08:43 +0200

    test_scpi_doc_playground: Add test to create dev-iface-xmls for all sessions
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- modulemanager/tests/test_scpi_doc_playground.cpp
+++ modulemanager/tests/test_scpi_doc_playground.cpp
@@ -1,4 +1,13 @@
 #include "test_scpi_doc_playground.h"
+#include "jsonsessionloadertest.h"
+#include "modulemanagerconfigtest.h"
+#include "modulemanagertest.h"
+#include "modulemanagercontroller.h"
+#include "vn_introspectionsystem.h"
+#include "vs_veinhash.h"
+#include "licensesystemmock.h"
+#include "moduleeventhandler.h"
+#include <QDataStream>
 #include <QTest>
 #include <QProcess>
 #include <QDirIterator>
@@ -30,4 +39,75 @@ void test_scpi_doc_playground::runCreateAlHtmls()
         QVERIFY(html.size() > 0);
     }
 */
+}
+
+void test_scpi_doc_playground::createDevIfaceXml()
+{
+    JsonSessionLoaderTest::enableTests();
+    ModulemanagerConfigTest::enableTest();
+    ModuleManagerTest::enableTest();
+    ModulemanagerConfig::setDemoDevice("mt310s2");
+    qRegisterMetaTypeStreamOperators<QList<int> >("QList<int>");
+    qRegisterMetaTypeStreamOperators<QList<float> >("QList<float>");
+    qRegisterMetaTypeStreamOperators<QList<double> >("QList<double>");
+    qRegisterMetaTypeStreamOperators<QList<QString> >("QList<QString>");
+    qRegisterMetaTypeStreamOperators<QVector<QString> >("QVector<QString>");
+    qRegisterMetaTypeStreamOperators<QList<QVariantMap> >("QList<QVariantMap>");
+    qputenv("QT_FATAL_CRITICALS", "1"); \
+
+    ModuleEventHandler evHandler;
+    ModuleManagerController mmController;
+    VeinNet::IntrospectionSystem introspectionSystem;
+    VeinStorage::VeinHash storSystem;
+    LicenseSystemMock licenseSystem;
+
+    QList<VeinEvent::EventSystem*> subSystems;
+    subSystems.append(&mmController);
+    subSystems.append(&introspectionSystem);
+    subSystems.append(&storSystem);
+    subSystems.append(&licenseSystem);
+    evHandler.setSubsystems(subSystems);
+
+    ModulemanagerConfig* mmConfig = ModulemanagerConfig::getInstance();
+    const QStringList availableSessionList = mmConfig->getAvailableSessions();
+
+    ModuleManagerTest modMan(availableSessionList);
+    modMan.setDemo(true);
+    QVERIFY(modMan.loadAllAvailableModulePlugins());
+    modMan.setStorage(&storSystem);
+    modMan.setLicenseSystem(&licenseSystem);
+    modMan.setEventHandler(&evHandler);
+    mmController.setStorage(&storSystem);
+
+    JsonSessionLoader sessionLoader;
+
+    QObject::connect(&sessionLoader, &JsonSessionLoader::sigLoadModule, &modMan, &ZeraModules::ModuleManager::startModule);
+    QObject::connect(&modMan, &ZeraModules::ModuleManager::sigSessionSwitched, &sessionLoader, &JsonSessionLoader::loadSession);
+
+    QObject::connect(&modMan, &ZeraModules::ModuleManager::sigModulesLoaded, &mmController, &ModuleManagerController::initializeEntity);
+    QObject::connect(&mmController, &ModuleManagerController::sigChangeSession, &modMan, &ZeraModules::ModuleManager::changeSessionFile);
+    QObject::connect(&mmController, &ModuleManagerController::sigModulesPausedChanged, &modMan, &ZeraModules::ModuleManager::setModulesPaused);
+
+    mmController.initOnce();
+
+    QDir().mkdir(QStringLiteral(SCPI_DOC_PLAYGROUND_PATH) + "/scpi-xmls2");
+
+    for(const QString &session: availableSessionList) {
+        modMan.changeSessionFile(session);
+        ModuleManagerTest::feedEventLoop();
+        QString currentSession = storSystem.getStoredValue(0, "Session").toString();
+        QCOMPARE(currentSession, session);
+
+        QString actDevIface = storSystem.getStoredValue(9999, "ACT_DEV_IFACE").toString();
+        if(actDevIface.isEmpty()) // we have to make module resilient to this situation
+            qFatal("ACT_DEV_IFACE empty - local modulemanager running???");
+
+        QString xmlFileName = QStringLiteral(SCPI_DOC_PLAYGROUND_PATH) + "/scpi-xmls2/" + session;
+        xmlFileName.replace("json", "xml");
+        QFile xmlFile(xmlFileName);
+        QVERIFY(xmlFile.open(QIODevice::ReadWrite));
+        QTextStream data(&xmlFile);
+        data << actDevIface;
+        QVERIFY(xmlFile.size() > 0);
+    }
 }
--- modulemanager/tests/test_scpi_doc_playground.h
+++ modulemanager/tests/test_scpi_doc_playground.h
@@ -9,6 +9,7 @@ class test_scpi_doc_playground : public QObject
 private slots:
     void createAlHtmlsExists();
     void runCreateAlHtmls();
+    void createDevIfaceXml();
 };
 
 #endif // TEST_SCPI_DOC_PLAYGROUND_H
