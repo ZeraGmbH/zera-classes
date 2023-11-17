d5beeef036a3a11d5485e474e866f7a28c7e2887
Anuja Mutalik <a.mutalik@zera.de>
2023-11-17 10:53:17 +0100

    blemodule/configs:Add parameter 'bluetoothon'
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- modules/modules/blemodule/lib/blemoduleconfigdata.h
+++ modules/modules/blemodule/lib/blemoduleconfigdata.h
@@ -1,13 +1,23 @@
 #ifndef BLEMODULECONFIGDATA_H
 #define BLEMODULECONFIGDATA_H
 
+#include <QString>
+
 namespace BLEMODULE
 {
+
+struct boolParameter
+{
+    QString m_sKey;
+    quint8 m_nActive; // active or not 1,0
+};
+
 
 class cBleModuleConfigData
 {
 public:
-
+    cBleModuleConfigData(){}
+    boolParameter m_bluetoothOn;
 };
 
 }
