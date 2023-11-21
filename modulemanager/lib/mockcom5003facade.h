444c2a0ddc9d6aef586a163e7e631abcd0dac870
Anuja Mutalik <a.mutalik@zera.de>
2023-11-21 14:11:17 +0100

    modulemanager/lib: Introduce MockCom5003Facade
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

---
+++ modulemanager/lib/mockcom5003facade.h
@@ -0,0 +1,18 @@
+#ifndef MOCKCOM5003FACADE_H
+#define MOCKCOM5003FACADE_H
+
+#include "resmanrunfacade.h"
+#include "mockcom5003d.h"
+#include "mocksec1000d.h"
+
+class MockCom5003Facade
+{
+public:
+    MockCom5003Facade(QString sysroot);
+private:
+    ResmanRunFacade m_resman;
+    MockCom5003d m_mockcom5003d;
+    MockSec1000d m_mocksec1000d;
+};
+
+#endif // MOCKCOM5003FACADE_H
