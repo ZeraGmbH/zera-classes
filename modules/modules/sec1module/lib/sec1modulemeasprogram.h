b8f31501eafbbdce7d0696dbefe05207b3e02e12
Anuja Mutalik <a.mutalik@zera.de>
2023-11-22 17:44:51 +0100

    sec,spm,sem: Demo mode - communicate with resman & pcbserver
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- modules/modules/sec1module/lib/sec1modulemeasprogram.h
+++ modules/modules/sec1module/lib/sec1modulemeasprogram.h
@@ -85,7 +85,6 @@ private slots:
     void setpcbREFConstantNotifier();
     void setsecINTNotifier();
     void activationDone();
-    void setupDemoOperation();
 
     void stopECCalculator();
     void freeECalculator();
