594c02f00a6dc7e271fbdebed8340b136871728e
Anuja Mutalik <a.mutalik@zera.de>
2023-11-27 14:50:59 +0100

    modulemanager/scpi-doc: Update to run with mock-services
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- modulemanager/scpi-doc/main.cpp
+++ modulemanager/scpi-doc/main.cpp
@@ -11,6 +11,28 @@
 #include <QDir>
 #include <QProcess>
 
+void generateDevIfaceForAllSessions(QStringList sessionsList, ModuleManagerTest *modMan, VeinStorage::VeinHash *storSystem)
+{
+    for(const QString &session: qAsConst(sessionsList)) {
+        modMan->changeSessionFile(session);
+        do
+            ModuleManagerTest::feedEventLoop();
+        while(!modMan->areAllModulesShutdown());
+
+        QString actDevIface = storSystem->getStoredValue(9999, "ACT_DEV_IFACE").toString();
+        if(actDevIface.isEmpty()) // we have to make module resilient to this situation
+            qFatal("ACT_DEV_IFACE empty - local modulemanager running???");
+
+        QString xmlFileName = QStringLiteral(SCPI_DOC_BUILD_PATH) + "/scpi-xmls/" + session;
+        xmlFileName.replace("json", "xml");
+        QFile xmlFile(xmlFileName);
+        xmlFile.open(QFile::ReadWrite);
+        QTextStream data(&xmlFile);
+        data << actDevIface;
+    }
+
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -46,11 +68,17 @@ int main(int argc, char *argv[])
     subSystems.append(&licenseSystem);
     evHandler.setSubsystems(subSystems);
 
-    QStringList sessionFileList = QString(SESSION_FILES).split(",");
-    for(QString &session: sessionFileList) {
+    QStringList mtSessions, comSessions;
+    QStringList allSessions = QString(SESSION_FILES).split(",");
+    for(QString &session: allSessions) {
         session = session.section('/', -1);
+        if(session.contains("mt310s2"))
+            mtSessions.append(session);
+        if(session.contains("com5003"))
+            comSessions.append(session);
     }
-    ModuleManagerTest modMan(sessionFileList);
+
+    ModuleManagerTest modMan(allSessions);
     modMan.setDemo(true);
     modMan.loadAllAvailableModulePlugins();
     modMan.setStorage(&storSystem);
@@ -71,23 +99,14 @@ int main(int argc, char *argv[])
 
     QDir().mkdir(QStringLiteral(SCPI_DOC_BUILD_PATH) + "/scpi-xmls");
 
-    for(const QString &session: qAsConst(sessionFileList)) {
-        modMan.changeSessionFile(session);
-        do
-            ModuleManagerTest::feedEventLoop();
-        while(!modMan.areAllModulesShutdown());
-
-        QString actDevIface = storSystem.getStoredValue(9999, "ACT_DEV_IFACE").toString();
-        if(actDevIface.isEmpty()) // we have to make module resilient to this situation
-            qFatal("ACT_DEV_IFACE empty - local modulemanager running???");
-
-        QString xmlFileName = QStringLiteral(SCPI_DOC_BUILD_PATH) + "/scpi-xmls/" + session;
-        xmlFileName.replace("json", "xml");
-        QFile xmlFile(xmlFileName);
-        xmlFile.open(QFile::ReadWrite);
-        QTextStream data(&xmlFile);
-        data << actDevIface;
-    }
+    generateDevIfaceForAllSessions(mtSessions, &modMan, &storSystem);
+    evHandler.clearSystems();
+    modMan.destroyModules();
+    do
+        ModuleManagerTest::feedEventLoop();
+    while(!modMan.areAllModulesShutdown());
+    modMan.changeMockServices("com5003");
+    generateDevIfaceForAllSessions(comSessions, &modMan, &storSystem);
 
     QProcess sh;
     sh.start("/bin/sh", QStringList() << QStringLiteral(SCPI_DOC_SOURCE_PATH) + "/xml-to-html/create-all-htmls" << QStringLiteral(SCPI_DOC_BUILD_PATH));
