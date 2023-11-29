c1c6bdf272ddfc4f88a6457f7b1e83051f41511f
Anuja Mutalik <a.mutalik@zera.de>
2023-11-29 10:57:29 +0100

    modulemanagersetupfacade: Add ModuleManagerController
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- modulemanager/lib/modulemanagersetupfacade.h
+++ modulemanager/lib/modulemanagersetupfacade.h
@@ -2,17 +2,20 @@
 #define MODULEMANAGERSETUPFACADE_H
 
 #include "moduleeventhandler.h"
+#include "modulemanagercontroller.h"
 
 class ModuleManagerSetupFacade : public QObject
 {
     Q_OBJECT
 public:
-    ModuleManagerSetupFacade(QObject *parent = nullptr);
+    ModuleManagerSetupFacade(QObject *parent = nullptr, bool devMode = false);
     void addSubsystem(VeinEvent::EventSystem* subsystem);
     void addSystem(VeinEvent::EventSystem* system);
     void clearSystems();
+    ModuleManagerController *getModuleManagerController();
 private:
     ModuleEventHandler m_eventHandler;
+    ModuleManagerController m_mmController;
 };
 
 #endif // MODULEMANAGERSETUPFACADE_H
