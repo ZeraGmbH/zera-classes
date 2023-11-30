8479dc3ec9561465aa5b9ea5ac8c2bd2f18ec861
Anuja Mutalik <a.mutalik@zera.de>
2023-11-30 14:14:44 +0100

    modulemanager: Move application abort in case of demo and services IP is set
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- modulemanager/lib/modulemanager.cpp
+++ modulemanager/lib/modulemanager.cpp
@@ -78,8 +78,14 @@ ModuleManager::ModuleManager(ModuleManagerSetupFacade *setupFacade, bool demo, Q
         qCritical() << "Missing session file(s)" << missingSessions;
     }
     qDebug() << "sessions available:" << m_sessionsAvailable;
-    if(m_demo)
+
+    if(m_demo) {
+        if(!QString(ZC_SERVICES_IP).isEmpty()) {
+            qWarning() << "Running in demo mode, so ZC_SERIVCES_IP must be empty! It is set to:" << ZC_SERVICES_IP;
+            Q_ASSERT(false);
+        }
         setMockServices(mmConfig->getDeviceName());
+    }
 }
 
 ModuleManager::~ModuleManager()
