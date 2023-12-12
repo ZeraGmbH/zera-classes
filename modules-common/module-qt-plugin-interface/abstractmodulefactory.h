d7e962bd95d92d5330b873e371b880e3a640271b
Andreas Müller <schnitzeltony@gmail.com>
2023-12-12 22:18:14 +0100

    abstractmodulefactory.h: Prettify
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/module-qt-plugin-interface/abstractmodulefactory.h
+++ modules-common/module-qt-plugin-interface/abstractmodulefactory.h
@@ -1,26 +1,23 @@
 #ifndef MODULEINTERFACE_H
 #define MODULEINTERFACE_H
-#include <QtPlugin>
 
 #include "virtualmodule.h"
-
+#include <QtPlugin>
 #include <QList>
 
 namespace VeinEvent
 {
-  class StorageSystem;
+    class StorageSystem;
 }
-
 
 class MeasurementModuleFactory
 {
 public:
-  virtual ~MeasurementModuleFactory() {}
-  virtual ZeraModules::VirtualModule *createModule(int entityId, VeinEvent::StorageSystem* storagesystem, bool demo, int moduleNum) = 0;
-
-  virtual void destroyModule(ZeraModules::VirtualModule *module)=0;
-  virtual QList<ZeraModules::VirtualModule*> listModules() const =0;
-  virtual QString getFactoryName() const =0;
+    virtual ~MeasurementModuleFactory() = default;
+    virtual ZeraModules::VirtualModule *createModule(int entityId, VeinEvent::StorageSystem* storagesystem, bool demo, int moduleNum) = 0;
+    virtual void destroyModule(ZeraModules::VirtualModule *module) = 0;
+    virtual QList<ZeraModules::VirtualModule*> listModules() const = 0;
+    virtual QString getFactoryName() const = 0;
 };
 
 #define MeasurementModuleFactory_iid "org.qt-project.Qt.Examples.Test/1.0"
