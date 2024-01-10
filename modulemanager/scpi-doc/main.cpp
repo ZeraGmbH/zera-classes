ff5aab94c6e2c8cd90de5fbcaea93c1a06b85ade
Anuja Mutalik <a.mutalik@zera.de>
2024-01-10 16:53:33 +0100

    Add ScpiIfaceExportGenerator: Creates dev-iface-xmls for all sessions of a device
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- modulemanager/scpi-doc/main.cpp
+++ modulemanager/scpi-doc/main.cpp
@@ -1,44 +1,9 @@
-#include "modulemanager.h"
-#include "licensesystemmock.h"
-#include "modulemanagerconfig.h"
-#include <timemachineobject.h>
+#include "scpiifaceexportgenerator.h"
 #include <QCoreApplication>
 #include <QCommandLineParser>
 #include <QDir>
 #include <QProcess>
 
-void generateDevIfaceXmls(QString deviceName)
-{
-    ModulemanagerConfig::setDemoDevice(deviceName, false);
-    ModulemanagerConfig* mmConfig = ModulemanagerConfig::getInstance();
-    QStringList sessions = mmConfig->getAvailableSessions();
-    LicenseSystemMock licenseSystem;
-    ModuleManagerSetupFacade modManSetupFacade(&licenseSystem);
-    ZeraModules::ModuleManager modMan(&modManSetupFacade, true);
-    modMan.loadAllAvailableModulePlugins();
-    modMan.setupConnections();
-    modMan.setMockServices(deviceName);
-    for(const QString &session: qAsConst(sessions)) {
-        modMan.changeSessionFile(session);
-        do
-            TimeMachineObject::feedEventLoop();
-        while(!modMan.modulesReady());
-        QString actDevIface = modManSetupFacade.getStorageSystem()->getStoredValue(9999, "ACT_DEV_IFACE").toString();
-        if(actDevIface.isEmpty()) // we have to make module resilient to this situation
-            qFatal("ACT_DEV_IFACE empty - local modulemanager running???");
-
-        QString xmlFileName = QStringLiteral(HTML_DOCS_PATH) + "/scpi-xmls/" + session;
-        xmlFileName.replace("json", "xml");
-        QFile xmlFile(xmlFileName);
-        xmlFile.open(QFile::ReadWrite);
-        QTextStream data(&xmlFile);
-        data << actDevIface;
-    }
-    modMan.destroyModules();
-    do
-        TimeMachineObject::feedEventLoop();
-    while(!modMan.modulesReady());
-}
 
 int main(int argc, char *argv[])
 {
@@ -49,12 +14,14 @@ int main(int argc, char *argv[])
     parser.process(a);
     QString zenuxRelease = parser.value(zenuxVersion);
 
-    ModuleManagerSetupFacade::registerMetaTypeStreamOperators();
-    qputenv("QT_FATAL_CRITICALS", "1"); \
+    qputenv("QT_FATAL_CRITICALS", "1");
 
-    QDir().mkdir(QStringLiteral(HTML_DOCS_PATH) + "/scpi-xmls");
-    generateDevIfaceXmls("mt310s2");
-    generateDevIfaceXmls("com5003");
+    QString xmlDir = QStringLiteral(HTML_DOCS_PATH) + "/scpi-xmls/";
+    QDir().mkdir(xmlDir);
+
+    ScpiIfaceExportGenerator scpiIfaceExportGenerator;
+    scpiIfaceExportGenerator.getAllSessionsScpiIfaceXmls("mt310s2", xmlDir);
+    scpiIfaceExportGenerator.getAllSessionsScpiIfaceXmls("com5003", xmlDir);
 
     QProcess sh;
     sh.start("/bin/sh", QStringList() << QStringLiteral(SCPI_DOC_SOURCE_PATH) + "/xml-to-html/create-all-htmls" << QStringLiteral(HTML_DOCS_PATH) << zenuxRelease);
