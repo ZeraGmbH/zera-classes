c488ed4e69acf4f19bfe0b31c3c41d9adafd62a6
Andreas Müller <schnitzeltony@gmail.com>
2024-01-18 13:26:50 +0100

    Prepare mock facades for tests by feeding event loop on ctor/dtor
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modulemanager/lib/mockcom5003facade.h
+++ modulemanager/lib/mockcom5003facade.h
@@ -10,11 +10,12 @@ class MockCom5003Facade
 {
 public:
     MockCom5003Facade();
+    virtual ~MockCom5003Facade();
 private:
-    ResmanRunFacade m_resman;
-    MockCom5003dFull m_mockcom5003d;
-    MockSec1000d m_mocksec1000d;
-    MockZdsp1d m_mockzdsp1d;
+    ResmanRunFacade *m_resman;
+    MockCom5003dFull *m_mockcom5003d;
+    MockSec1000d *m_mocksec1000d;
+    MockZdsp1d *m_mockzdsp1d;
 };
 
 #endif // MOCKCOM5003FACADE_H
--- modulemanager/lib/mockmt310s2facade.h
+++ modulemanager/lib/mockmt310s2facade.h
@@ -10,11 +10,12 @@ class MockMt310s2Facade
 {
 public:
     MockMt310s2Facade();
+    virtual ~MockMt310s2Facade();
 private:
-    ResmanRunFacade m_resman;
-    MockMt310s2dFull m_mockmt310s2d;
-    MockSec1000d m_mocksec1000d;
-    MockZdsp1d m_mockzdsp1d;
+    ResmanRunFacade *m_resman;
+    MockMt310s2dFull *m_mockmt310s2d;
+    MockSec1000d *m_mocksec1000d;
+    MockZdsp1d *m_mockzdsp1d;
 };
 
 #endif // MOCKMT310S2FACADE_H
