205ec236d203b648b8d40290801e303370a92759
Andreas Müller <schnitzeltony@gmail.com>
2024-01-18 07:54:49 +0100

    modulemanager: Use full mock for com5003d
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modulemanager/lib/mockcom5003facade.h
+++ modulemanager/lib/mockcom5003facade.h
@@ -2,8 +2,9 @@
 #define MOCKCOM5003FACADE_H
 
 #include "resmanrunfacade.h"
-#include "mockcom5003d.h"
+#include "mockcom5003dfull.h"
 #include "mocksec1000d.h"
+#include "mockzdsp1d.h"
 
 class MockCom5003Facade
 {
@@ -11,8 +12,9 @@ public:
     MockCom5003Facade();
 private:
     ResmanRunFacade m_resman;
-    MockCom5003d m_mockcom5003d;
+    MockCom5003dFull m_mockcom5003d;
     MockSec1000d m_mocksec1000d;
+    MockZdsp1d m_mockzdsp1d;
 };
 
 #endif // MOCKCOM5003FACADE_H
