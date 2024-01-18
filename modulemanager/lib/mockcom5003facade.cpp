c48183956f4b412903e278b55370e61e026e6e79
Andreas Müller <schnitzeltony@gmail.com>
2024-01-18 13:26:50 +0100

    Prepare mock facades for tests by feeding event loop on ctor/dtor
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modulemanager/lib/mockcom5003facade.cpp
+++ modulemanager/lib/mockcom5003facade.cpp
@@ -1,5 +1,22 @@
 #include "mockcom5003facade.h"
+#include <demoeventloopfeeder.h>
 
 MockCom5003Facade::MockCom5003Facade()
 {
+    m_resman = new ResmanRunFacade;
+    DemoEventLoopFeeder::feedEventLoop();
+    m_mockcom5003d = new MockCom5003dFull;
+    m_mocksec1000d = new MockSec1000d;
+    m_mockzdsp1d = new MockZdsp1d;
+    DemoEventLoopFeeder::feedEventLoop();
+}
+
+MockCom5003Facade::~MockCom5003Facade()
+{
+    delete m_mockzdsp1d;
+    delete m_mocksec1000d;
+    delete m_mockcom5003d;
+    DemoEventLoopFeeder::feedEventLoop();
+    delete m_resman;
+    DemoEventLoopFeeder::feedEventLoop();
 }
--- modulemanager/lib/mockmt310s2facade.cpp
+++ modulemanager/lib/mockmt310s2facade.cpp
@@ -1,5 +1,22 @@
 #include "mockmt310s2facade.h"
+#include <demoeventloopfeeder.h>
 
 MockMt310s2Facade::MockMt310s2Facade()
 {
+    m_resman = new ResmanRunFacade;
+    DemoEventLoopFeeder::feedEventLoop();
+    m_mockmt310s2d = new MockMt310s2dFull;
+    m_mocksec1000d = new MockSec1000d;
+    m_mockzdsp1d = new MockZdsp1d;
+    DemoEventLoopFeeder::feedEventLoop();
+}
+
+MockMt310s2Facade::~MockMt310s2Facade()
+{
+    delete m_mockzdsp1d;
+    delete m_mocksec1000d;
+    delete m_mockmt310s2d;
+    DemoEventLoopFeeder::feedEventLoop();
+    delete m_resman;
+    DemoEventLoopFeeder::feedEventLoop();
 }
