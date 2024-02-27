4bd6503f08ae585a06c7bd0363100704a48c7b1b
Asmae Kaouissi <a.kaouissi@zera.de>
2024-02-27 14:51:26 +0100

    test_rms_module_regression: Make use of ModuleManagerTestRunner
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- modules/rmsmodule/tests/test_rms_module_regression.h
+++ modules/rmsmodule/tests/test_rms_module_regression.h
@@ -12,22 +12,12 @@ class test_rms_module_regression : public QObject
 {
     Q_OBJECT
 private slots:
-    void init();
-    void cleanup();
-
     void minimalSession();
     void moduleConfigFromResource();
     void veinDumpInitial();
     void checkActualValueCount();
     void injectActualValues();
     void injectActualTwice();
-private:
-    void setupServices(QString sessionFileName);
-
-    std::unique_ptr<LicenseSystemMock> m_licenseSystem;
-    std::unique_ptr<ModuleManagerSetupFacade> m_modmanFacade;
-    std::unique_ptr<TestModuleManager> m_modMan;
-    TestFactoryServiceInterfacesPtr m_serviceInterfaceFactory;
 };
 
 #endif // TEST_RMS_MODULE_REGRESSION_H
