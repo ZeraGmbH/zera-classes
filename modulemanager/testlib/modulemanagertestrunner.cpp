27a3b12ca276ceedec59c8459c596a7de7405b35
Andreas Müller <schnitzeltony@gmail.com>
2024-02-27 10:10:59 +0100

    Add ModuleManagerTestRunner - a convenience class to start services in tests
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ modulemanager/testlib/modulemanagertestrunner.cpp
@@ -0,0 +1,43 @@
+#include "modulemanagertestrunner.h"
+#include <timemachineobject.h>
+
+ModuleManagerTestRunner::ModuleManagerTestRunner(QString sessionFileName, QString deviceName)
+{
+    m_licenseSystem = std::make_unique<LicenseSystemMock>();
+    m_modmanFacade = std::make_unique<ModuleManagerSetupFacade>(m_licenseSystem.get());
+    m_serviceInterfaceFactory = std::make_shared<TestFactoryServiceInterfaces>();
+    m_modMan = std::make_unique<TestModuleManager>(m_modmanFacade.get(), m_serviceInterfaceFactory, true);
+    m_modMan->loadAllAvailableModulePlugins();
+    m_modMan->setupConnections();
+    m_modMan->startAllServiceMocks(deviceName);
+    m_modMan->loadSession(sessionFileName);
+    m_modMan->waitUntilModulesAreReady();
+    m_vfCmdEventHandlerSystem = std::make_shared<VfCmdEventHandlerSystem>();
+    m_modmanFacade->addSubsystem(m_vfCmdEventHandlerSystem.get());
+}
+
+ModuleManagerTestRunner::~ModuleManagerTestRunner()
+{
+    if(m_modMan)
+        m_modMan->destroyModulesAndWaitUntilAllShutdown();
+    m_modMan = nullptr;
+    TimeMachineObject::feedEventLoop();
+    m_serviceInterfaceFactory = nullptr;
+    m_modmanFacade = nullptr;
+    m_licenseSystem = nullptr;
+}
+
+VeinStorage::VeinHash *ModuleManagerTestRunner::getVeinStorageSystem()
+{
+    return m_modmanFacade->getStorageSystem();
+}
+
+const QList<TestDspInterfacePtr> &ModuleManagerTestRunner::getDspInterfaceList() const
+{
+    return m_serviceInterfaceFactory->getInterfaceList();
+}
+
+VfCmdEventHandlerSystemPtr ModuleManagerTestRunner::getVfCmdEventHandlerSystemPtr()
+{
+    return m_vfCmdEventHandlerSystem;
+}
