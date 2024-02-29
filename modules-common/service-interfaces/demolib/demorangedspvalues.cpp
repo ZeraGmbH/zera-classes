20cb55c9d69d991ee8e4cdb4df24ff7b42443e94
Andreas Müller <schnitzeltony@gmail.com>
2024-02-29 22:09:18 +0100

    Make DemoRangeDspValues more convenient
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/service-interfaces/demolib/demorangedspvalues.cpp
+++ modules-common/service-interfaces/demolib/demorangedspvalues.cpp
@@ -2,33 +2,28 @@
 #include <cmath>
 
 DemoRangeDspValues::DemoRangeDspValues(int channels) :
-    m_channels(channels)
+    m_channels(channels),
+    m_peakValues(channels),
+    m_rmsValues(channels),
+    m_freqValue(1),
+    m_dcValues(channels)
 {
 }
 
-void DemoRangeDspValues::setValue(QVector<float> randomChannelRMS, double frequency)
+void DemoRangeDspValues::setRmsValue(int channel, float rmsValue)
 {
-    m_demoRangeValues.clear();
-    // peak
-    for (int channel=0; channel<m_channels; ++channel) {
-        double randPeak = randomChannelRMS[channel] * M_SQRT2;
-        m_demoRangeValues.append(randPeak);
-    }
-    // RMS
-    for (int channel=0; channel<m_channels; ++channel) {
-        double randRms = randomChannelRMS[channel];
-        m_demoRangeValues.append(randRms);
-    }
-    // frequency
-    m_demoRangeValues.append(frequency);
-    // peak DC (no DC for now)
-    for (int channel=0; channel<m_channels; ++channel) {
-        double randPeak = randomChannelRMS[channel] * M_SQRT2;
-        m_demoRangeValues.append(randPeak);
-    }
+    float peakValue = rmsValue * M_SQRT2;
+    m_peakValues[channel] = peakValue;
+    m_rmsValues[channel] = rmsValue;
+    m_dcValues[channel] = peakValue;
+}
+
+void DemoRangeDspValues::setFrequency(float frequency)
+{
+    m_freqValue[0] = frequency;
 }
 
 QVector<float> DemoRangeDspValues::getDspValues()
 {
-    return m_demoRangeValues;
+    return m_peakValues + m_rmsValues + m_freqValue + m_dcValues;
 }
--- modules-common/service-interfaces/demolib/demorangedspvalues.h
+++ modules-common/service-interfaces/demolib/demorangedspvalues.h
@@ -7,11 +7,15 @@ class DemoRangeDspValues
 {
 public:
     DemoRangeDspValues(int channels);
-    void setValue(QVector<float> randomChannelRMS, double frequency);
+    void setRmsValue(int channel, float rmsValue);
+    void setFrequency(float frequency);
     QVector<float> getDspValues();
 private:
     int m_channels;
-    QVector<float> m_demoRangeValues;
+    QVector<float> m_peakValues;
+    QVector<float> m_rmsValues;
+    QVector<float> m_freqValue;
+    QVector<float> m_dcValues;
 };
 
 #endif // DEMORANGEDSPVALUES_H
