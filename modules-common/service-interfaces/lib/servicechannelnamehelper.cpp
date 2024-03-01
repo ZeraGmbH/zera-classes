976e55c998b4ac5e9cd5e2dbc4c3e4f62cf16855
Andreas Müller <schnitzeltony@gmail.com>
2024-03-01 19:57:25 +0100

    ServiceChannelNameHelper: Rework so we know voltage/current channels
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/service-interfaces/lib/servicechannelnamehelper.cpp
+++ modules-common/service-interfaces/lib/servicechannelnamehelper.cpp
@@ -1,14 +1,26 @@
 #include "servicechannelnamehelper.h"
 #include <QStringList>
 
+QStringList ServiceChannelNameHelper::getVoltageChannelNamesUsed(bool phaseSeqenceSwapped)
+{
+    if(phaseSeqenceSwapped)
+        return QStringList() << "m0" << "m2" << "m1" << "m6";
+    return QStringList() << "m0" << "m1" << "m2" << "m6";
+}
+
+QStringList ServiceChannelNameHelper::getCurrentChannelNamesUsed(bool phaseSeqenceSwapped)
+{
+    if(phaseSeqenceSwapped)
+        return QStringList() << "m3" << "m5" << "m6" << "m7";
+    return QStringList() << "m3" << "m4" << "m5" << "m7";
+}
+
 bool ServiceChannelNameHelper::isPhaseNeutralVoltage(QString valueChannelName)
 {
-    QStringList voltagePhases = QStringList() << "m0" << "m1" << "m2" << "m6";
-    return voltagePhases.contains(valueChannelName);
+    return getVoltageChannelNamesUsed().contains(valueChannelName);
 }
 
 bool ServiceChannelNameHelper::isCurrent(QString valueChannelName)
 {
-    QStringList currentPhases = QStringList() << "m3" << "m4" << "m5" << "m7";
-    return currentPhases.contains(valueChannelName);
+    return getCurrentChannelNamesUsed().contains(valueChannelName);
 }
