6753737144d98ee68709d49633e7ad626ff4459d
Andreas Müller <schnitzeltony@gmail.com>
2024-03-01 19:57:25 +0100

    ServiceChannelNameHelper: Rework so we know voltage/current channels
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/service-interfaces/lib/servicechannelnamehelper.h
+++ modules-common/service-interfaces/lib/servicechannelnamehelper.h
@@ -6,6 +6,8 @@
 class ServiceChannelNameHelper
 {
 public:
+    static QStringList getVoltageChannelNamesUsed(bool phaseSeqenceSwapped = false);
+    static QStringList getCurrentChannelNamesUsed(bool phaseSeqenceSwapped = false);
     static bool isPhaseNeutralVoltage(QString valueChannelName);
     static bool isCurrent(QString valueChannelName);
 };
