2b8d7ae3efcbc972a64f1df2ee1cb533d82e20d6
Andreas Müller <schnitzeltony@gmail.com>
2024-03-04 17:28:23 +0100

    test_adj_module_regression: Add veinDumpInitial test case
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/adjustmentmodule/tests/test_adj_module_regression.cpp
+++ modules/adjustmentmodule/tests/test_adj_module_regression.cpp
@@ -1,6 +1,7 @@
 #include "test_adj_module_regression.h"
 #include "modulemanagertestrunner.h"
 #include "vs_veinhash.h"
+#include <QBuffer>
 #include <QTest>
 
 QTEST_MAIN(test_adj_module_regression)
@@ -10,6 +11,9 @@ static int constexpr rmsEntityId = 1040;
 static int constexpr dftEntityId = 1050;
 static int constexpr fftEntityId = 1060;
 static int constexpr adjEntityId = 1190;
+const QList<int> test_adj_module_regression::m_entityIdList =
+    QList<int>() << rangeEntityId << rmsEntityId << dftEntityId << fftEntityId << adjEntityId;
+
 
 void test_adj_module_regression::minimalSession()
 {
@@ -22,4 +26,25 @@ void test_adj_module_regression::minimalSession()
     QVERIFY(storageHash->hasEntity(dftEntityId));
     QVERIFY(storageHash->hasEntity(fftEntityId));
     QVERIFY(storageHash->hasEntity(adjEntityId));
+}
+
+void test_adj_module_regression::veinDumpInitial()
+{
+    QFile file(":/dumpInitial.json");
+    QVERIFY(file.open(QFile::ReadOnly));
+    QString jsonExpected = file.readAll();
+
+    ModuleManagerTestRunner testRunner(":/session-minimal.json");
+    VeinStorage::VeinHash* storageHash = testRunner.getVeinStorageSystem();
+    QByteArray jsonDumped;
+    QBuffer buff(&jsonDumped);
+    storageHash->dumpToFile(&buff, m_entityIdList);
+
+    if(jsonExpected != jsonDumped) {
+        qWarning("Expected storage hash:");
+        qInfo("%s", qPrintable(jsonExpected));
+        qWarning("Dumped storage hash:");
+        qInfo("%s", qPrintable(jsonDumped));
+        QCOMPARE(jsonExpected, jsonDumped);
+    }
 }
--- modules/adjustmentmodule/tests/test_adj_module_regression.h
+++ modules/adjustmentmodule/tests/test_adj_module_regression.h
@@ -8,6 +8,9 @@ class test_adj_module_regression : public QObject
     Q_OBJECT
 private slots:
     void minimalSession();
+    void veinDumpInitial();
+private:
+    static const QList<int> m_entityIdList;
 };
 
 #endif // TEST_ADJ_MODULE_REGRESSION_H
