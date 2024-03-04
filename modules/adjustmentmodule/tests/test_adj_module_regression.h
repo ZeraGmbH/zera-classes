b18c00e24042a8645cbabf7ccfe5ca7e72c7c2ac
Andreas Müller <schnitzeltony@gmail.com>
2024-03-04 17:12:06 +0100

    adjustmentmodule: Introduce the first regression test
    This was a bit tricky because tests for adjustment module are the first to rely
    on modules other than adjustment module.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ modules/adjustmentmodule/tests/test_adj_module_regression.h
@@ -0,0 +1,13 @@
+#ifndef TEST_ADJ_MODULE_REGRESSION_H
+#define TEST_ADJ_MODULE_REGRESSION_H
+
+#include <QObject>
+
+class test_adj_module_regression : public QObject
+{
+    Q_OBJECT
+private slots:
+    void minimalSession();
+};
+
+#endif // TEST_ADJ_MODULE_REGRESSION_H
