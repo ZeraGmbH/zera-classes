e5ebc46ca6213eea1baeb5f6b2fc3b00bedc7ba7
Asmae Kaouissi <a.kaouissi@zera.de>
2024-03-08 15:15:59 +0100

    Introduce test_power1_module_regression
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

---
+++ modules/power1module/tests/test_power1_module_regression.h
@@ -0,0 +1,17 @@
+#ifndef TEST_POWER1_MODULE_REGRESSION_H
+#define TEST_POWER1_MODULE_REGRESSION_H
+
+#include <QObject>
+
+class test_power1_module_regression : public QObject
+{
+    Q_OBJECT
+private slots:
+    void minimalSession();
+    void moduleConfigFromResource();
+    void veinDumpInitial();
+    void injectActualValues();
+
+};
+
+#endif // TEST_POWER1_MODULE_REGRESSION_H
