da3d78b36c57ee6a63b1cdac09ae8dc149cdaa31
Andreas Müller <schnitzeltony@gmail.com>
2024-03-11 18:00:21 +0100

    adjustmentmodule: Deny adjustment gain values that differ more than 10%
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/adjustmentmodule/tests/test_adj_module_gain.cpp
+++ modules/adjustmentmodule/tests/test_adj_module_gain.cpp
@@ -1,9 +1,17 @@
 #include "test_adj_module_gain.h"
-#include "modulemanagertestrunner.h"
+#include "adjustmentmodule.h"
+#include "adjustmentmodulemeasprogram.h"
+#include "demovaluesdspadjustment.h"
 #include <scpimoduleclientblocked.h>
 #include <QTest>
 
 QTEST_MAIN(test_adj_module_gain)
+
+constexpr double testvoltage = 100;
+constexpr double testcurrent = 10;
+constexpr double testangle = 0;
+constexpr double testfrequency = 50;
+constexpr double limitOffset = 0.1;
 
 void test_adj_module_gain::noActValuesWithPermission()
 {
@@ -12,14 +20,85 @@ void test_adj_module_gain::noActValuesWithPermission()
     ScpiModuleClientBlocked scpiClient;
     // Init is missing by intention
     QString response = scpiClient.sendReceive("calc:adj1:ampl UL1,250V,199.9893000801;|*stb?");
-    QCOMPARE(response, "+0"); // Without sending values we get 0.0 which is not invalid
+    QCOMPARE(response, "+4");
 }
 
-void test_adj_module_gain::noActValuesWithoutPermission()
+void test_adj_module_gain::validActValuesWithPermission()
 {
-    ModuleManagerTestRunner testRunner(":/session-minimal.json", false);
+    ModuleManagerTestRunner testRunner(":/session-minimal.json", true);
+    setActualTestValues(testRunner);
 
     ScpiModuleClientBlocked scpiClient;
-    QString response = scpiClient.sendReceive("calc:adj1:ampl UL1,250V,199.9893000801;|*stb?");
+    QString response = scpiClient.sendReceive("calc:adj1:ampl UL1,250V,100;|*stb?");
+    QCOMPARE(response, "+0");
+}
+
+void test_adj_module_gain::validActValuesWithoutPermission()
+{
+    ModuleManagerTestRunner testRunner(":/session-minimal.json", false);
+    setActualTestValues(testRunner);
+
+    ScpiModuleClientBlocked scpiClient;
+    QString response = scpiClient.sendReceive("calc:adj1:ampl UL1,250V,100;|*stb?");
     QCOMPARE(response, "+4");
+}
+
+void test_adj_module_gain::outOfLimitLower()
+{
+    ModuleManagerTestRunner testRunner(":/session-minimal.json", true);
+    setActualTestValues(testRunner);
+
+    double adjRefVoltage = testvoltage * (1-maxAmplitudeErrorPercent/100) - limitOffset;
+    QByteArray send = QString("calc:adj1:ampl UL1,250V,%1;|*stb?").arg(adjRefVoltage).toLatin1();
+    ScpiModuleClientBlocked scpiClient;
+    QString response = scpiClient.sendReceive(send);
+    QCOMPARE(response, "+4");
+}
+
+void test_adj_module_gain::outOfLimitUpper()
+{
+    ModuleManagerTestRunner testRunner(":/session-minimal.json", true);
+    setActualTestValues(testRunner);
+
+    double adjRefVoltage = testvoltage * (1+maxAmplitudeErrorPercent/100) + limitOffset;
+    QByteArray send = QString("calc:adj1:ampl UL1,250V,%1;|*stb?").arg(adjRefVoltage).toLatin1();
+    ScpiModuleClientBlocked scpiClient;
+    QString response = scpiClient.sendReceive(send);
+    QCOMPARE(response, "+4");
+}
+
+void test_adj_module_gain::withinLimitLower()
+{
+    ModuleManagerTestRunner testRunner(":/session-minimal.json", true);
+    setActualTestValues(testRunner);
+
+    double adjRefVoltage = testvoltage * (1-maxAmplitudeErrorPercent/100) + limitOffset;
+    QByteArray send = QString("calc:adj1:ampl UL1,250V,%1;|*stb?").arg(adjRefVoltage).toLatin1();
+    ScpiModuleClientBlocked scpiClient;
+    QString response = scpiClient.sendReceive(send);
+    QCOMPARE(response, "+0");
+}
+
+void test_adj_module_gain::withinLimitUpper()
+{
+    ModuleManagerTestRunner testRunner(":/session-minimal.json", true);
+    setActualTestValues(testRunner);
+
+    double adjRefVoltage = testvoltage * (1+maxAmplitudeErrorPercent/100) - limitOffset;
+    QByteArray send = QString("calc:adj1:ampl UL1,250V,%1;|*stb?").arg(adjRefVoltage).toLatin1();
+    ScpiModuleClientBlocked scpiClient;
+    QString response = scpiClient.sendReceive(send);
+    QCOMPARE(response, "+0");
+}
+
+void test_adj_module_gain::setActualTestValues(ModuleManagerTestRunner &testRunner)
+{
+    const QList<TestDspInterfacePtr>& dspInterfaces = testRunner.getDspInterfaceList();
+    DemoValuesDspAdjustment dspValues(dspInterfaces[DSP_INTERFACE_DFT]->getValueList());
+    dspValues.setAllValuesSymmetric(testvoltage, testcurrent, testangle, testfrequency);
+    dspValues.fireActualValues(
+        dspInterfaces[DSP_INTERFACE_DFT],
+        dspInterfaces[DSP_INTERFACE_FFT],
+        dspInterfaces[DSP_INTERFACE_RANGE_PROGRAM], // Range is for frequency only
+        dspInterfaces[DSP_INTERFACE_RMS]);
 }
