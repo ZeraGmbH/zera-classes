d0da2a3d8365c866fcca6343f8fd5ce196698ff2
Asmae Kaouissi <a.kaouissi@zera.de>
2024-03-21 14:27:35 +0100

    movingwindowfilter: Use TimerTemplateQtPtr for m_integrationTimer
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- modules-common/zera-basemodule/tests/test_movingwindowfilter.cpp
+++ modules-common/zera-basemodule/tests/test_movingwindowfilter.cpp
@@ -1,15 +1,21 @@
 #include "test_movingwindowfilter.h"
 #include "movingwindowfilterwithoutsumfifo.h"
 #include <timemachineobject.h>
+#include <timemachinefortest.h>
+#include <timerfactoryqtfortest.h>
 #include <qtest.h>
 #include <QSignalSpy>
 
 QTEST_MAIN(test_movingwindowfilter)
 
-void test_movingwindowfilter::init()
+void test_movingwindowfilter::initTestCase()
 {
-    if(!m_filteredValues.empty())
-        m_filteredValues.clear();
+    TimerFactoryQtForTest::enableTest();
+}
+
+void test_movingwindowfilter::cleanup()
+{
+    m_filteredValues.clear();
 }
 
 void test_movingwindowfilter::threeSetOfActualValuesWithoutIntegrationTimeElapsed()
@@ -39,6 +45,36 @@ void test_movingwindowfilter::threeSetOfActualValuesWithoutIntegrationTimeElapse
     TimeMachineObject::feedEventLoop();
     for(int i = 0; i < samples1.count(); i++)
         averageSamples[i] = (static_cast<double>(samples1[i]) + static_cast<double>(samples2[i]) + static_cast<double>(samples3[i])) / 3;
+    QCOMPARE(averageSamples, m_filteredValues);
+}
+
+void test_movingwindowfilter::threeSetOfActualValuesWithIntegrationTimeElapsed()
+{
+    MovingWindowFilterWithoutSumFifo filter(1.0);
+    TimeMachineObject::feedEventLoop();
+
+    QVector<float> samples1{1.0, 2.0, 3.0};
+    QVector<float> samples2{4.0, 5.0, 6.0};
+    QVector<float> samples3{7.0, 8.0, 9.0};
+    QVector<float> averageSamples(samples1.count());
+
+    connect(&filter, &MovingWindowFilterWithoutSumFifo::actualValues, this, &test_movingwindowfilter::receiveFilteredValues);
+
+    filter.receiveActualValues(&samples1);
+    TimeMachineObject::feedEventLoop();
+    filter.receiveActualValues(&samples2);
+    TimeMachineObject::feedEventLoop();
+
+    for(int i = 0; i < samples1.count(); i++)
+        //cast 'samples' into 'double' to match with steps & accuracy from MovingWindowFilterWithoutSumFifo calculations
+        averageSamples[i] = (static_cast<double>(samples1[i]) + static_cast<double>(samples2[i])) / 2;
+    QCOMPARE(averageSamples, m_filteredValues);
+
+    TimeMachineForTest::getInstance()->processTimers(1000);
+    filter.receiveActualValues(&samples3);
+    TimeMachineObject::feedEventLoop();
+    for(int i = 0; i < samples1.count(); i++)
+        averageSamples[i] = (static_cast<double>(samples2[i]) + static_cast<double>(samples3[i])) / 2;
     QCOMPARE(averageSamples, m_filteredValues);
 }
 
--- modules-common/zera-basemodule/tests/test_movingwindowfilter.h
+++ modules-common/zera-basemodule/tests/test_movingwindowfilter.h
@@ -7,8 +7,10 @@ class test_movingwindowfilter : public QObject
 {
     Q_OBJECT
 private slots:
-    void init();
+    void initTestCase();
+    void cleanup();
     void threeSetOfActualValuesWithoutIntegrationTimeElapsed();
+    void threeSetOfActualValuesWithIntegrationTimeElapsed();
 private:
     void receiveFilteredValues(QVector<float> *filteredValues);
     QVector<float> m_filteredValues;
