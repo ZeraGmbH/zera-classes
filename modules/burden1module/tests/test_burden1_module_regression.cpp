25240e9a7a6c9e88107d46b1ea7c77149e8c49c8
Andreas Müller <schnitzeltony@gmail.com>
2024-03-22 18:20:32 +0100

    test_burden1_module_regression: Add tests on value calculation
    Doing them that late is kind of monkey work...
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/burden1module/tests/test_burden1_module_regression.cpp
+++ modules/burden1module/tests/test_burden1_module_regression.cpp
@@ -1,6 +1,8 @@
 #include "test_burden1_module_regression.h"
+#include "demovaluesdspdft.h"
 #include "modulemanagertestrunner.h"
 #include "burden1measdelegate.h"
+#include <timemachineobject.h>
 #include <QBuffer>
 #include <QTest>
 
@@ -55,4 +57,106 @@ void test_burden1_module_regression::resistanceCalculation()
     crossSection = 10.0;
     expected = length / (crossSection * conductanceCopper);
     QCOMPARE(BURDEN1MODULE::cBurden1MeasDelegate::calcWireResistence(length, crossSection), expected);
+}
+
+void test_burden1_module_regression::voltageBurden230V1ADefaultSettings()
+{
+    ModuleManagerTestRunner testRunner(":/session-minimal.json");
+
+    const QList<TestDspInterfacePtr>& dspInterfaces = testRunner.getDspInterfaceList();
+    QCOMPARE(dspInterfaces.count(), 1);
+    QStringList valueList = dspInterfaces[0]->getValueList();
+    DemoValuesDspDft dspValues(valueList, 1);
+    constexpr double voltage = 230;
+    constexpr double current = 5;
+    constexpr double angle = 0;
+    dspValues.setAllValuesSymmetric(voltage, current, angle);
+    dspInterfaces[0]->fireActValInterrupt(dspValues.getDspValues(), 0);
+    TimeMachineObject::feedEventLoop();
+
+    VeinStorage::VeinHash* storageHash = testRunner.getVeinStorageSystem();
+
+    QVariant burden;
+    burden = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Burden1");
+    QCOMPARE(burden.toFloat(), calcUBurdenSb(voltage, current, 1, 1, 1));
+    burden = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Burden2");
+    QCOMPARE(burden.toFloat(), calcUBurdenSb(voltage, current, 1, 1, 1));
+    burden = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Burden3");
+    QCOMPARE(burden.toFloat(), calcUBurdenSb(voltage, current, 1, 1, 1));
+
+    QVariant burdenRel;
+    burdenRel = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Ratio1");
+    QCOMPARE(burdenRel.toFloat(), calcUBurdenSn(1, voltage, current, 1, 1, 1));
+    burdenRel = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Ratio2");
+    QCOMPARE(burdenRel.toFloat(), calcUBurdenSn(1, voltage, current, 1, 1, 1));
+    burdenRel = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Ratio3");
+    QCOMPARE(burdenRel.toFloat(), calcUBurdenSn(1, voltage, current, 1, 1, 1));
+
+    QVariant calcAngle;
+    calcAngle = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_PFactor1");
+    QCOMPARE(calcAngle.toFloat(), 1.0);
+    calcAngle = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_PFactor2");
+    QCOMPARE(calcAngle.toFloat(), 1.0);
+    calcAngle = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_PFactor3");
+    QCOMPARE(calcAngle.toFloat(), 1.0);
+}
+
+void test_burden1_module_regression::voltageBurden100V1A60DefaultSettings()
+{
+    ModuleManagerTestRunner testRunner(":/session-minimal.json");
+
+    const QList<TestDspInterfacePtr>& dspInterfaces = testRunner.getDspInterfaceList();
+    QStringList valueList = dspInterfaces[0]->getValueList();
+    DemoValuesDspDft dspValues(valueList, 1);
+    constexpr double voltage = 100;
+    constexpr double current = 1;
+    constexpr double angle = 60;
+    dspValues.setAllValuesSymmetric(voltage, current, angle);
+    dspInterfaces[0]->fireActValInterrupt(dspValues.getDspValues(), 0);
+    TimeMachineObject::feedEventLoop();
+
+    VeinStorage::VeinHash* storageHash = testRunner.getVeinStorageSystem();
+
+    QVariant burden;
+    burden = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Burden1");
+    QCOMPARE(burden.toFloat(), calcUBurdenSb(voltage, current, 1, 1, 1));
+    burden = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Burden2");
+    QCOMPARE(burden.toFloat(), calcUBurdenSb(voltage, current, 1, 1, 1));
+    burden = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Burden3");
+    QCOMPARE(burden.toFloat(), calcUBurdenSb(voltage, current, 1, 1, 1));
+
+    QVariant burdenRel;
+    burdenRel = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Ratio1");
+    QCOMPARE(burdenRel.toFloat(), calcUBurdenSn(1, voltage, current, 1, 1, 1));
+    burdenRel = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Ratio2");
+    QCOMPARE(burdenRel.toFloat(), calcUBurdenSn(1, voltage, current, 1, 1, 1));
+    burdenRel = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_Ratio3");
+    QCOMPARE(burdenRel.toFloat(), calcUBurdenSn(1, voltage, current, 1, 1, 1));
+
+    QVariant calcAngle;
+    calcAngle = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_PFactor1");
+    QCOMPARE(calcAngle.toFloat(), calcCos(angle));
+    calcAngle = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_PFactor2");
+    QCOMPARE(calcAngle.toFloat(), calcCos(angle));
+    calcAngle = storageHash->getStoredValue(dftBurdenVoltageId, "ACT_PFactor3");
+    QCOMPARE(calcAngle.toFloat(), calcCos(angle));
+}
+
+float test_burden1_module_regression::calcUBurdenSb(double voltage, double current, double nominalVoltage, double wireLen, double crossSection)
+{
+    double cableResistance = BURDEN1MODULE::cBurden1MeasDelegate::calcWireResistence(wireLen, crossSection);
+    double nominator = current * nominalVoltage * nominalVoltage;
+    double denominator = voltage + (current * cableResistance);
+    return nominator / denominator;
+}
+
+float test_burden1_module_regression::calcUBurdenSn(double nominalBurden, double voltage, double current, double nominalVoltage, double wireLen, double crossSection)
+{
+    double burden = calcUBurdenSb(voltage, current, nominalVoltage, wireLen, crossSection);
+    return burden * 100.0 / nominalBurden;
+}
+
+float test_burden1_module_regression::calcCos(double angle)
+{
+    return cos(angle * M_PI / 180.0);
 }
--- modules/burden1module/tests/test_burden1_module_regression.h
+++ modules/burden1module/tests/test_burden1_module_regression.h
@@ -11,6 +11,14 @@ private slots:
     void veinDumpInitial();
 
     void resistanceCalculation();
+
+    void voltageBurden230V1ADefaultSettings();
+    void voltageBurden100V1A60DefaultSettings();
+
+private:
+    float calcUBurdenSb(double voltage, double current, double nominalVoltage, double wireLen, double crossSection);
+    float calcUBurdenSn(double nominalBurden, double voltage, double current, double nominalVoltage, double wireLen, double crossSection);
+    float calcCos(double angle);
 };
 
 #endif // TEST_BURDEN1_MODULE_REGRESSION_H
