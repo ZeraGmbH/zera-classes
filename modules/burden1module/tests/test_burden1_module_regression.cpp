843fd8a957e12a73560f63475a33cc2e23f1d854
Andreas Müller <schnitzeltony@gmail.com>
2024-03-22 15:31:48 +0100

    test_burden1_module_regression: Choose better entity id constant names
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/burden1module/tests/test_burden1_module_regression.cpp
+++ modules/burden1module/tests/test_burden1_module_regression.cpp
@@ -7,8 +7,8 @@
 QTEST_MAIN(test_burden1_module_regression)
 
 static int constexpr dftEntityId = 1050;
-static int constexpr dftBurden1Id = 1160;
-static int constexpr dftBurden2Id = 1161;
+static int constexpr dftBurdenCurrentId = 1160;
+static int constexpr dftBurdenVoltageId = 1161;
 
 void test_burden1_module_regression::minimalSession()
 {
@@ -17,8 +17,8 @@ void test_burden1_module_regression::minimalSession()
     QList<int> entityList = storageHash->getEntityList();
     QCOMPARE(entityList.count(), 4);
     QVERIFY(storageHash->hasEntity(dftEntityId));
-    QVERIFY(storageHash->hasEntity(dftBurden1Id));
-    QVERIFY(storageHash->hasEntity(dftBurden2Id));
+    QVERIFY(storageHash->hasEntity(dftBurdenCurrentId));
+    QVERIFY(storageHash->hasEntity(dftBurdenVoltageId));
 }
 
 void test_burden1_module_regression::veinDumpInitial()
@@ -31,7 +31,7 @@ void test_burden1_module_regression::veinDumpInitial()
     VeinStorage::VeinHash* storageHash = testRunner.getVeinStorageSystem();
     QByteArray jsonDumped;
     QBuffer buff(&jsonDumped);
-    storageHash->dumpToFile(&buff, QList<int>() << dftBurden1Id << dftBurden2Id);
+    storageHash->dumpToFile(&buff, QList<int>() << dftBurdenCurrentId << dftBurdenVoltageId);
 
     if(jsonExpected != jsonDumped) {
         qWarning("Expected storage hash:");
