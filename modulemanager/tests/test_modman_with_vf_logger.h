aa70705446799488f12bcb03b88e00df78c6358b
Andreas Müller <schnitzeltony@gmail.com>
2024-04-10 10:23:47 +0200

    test_modman_with_vf_logger: Refactor for multiple test cases
    This is much cleaner than the production code but we keep that untouched -
    our target is getting rid of vf-script/vf-logger crap
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modulemanager/tests/test_modman_with_vf_logger.h
+++ modulemanager/tests/test_modman_with_vf_logger.h
@@ -1,14 +1,33 @@
 #ifndef TEST_MODMAN_WITH_VF_LOGGER_H
 #define TEST_MODMAN_WITH_VF_LOGGER_H
 
+#include "modulemanagertestrunner.h"
+#include "veinqml.h"
+#include "vsc_scriptsystem.h"
+#include "zeradblogger.h"
 #include <QObject>
+#include <memory>
 
 class test_modman_with_vf_logger : public QObject
 {
     Q_OBJECT
 private slots:
     void basicCheckRmsModule();
-    void setupSytemWithLogger();
+    void checkEntitiesCreated();
+    void checkLoggerComponentValues();
+    void init();
+    void cleanup();
+private:
+    void onVfQmlStateChanged(VeinApiQml::VeinQml::ConnectionState t_state);
+    void onSerialNoLicensed();
+    void createModmanWithLogger();
+    void startModman();
+    std::unique_ptr<ModuleManagerTestRunner> m_testRunner;
+    std::unique_ptr<VeinScript::ScriptSystem> m_scriptSystem;
+    std::unique_ptr<VeinApiQml::VeinQml> m_qmlSystem;
+    std::unique_ptr<ZeraDBLogger> m_dataLoggerSystem;
+    bool m_initQmlSystemOnce = false;
+    bool m_dataLoggerSystemInitialized = false;
 };
 
 #endif // TEST_MODMAN_WITH_VF_LOGGER_H
