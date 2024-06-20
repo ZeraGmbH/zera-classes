bfa571a8c0f2cb476c79250d66ec8d64e9cbb1ce
Asmae Kaouissi <a.kaouissi@zera.de>
2024-06-20 11:52:13 +0200

    veindatacollector: convert values to QJsonObject
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- modules-common/vf-moduleinterface/veindatacollector.h
+++ modules-common/vf-moduleinterface/veindatacollector.h
@@ -1,9 +1,10 @@
 #ifndef VEINDATACOLLECTOR_H
 #define VEINDATACOLLECTOR_H
 
-#include <QVariant>
-#include <QObject>
 #include <ve_storagesystem.h>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QObject>
 
 class VeinDataCollector : public QObject
 {
@@ -13,13 +14,19 @@ public:
     void collectRMSValues();
     void collectPowerValuesForEmobAC();
     void collectPowerValuesForEmobDC();
-
     QHash<QString, QList<QVariant>> getStoredValuesOfEntity(int entityId);
+
+signals:
+    void newStoredValue(QJsonObject jsonObject);
 
 private slots:
     void appendValue(int entityId, QString componentName, QVariant value);
 
 private:
+    void convertToJson();
+    QJsonArray convertListToJsonArray(QList<QVariant> list);
+    QJsonObject convertHashToJsonObject(QHash<QString, QList<QVariant> > hash);
+
     VeinEvent::StorageSystem* m_storage;
     QHash<int , QHash<QString, QList<QVariant>> > m_veinValuesHash;
 };
