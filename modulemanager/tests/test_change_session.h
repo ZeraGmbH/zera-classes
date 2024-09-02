3a0dbe4b3460e13c0b2e46168bdba7f16d23eb5c
Asmae Kaouissi <a.kaouissi@zera.de>
2024-09-02 17:00:17 +0200

    add test_change_session
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

---
+++ modulemanager/tests/test_change_session.h
@@ -0,0 +1,22 @@
+#ifndef TEST_CHANGE_SESSION_H
+#define TEST_CHANGE_SESSION_H
+
+#include "testmodulemanager.h"
+#include <QObject>
+
+class test_change_session : public QObject
+{
+    Q_OBJECT
+private slots:
+    void initTestCase();
+    void changeSessionMt310s2FromComponent();
+    void changeSessionMt310s2SCPICmd();
+
+    void changeSessionCom5003FromComponent();
+    void changeSessionCom5003SCPICmd();
+
+private:
+    AbstractFactoryServiceInterfacesPtr m_serviceInterfaceFactory;
+};
+
+#endif // TEST_CHANGE_SESSION_H
