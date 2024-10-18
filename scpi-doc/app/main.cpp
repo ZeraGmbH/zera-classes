159679b960f49761bac797980f835168a189c087
amutalik <a.mutalik@zera.de>
2024-10-18 16:35:28 +0200

    Add scpi-doc/xml-to-html/create-html, to create one html at a time
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- scpi-doc/app/main.cpp
+++ scpi-doc/app/main.cpp
@@ -7,6 +7,21 @@
 #include <QProcess>
 
 
+void createHtml(QString zenuxRelease, QFileInfo sessionXml, QString adjustment, QString htmlPath)
+{
+    QProcess sh;
+    QStringList paramList;
+    paramList = QStringList()
+                << QStringLiteral(SCPI_DOC_SOURCE_PATH) + "/create-html"
+                << zenuxRelease
+                << sessionXml.filePath()
+                << adjustment
+                << htmlPath;
+    sh.start("/bin/sh", paramList);
+    sh.waitForFinished();
+    printf("%s", qPrintable(sh.readAll()));
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -18,23 +33,33 @@ int main(int argc, char *argv[])
 
     qputenv("QT_FATAL_CRITICALS", "1");
 
-    QString xmlDir = QStringLiteral(HTML_DOCS_PATH) + "scpi-xmls/";
-    QDir().mkdir(xmlDir);
-
-    ScpiIfaceExportGenerator scpiIfaceExportGenerator;
-    scpiIfaceExportGenerator.getAllSessionsScpiIfaceXmls("mt310s2", xmlDir);
-    scpiIfaceExportGenerator.getAllSessionsScpiIfaceXmls("com5003", xmlDir);
-
     QJsonObject mappedJson = SessionNamesMappingJson::createSessionNamesMappingJsonAllDevices(ModulemanagerConfig::getConfigFileNameFull());
     if(!cJsonFileLoader::storeJsonFile(QStringLiteral(HTML_DOCS_PATH) + "SessionNamesMapping.json", mappedJson))
         qFatal("Session names mapping json file could not be created!");
 
-    QProcess sh;
-    QStringList paramList = QStringList() << QStringLiteral(SCPI_DOC_SOURCE_PATH) + "/create-all-htmls" << QStringLiteral(HTML_DOCS_PATH) << zenuxRelease;
-    sh.start("/bin/sh", paramList);
-    sh.waitForFinished();
-    printf("%s", qPrintable(sh.readAll()));
+    QString xmlDirPath = QStringLiteral(HTML_DOCS_PATH) + "scpi-xmls/";
+    QDir().mkdir(xmlDirPath);
+    QString htmlDirPath = QStringLiteral(HTML_DOCS_PATH) + "html-docs/";
+    QDir().mkdir(htmlDirPath);
 
-    QDir dir(QStringLiteral(HTML_DOCS_PATH) + "scpi-xmls");
-    dir.removeRecursively();
+    ScpiIfaceExportGenerator scpiIfaceExportGenerator;
+    scpiIfaceExportGenerator.getAllSessionsScpiIfaceXmls("mt310s2", xmlDirPath);
+    scpiIfaceExportGenerator.getAllSessionsScpiIfaceXmls("com5003", xmlDirPath);
+
+    QDir xmlDir(xmlDirPath);
+    QString htmlPath;
+    for(auto xmlFile: xmlDir.entryInfoList({"*.xml"})) {
+        htmlPath = htmlDirPath + xmlFile.fileName().replace("xml", "html");
+        createHtml(zenuxRelease, xmlFile, "false", htmlPath);
+    }
+
+    QFileInfo mtDefaultSessionXml(xmlDirPath + "/mt310s2-meas-session.xml");
+    htmlPath = htmlDirPath + mtDefaultSessionXml.fileName().replace("meas-session.xml", "adjustment.html");
+    createHtml(zenuxRelease, mtDefaultSessionXml, "true", htmlPath);
+
+    QFileInfo comDefaultSessionXml(xmlDirPath + "/com5003-meas-session.xml");
+    htmlPath = htmlDirPath + comDefaultSessionXml.fileName().replace("meas-session.xml", "adjustment.html");
+    createHtml(zenuxRelease, comDefaultSessionXml, "true", htmlPath);
+
+    xmlDir.removeRecursively();
 }
