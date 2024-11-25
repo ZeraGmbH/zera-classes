525b48365fb8c5e3791ff4889e0910304d3c272f
Andreas Müller <schnitzeltony@gmail.com>
2024-11-26 00:22:38 +0100

    Range: First steps onto independency/testability
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/channel-range-observer/lib/range.cpp
+++ modules-common/channel-range-observer/lib/range.cpp
@@ -1,5 +1,46 @@
 #include "range.h"
+#include <taskcontainersequence.h>
+#include <tasklambdarunner.h>
+#include <taskserverconnectionstart.h>
+#include <proxy.h>
 
 namespace ChannelRangeObserver {
+
+Range::Range(const QString &channelMName,
+             const NetworkConnectionInfo &netInfo,
+             VeinTcp::AbstractTcpNetworkFactoryPtr tcpFactory) :
+    m_channelMName(channelMName),
+    m_netInfo(netInfo),
+    m_tcpFactory(tcpFactory),
+    m_pcbClient(Zera::Proxy::getInstance()->getConnectionSmart(netInfo, tcpFactory)),
+    m_pcbInterface(std::make_shared<Zera::cPCBInterface>())
+{
+
+}
+
+void Range::startFetch()
+{
+    TaskContainerInterfacePtr task = TaskContainerSequence::create();
+    task->addSub(getPcbConnectionTask());
+    /*task->addSub(TaskLambdaRunner::create([&]() {
+        taskContainer->addSub(TaskRangeGetIsAvailable::create(
+            m_pcbInterface, m_channelMName, rangeName, newRange->m_available,
+            TRANSACTION_TIMEOUT,
+            [&]{ notifyError(QString("Could not fetch range availability: Channel %1 / Range %2").arg(m_channelMName, rangeName)); }));
+        taskContainer->addSub(TaskRangeGetUrValue::create(
+            m_pcbInterface, m_channelMName, rangeName, newRange->m_urValue,
+            TRANSACTION_TIMEOUT,
+            [&]{ notifyError(QString("Could not fetch range UrValue: Channel %1 / Range %2").arg(m_channelMName, rangeName)); }));
+
+        // TODO: add more range tasks
+
+        return true;
+    }));*/
+}
+
+TaskTemplatePtr Range::getPcbConnectionTask()
+{
+    return TaskServerConnectionStart::create(m_pcbClient, CONNECTION_TIMEOUT);
+}
 
 }
--- modules-common/channel-range-observer/lib/range.h
+++ modules-common/channel-range-observer/lib/range.h
@@ -1,12 +1,32 @@
 #ifndef CHANNEL_RANGE_OBSERVER_RANGE_H
 #define CHANNEL_RANGE_OBSERVER_RANGE_H
 
+#include <abstracttcpnetworkfactory.h>
+#include <networkconnectioninfo.h>
+#include <pcbinterface.h>
+#include <tasktemplate.h>
+
 namespace ChannelRangeObserver {
 
-struct Range
+class Range : public QObject
 {
+    Q_OBJECT
+public:
     bool m_available = false;
     double m_urValue = 0.0;
+
+    Range(const QString &channelMName,
+          const NetworkConnectionInfo &netInfo,
+          VeinTcp::AbstractTcpNetworkFactoryPtr tcpFactory);
+    void startFetch();
+private:
+    TaskTemplatePtr getPcbConnectionTask();
+    const QString m_channelMName;
+
+    const NetworkConnectionInfo m_netInfo;
+    const VeinTcp::AbstractTcpNetworkFactoryPtr m_tcpFactory;
+    const Zera::ProxyClientPtr m_pcbClient;
+    const Zera::PcbInterfacePtr m_pcbInterface;
 };
 
 }
