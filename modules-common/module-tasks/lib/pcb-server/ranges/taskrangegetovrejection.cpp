a6e47de66ff82fac004d6b98b184b69279c0e1db
Andreas Müller <schnitzeltony@gmail.com>
2024-11-30 22:19:43 +0100

    TaskRangeGetOvRejection: Polish code
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/module-tasks/lib/pcb-server/ranges/taskrangegetovrejection.cpp
+++ modules-common/module-tasks/lib/pcb-server/ranges/taskrangegetovrejection.cpp
@@ -2,31 +2,32 @@
 #include "taskdecoratortimeout.h"
 
 TaskTemplatePtr TaskRangeGetOvRejection::create(Zera::PcbInterfacePtr pcbInterface,
-                                                   QString channelSysName, QString rangeName,
-                                                   double &valueReceived,
-                                                   int timeout, std::function<void ()> additionalErrorHandler)
+                                                QString channelMName, QString rangeName,
+                                                double &valueReceived,
+                                                int timeout, std::function<void ()> additionalErrorHandler)
 {
     return TaskDecoratorTimeout::wrapTimeout(timeout,
                                              std::make_unique<TaskRangeGetOvRejection>(
                                                  pcbInterface,
-                                                 channelSysName, rangeName,
+                                                 channelMName, rangeName,
                                                  valueReceived),
                                              additionalErrorHandler);
 }
 
 TaskRangeGetOvRejection::TaskRangeGetOvRejection(Zera::PcbInterfacePtr pcbInterface,
-                                                     QString channelSysName, QString rangeName,
-                                                     double &valueReceived) :
+                                                 QString channelMName, QString rangeName,
+                                                 double &valueReceived) :
     TaskServerTransactionTemplate(pcbInterface),
     m_pcbInterface(pcbInterface),
-    m_channelSysName(channelSysName), m_rangeName(rangeName),
+    m_channelMName(channelMName),
+    m_rangeName(rangeName),
     m_valueReceived(valueReceived)
 {
 }
 
 quint32 TaskRangeGetOvRejection::sendToServer()
 {
-    return m_pcbInterface->getOVRejection(m_channelSysName, m_rangeName);
+    return m_pcbInterface->getOVRejection(m_channelMName, m_rangeName);
 }
 
 bool TaskRangeGetOvRejection::handleCheckedServerAnswer(QVariant answer)
