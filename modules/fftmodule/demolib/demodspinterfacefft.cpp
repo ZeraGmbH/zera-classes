e10e7387d3a220775f851d302227fecc4f651c5a
Andreas Müller <schnitzeltony@gmail.com>
2024-12-09 13:26:14 +0100

    demodspinterfacefft: Rework demo-values for analysis of MTVis
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/fftmodule/demolib/demodspinterfacefft.cpp
+++ modules/fftmodule/demolib/demodspinterfacefft.cpp
@@ -23,15 +23,15 @@ void DemoDspInterfaceFft::onTimer()
 
     DemoValuesDspFft dspValues(totalChannels);
 
-    dspValues.setValue(0, 0, 5*randomDeviation, 5*randomDeviation);
+    dspValues.setValue(0, 0, 5*randomDeviation, 0);
     dspValues.setValue(0, 1, 230*randomDeviation, 0);
     dspValues.setValue(0, 2, 0, 42*randomDeviation);
 
-    dspValues.setValue(1, 0, 5, 5);
+    dspValues.setValue(1, 0, -5, 0);
     dspValues.setValue(1, 1, 230, 0);
     dspValues.setValue(1, 2, 0, 42);
 
-    dspValues.setValue(2, 0, 5, 5);
+    dspValues.setValue(2, 0, 5, 0);
     dspValues.setValue(2, 1, 230/M_SQRT2, 230/M_SQRT2);
     dspValues.setValue(2, 2, 42/M_SQRT2, 42/M_SQRT2);
 
@@ -39,9 +39,9 @@ void DemoDspInterfaceFft::onTimer()
     dspValues.setValue(3, 1, 4*randomDeviation, 0);
     dspValues.setValue(3, 2, 0, 6*randomDeviation);
 
-    dspValues.setValue(4, 0, 2, 2);
-    dspValues.setValue(4, 1, 4, 0);
-    dspValues.setValue(4, 2, 0, 6);
+    dspValues.setValue(4, 0, -20, 0);
+    dspValues.setValue(4, 1, -40, 0);
+    dspValues.setValue(4, 2, 0, -60);
 
     dspValues.setValue(5, 0, 2, 2);
     dspValues.setValue(5, 1, 4/M_SQRT2, 4/M_SQRT2);
