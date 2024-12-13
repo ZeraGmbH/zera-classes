8abb13af27fcc76ec564951eb32bb0eeb1ab05b4
Andreas Müller <schnitzeltony@gmail.com>
2024-12-13 09:00:55 +0100

    thdnmodulemeasprogram: Get sample rate from channel range observer
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/thdnmodule/lib/thdnmodulemeasprogram.h
+++ modules/thdnmodule/lib/thdnmodulemeasprogram.h
@@ -34,7 +34,6 @@ enum thdnmoduleCmds
     readresourcetypes,
     readresource,
     readresourceinfo,
-    readsamplerate,
     readalias,
     readunit,
     readdspchannel,
@@ -83,7 +82,6 @@ private:
     QHash<QString, cMeasChannelInfo> m_measChannelInfoHash;
     QList<QString> channelInfoReadList; // a list of all channel info we have to read
     QString channelInfoRead; // the actual channel info we are working on
-    quint32 m_nSRate; // number of samples / signal period
 
     cDspMeasData* m_pTmpDataDsp;
     cDspMeasData* m_pParameterDSP;
@@ -99,7 +97,6 @@ private:
     QState m_readResourceInfoState;
     QState m_readResourceInfoDoneState;
     QState m_pcbserverConnectState;
-    QState m_readSampleRateState;
     QState m_readChannelInformationState;
     QState m_readChannelAliasState;
     QState m_readChannelUnitState;
@@ -143,7 +140,6 @@ private slots:
     void readResourceInfoDone();
 
     void pcbserverConnect();
-    void readSampleRate();
     void readChannelInformation();
     void readChannelAlias();
     void readChannelUnit();
