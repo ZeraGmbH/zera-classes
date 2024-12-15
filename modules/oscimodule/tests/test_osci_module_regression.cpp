5073d44da0b7b119ba9ac5f931c4d8f69dfb6995
Andreas Müller <schnitzeltony@gmail.com>
2024-12-15 17:24:54 +0100

    oscimodule: Add test for reference channel change
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/oscimodule/tests/test_osci_module_regression.cpp
+++ modules/oscimodule/tests/test_osci_module_regression.cpp
@@ -1,9 +1,14 @@
 #include "test_osci_module_regression.h"
 #include "modulemanagertestrunner.h"
 #include <testloghelpers.h>
+#include <vf_entity_component_event_item.h>
+#include <vf_client_component_setter.h>
+#include <timemachineobject.h>
 #include <QTest>
 
 QTEST_MAIN(test_osci_module_regression)
+
+static int constexpr osciEntityId = 1120;
 
 void test_osci_module_regression::dumpDspSetup()
 {
@@ -15,4 +20,33 @@ void test_osci_module_regression::dumpDspSetup()
     QString measProgramDumped = TestLogHelpers::dump(dspInterfaces[0]->dumpAll());
     QString measProgramExpected = TestLogHelpers::loadFile(":/dspDumps/dumpMeasProgram.json");
     QVERIFY(TestLogHelpers::compareAndLogOnDiff(measProgramExpected, measProgramDumped));
+}
+
+void test_osci_module_regression::dumpDspSetReference()
+{
+    ModuleManagerTestRunner testRunner(":/session-minimal.json");
+    setReferenceChannel(testRunner.getVfCmdEventHandlerSystemPtr(), "m0", "m1");
+    setReferenceChannel(testRunner.getVfCmdEventHandlerSystemPtr(), "m1", "m2");
+    setReferenceChannel(testRunner.getVfCmdEventHandlerSystemPtr(), "m2", "m3");
+    setReferenceChannel(testRunner.getVfCmdEventHandlerSystemPtr(), "m3", "m4");
+    setReferenceChannel(testRunner.getVfCmdEventHandlerSystemPtr(), "m4", "m5");
+    setReferenceChannel(testRunner.getVfCmdEventHandlerSystemPtr(), "m5", "m0");
+
+    const QList<TestDspInterfacePtr>& dspInterfaces = testRunner.getDspInterfaceList();
+    QCOMPARE(dspInterfaces.count(), 1);
+
+    QString measProgramDumped = TestLogHelpers::dump(dspInterfaces[0]->dumpAll(true));
+    QString measProgramExpected = TestLogHelpers::loadFile(":/dspDumps/dump-ref-UL1-to-other.json");
+    QVERIFY(TestLogHelpers::compareAndLogOnDiff(measProgramExpected, measProgramDumped));
+}
+
+void test_osci_module_regression::setReferenceChannel(VfCmdEventHandlerSystemPtr vfCmdEventHandlerSystem, QString channelMNameOld, QString channelMNameNew)
+{
+    VfCmdEventItemEntityPtr entityItem = VfEntityComponentEventItem::create(osciEntityId);
+    vfCmdEventHandlerSystem->addItem(entityItem);
+
+    VfClientComponentSetterPtr setter = VfClientComponentSetter::create("PAR_RefChannel", entityItem);
+    entityItem->addItem(setter);
+    setter->startSetComponent(channelMNameOld, channelMNameNew);
+    TimeMachineObject::feedEventLoop();
 }
--- modules/oscimodule/tests/test_osci_module_regression.h
+++ modules/oscimodule/tests/test_osci_module_regression.h
@@ -1,13 +1,19 @@
 #ifndef TEST_OSCI_MODULE_REGRESSION_H
 #define TEST_OSCI_MODULE_REGRESSION_H
 
-#include <QObject>
+#include "vf_cmd_event_handler_system.h"
 
 class test_osci_module_regression : public QObject
 {
     Q_OBJECT
 private slots:
     void dumpDspSetup();
+    void dumpDspSetReference();
+private:
+         // we expect channel m-Name!!!
+    void setReferenceChannel(VfCmdEventHandlerSystemPtr vfCmdEventHandlerSystem,
+                             QString channelMNameOld, // MUST be correct - otherwise ignored
+                             QString channelMNameNew);
 };
 
 #endif // TEST_OSCI_MODULE_REGRESSION_H
