48d37e6ee6cd08aff9ecb7a698143f5fb8fb6732
Andreas Müller <schnitzeltony@gmail.com>
2024-12-21 18:25:01 +0100

    fftmodule: Do not set up state machines twice
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/fftmodule/lib/fftmodule.cpp
+++ modules/fftmodule/lib/fftmodule.cpp
@@ -18,35 +18,6 @@ cFftModule::cFftModule(ModuleFactoryParam moduleParam) :
     m_sModuleName = QString("%1%2").arg(BaseModuleName).arg(moduleParam.m_moduleNum);
     m_sModuleDescription = QString("This module measures configured number of fft values for configured channels");
     m_sSCPIModuleName = QString("%1%2").arg(BaseSCPIModuleName).arg(moduleParam.m_moduleNum);
-
-    m_ActivationStartState.addTransition(this, &cFftModule::activationContinue, &m_ActivationExecState);
-    m_ActivationExecState.addTransition(this, &cFftModule::activationContinue, &m_ActivationDoneState);
-    m_ActivationDoneState.addTransition(this, &cFftModule::activationNext, &m_ActivationExecState);
-    m_ActivationDoneState.addTransition(this, &cFftModule::activationContinue, &m_ActivationFinishedState);
-    m_ActivationMachine.addState(&m_ActivationStartState);
-    m_ActivationMachine.addState(&m_ActivationExecState);
-    m_ActivationMachine.addState(&m_ActivationDoneState);
-    m_ActivationMachine.addState(&m_ActivationFinishedState);
-    m_ActivationMachine.setInitialState(&m_ActivationStartState);
-    connect(&m_ActivationStartState, &QState::entered, this, &cFftModule::activationStart);
-    connect(&m_ActivationExecState, &QState::entered, this, &cFftModule::activationExec);
-    connect(&m_ActivationDoneState, &QState::entered, this, &cFftModule::activationDone);
-    connect(&m_ActivationFinishedState, &QState::entered, this, &cFftModule::activationFinished);
-
-    m_DeactivationStartState.addTransition(this, &cFftModule::deactivationContinue, &m_DeactivationExecState);
-    m_DeactivationExecState.addTransition(this, &cFftModule::deactivationContinue, &m_DeactivationDoneState);
-    m_DeactivationDoneState.addTransition(this, &cFftModule::deactivationNext, &m_DeactivationExecState);
-    m_DeactivationDoneState.addTransition(this, &cFftModule::deactivationContinue, &m_DeactivationFinishedState);
-    m_DeactivationMachine.addState(&m_DeactivationStartState);
-    m_DeactivationMachine.addState(&m_DeactivationExecState);
-    m_DeactivationMachine.addState(&m_DeactivationDoneState);
-    m_DeactivationMachine.addState(&m_DeactivationFinishedState);
-    m_DeactivationMachine.setInitialState(&m_DeactivationStartState);
-    connect(&m_DeactivationStartState, &QState::entered, this, &cFftModule::deactivationStart);
-    connect(&m_DeactivationExecState, &QState::entered, this, &cFftModule::deactivationExec);
-    connect(&m_DeactivationDoneState, &QState::entered, this, &cFftModule::deactivationDone);
-    connect(&m_DeactivationFinishedState, &QState::entered, this, &cFftModule::deactivationFinished);
-
 }
 
 void cFftModule::setupModule()
