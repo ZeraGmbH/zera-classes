c31849d132f836bb7b9c2f6bef3560f02b7adfaf
Andreas Müller <schnitzeltony@gmail.com>
2024-12-21 19:17:45 +0100

    spm1module: Do not set up state machines twice
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/spm1module/lib/spm1module.cpp
+++ modules/spm1module/lib/spm1module.cpp
@@ -13,34 +13,6 @@ cSpm1Module::cSpm1Module(ModuleFactoryParam moduleParam) :
     m_sModuleName = QString("%1%2").arg(BaseModuleName).arg(moduleParam.m_moduleNum);
     m_sModuleDescription = QString("This module povides a configurable error calculator");
     m_sSCPIModuleName = QString(BaseSCPIModuleName);
-
-    m_ActivationStartState.addTransition(this, &cSpm1Module::activationContinue, &m_ActivationExecState);
-    m_ActivationExecState.addTransition(this, &cSpm1Module::activationContinue, &m_ActivationDoneState);
-    m_ActivationDoneState.addTransition(this, &cSpm1Module::activationNext, &m_ActivationExecState);
-    m_ActivationDoneState.addTransition(this, &cSpm1Module::activationContinue, &m_ActivationFinishedState);
-    m_ActivationMachine.addState(&m_ActivationStartState);
-    m_ActivationMachine.addState(&m_ActivationExecState);
-    m_ActivationMachine.addState(&m_ActivationDoneState);
-    m_ActivationMachine.addState(&m_ActivationFinishedState);
-    m_ActivationMachine.setInitialState(&m_ActivationStartState);
-    connect(&m_ActivationStartState, &QState::entered, this, &cSpm1Module::activationStart);
-    connect(&m_ActivationExecState, &QState::entered, this, &cSpm1Module::activationExec);
-    connect(&m_ActivationDoneState, &QState::entered, this, &cSpm1Module::activationDone);
-    connect(&m_ActivationFinishedState, &QState::entered, this, &cSpm1Module::activationFinished);
-
-    m_DeactivationStartState.addTransition(this, &cSpm1Module::deactivationContinue, &m_DeactivationExecState);
-    m_DeactivationExecState.addTransition(this, &cSpm1Module::deactivationContinue, &m_DeactivationDoneState);
-    m_DeactivationDoneState.addTransition(this, &cSpm1Module::deactivationNext, &m_DeactivationExecState);
-    m_DeactivationDoneState.addTransition(this, &cSpm1Module::deactivationContinue, &m_DeactivationFinishedState);
-    m_DeactivationMachine.addState(&m_DeactivationStartState);
-    m_DeactivationMachine.addState(&m_DeactivationExecState);
-    m_DeactivationMachine.addState(&m_DeactivationDoneState);
-    m_DeactivationMachine.addState(&m_DeactivationFinishedState);
-    m_DeactivationMachine.setInitialState(&m_DeactivationStartState);
-    connect(&m_DeactivationStartState, &QState::entered, this, &cSpm1Module::deactivationStart);
-    connect(&m_DeactivationExecState, &QState::entered, this, &cSpm1Module::deactivationExec);
-    connect(&m_DeactivationDoneState, &QState::entered, this, &cSpm1Module::deactivationDone);
-    connect(&m_DeactivationFinishedState, &QState::entered, this, &cSpm1Module::deactivationFinished);
 }
 
 void cSpm1Module::setupModule()
