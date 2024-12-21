2edc5e733322a4329fb95607d07860f78091e4ef
Andreas Müller <schnitzeltony@gmail.com>
2024-12-21 19:03:34 +0100

    power3module: Do not set up state machines twice
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/power3module/lib/power3module.cpp
+++ modules/power3module/lib/power3module.cpp
@@ -12,34 +12,6 @@ cPower3Module::cPower3Module(ModuleFactoryParam moduleParam) :
     m_sModuleName = QString("%1%2").arg(BaseModuleName).arg(moduleParam.m_moduleNum);
     m_sModuleDescription = QString("This module measures configured number of harmonic power values from configured input values");
     m_sSCPIModuleName = QString("%1%2").arg(BaseSCPIModuleName).arg(moduleParam.m_moduleNum);
-
-    m_ActivationStartState.addTransition(this, &cPower3Module::activationContinue, &m_ActivationExecState);
-    m_ActivationExecState.addTransition(this, &cPower3Module::activationContinue, &m_ActivationDoneState);
-    m_ActivationDoneState.addTransition(this, &cPower3Module::activationNext, &m_ActivationExecState);
-    m_ActivationDoneState.addTransition(this, &cPower3Module::activationContinue, &m_ActivationFinishedState);
-    m_ActivationMachine.addState(&m_ActivationStartState);
-    m_ActivationMachine.addState(&m_ActivationExecState);
-    m_ActivationMachine.addState(&m_ActivationDoneState);
-    m_ActivationMachine.addState(&m_ActivationFinishedState);
-    m_ActivationMachine.setInitialState(&m_ActivationStartState);
-    connect(&m_ActivationStartState, &QState::entered, this, &cPower3Module::activationStart);
-    connect(&m_ActivationExecState, &QState::entered, this, &cPower3Module::activationExec);
-    connect(&m_ActivationDoneState, &QState::entered, this, &cPower3Module::activationDone);
-    connect(&m_ActivationFinishedState, &QState::entered, this, &cPower3Module::activationFinished);
-
-    m_DeactivationStartState.addTransition(this, &cPower3Module::deactivationContinue, &m_DeactivationExecState);
-    m_DeactivationExecState.addTransition(this, &cPower3Module::deactivationContinue, &m_DeactivationDoneState);
-    m_DeactivationDoneState.addTransition(this, &cPower3Module::deactivationNext, &m_DeactivationExecState);
-    m_DeactivationDoneState.addTransition(this, &cPower3Module::deactivationContinue, &m_DeactivationFinishedState);
-    m_DeactivationMachine.addState(&m_DeactivationStartState);
-    m_DeactivationMachine.addState(&m_DeactivationExecState);
-    m_DeactivationMachine.addState(&m_DeactivationDoneState);
-    m_DeactivationMachine.addState(&m_DeactivationFinishedState);
-    m_DeactivationMachine.setInitialState(&m_DeactivationStartState);
-    connect(&m_DeactivationStartState, &QState::entered, this, &cPower3Module::deactivationStart);
-    connect(&m_DeactivationExecState, &QState::entered, this, &cPower3Module::deactivationExec);
-    connect(&m_DeactivationDoneState, &QState::entered, this, &cPower3Module::deactivationDone);
-    connect(&m_DeactivationFinishedState, &QState::entered, this, &cPower3Module::deactivationFinished);
 }
 
 void cPower3Module::setupModule()
