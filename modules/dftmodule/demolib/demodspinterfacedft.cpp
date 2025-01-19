e256d2249b2cd556ea3cbbb10855fe13dd058abd
Andreas Müller <schnitzeltony@gmail.com>
2025-01-19 01:14:19 +0100

    demodspinterfacedft: Remove unused code
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/dftmodule/demolib/demodspinterfacedft.cpp
+++ modules/dftmodule/demolib/demodspinterfacedft.cpp
@@ -24,20 +24,10 @@ void DemoDspInterfaceDft::onTimer()
             m_currentAngle = 0;
         dftValues.setAllValuesSymmetric(230, 1, m_currentAngle, false);
         demoValues = dftValues.getDspValues();
-        for(int i=0; i<demoValues.count(); i++) {
-            float randomVal = (double)rand() / RAND_MAX;
-            float randomDeviation = 0.5 + randomVal;
-            //demoValues[i] *= randomDeviation;
-        }
     }
     else {
         dftValues.setAllValuesSymmetric(10, 10, 0, false); // COM DC ref
         demoValues = dftValues.getDspValues();
-        for(int i=0; i<demoValues.count(); i++) {
-            float randomVal = (double)rand() / RAND_MAX;
-            float randomDeviation = 0.95 + 0.1*randomVal;
-            //demoValues[i] *= randomDeviation;
-        }
     }
     fireActValInterrupt(demoValues, /* dummy */ 0);
 }
