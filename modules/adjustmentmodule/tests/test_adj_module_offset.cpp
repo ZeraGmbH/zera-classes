3863700a066e638177026546223902544a132c41
Andreas Müller <schnitzeltony@gmail.com>
2025-02-02 23:03:25 +0100

    test_adj_module_offset: Speedup by starting system only on demand
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/adjustmentmodule/tests/test_adj_module_offset.cpp
+++ modules/adjustmentmodule/tests/test_adj_module_offset.cpp
@@ -1,6 +1,5 @@
 #include "test_adj_module_offset.h"
 #include "adjmoduletesthelper.h"
-#include <scpimoduleclientblocked.h>
 #include <QTest>
 
 QTEST_MAIN(test_adj_module_offset)
@@ -8,18 +7,32 @@ QTEST_MAIN(test_adj_module_offset)
 constexpr double testvoltage = 120;
 constexpr double testcurrent = 10;
 
+void test_adj_module_offset::init()
+{
+    if(!m_testRunner)
+        m_testRunner = std::make_unique<ModuleManagerTestRunner>(":/session-minimal-dc.json", true);
+    if(!m_scpiClient)
+        m_scpiClient = std::make_unique<ScpiModuleClientBlocked>();
+    QString response = m_scpiClient->sendReceive("*cls|CALC:ADJ1:INIT UAUX,250V;|*stb?");
+    QCOMPARE(response, "+0");
+}
+
+void test_adj_module_offset::destroyCommonTestRunner()
+{
+    m_scpiClient = nullptr;
+    m_testRunner = nullptr;
+}
+
 void test_adj_module_offset::validActValuesWithPermission()
 {
-    ModuleManagerTestRunner testRunner(":/session-minimal-dc.json", true);
-    AdjModuleTestHelper::setAllValuesSymmetricDc(testRunner, testvoltage, testcurrent);
-
-    ScpiModuleClientBlocked scpiClient;
-    QString response = scpiClient.sendReceive("calc:adj1:offs UAUX,250V,0;|*stb?");
+    AdjModuleTestHelper::setAllValuesSymmetricDc(*m_testRunner, testvoltage, testcurrent);
+    QString response = m_scpiClient->sendReceive("calc:adj1:offs UAUX,250V,0;|*stb?");
     QCOMPARE(response, "+0");
 }
 
 void test_adj_module_offset::validActValuesWithoutPermission()
 {
+    destroyCommonTestRunner();
     ModuleManagerTestRunner testRunner(":/session-minimal.json", false);
     AdjModuleTestHelper::setAllValuesSymmetricDc(testRunner, testvoltage, testcurrent);
 
@@ -30,20 +43,14 @@ void test_adj_module_offset::validActValuesWithoutPermission()
 
 void test_adj_module_offset::denyNonOffsetChannel()
 {
-    ModuleManagerTestRunner testRunner(":/session-minimal-dc.json", true);
-    AdjModuleTestHelper::setAllValuesSymmetricDc(testRunner, testvoltage, testcurrent);
-
-    ScpiModuleClientBlocked scpiClient;
-    QString response = scpiClient.sendReceive("calc:adj1:offs UL1,250V,0;|*stb?");
+    AdjModuleTestHelper::setAllValuesSymmetricDc(*m_testRunner, testvoltage, testcurrent);
+    QString response = m_scpiClient->sendReceive("calc:adj1:offs UL1,250V,0;|*stb?");
     QCOMPARE(response, "+4");
 }
 
 void test_adj_module_offset::denyRangeNotSet()
 {
-    ModuleManagerTestRunner testRunner(":/session-minimal-dc.json", true);
-    AdjModuleTestHelper::setAllValuesSymmetricDc(testRunner, testvoltage, testcurrent);
-
-    ScpiModuleClientBlocked scpiClient;
-    QString response = scpiClient.sendReceive("calc:adj1:offs UAUX,8V,0;|*stb?");
+    AdjModuleTestHelper::setAllValuesSymmetricDc(*m_testRunner, testvoltage, testcurrent);
+    QString response = m_scpiClient->sendReceive("calc:adj1:offs UAUX,8V,0;|*stb?");
     QCOMPARE(response, "+4");
 }
