af010d6d184af204b8b4706ad9c96bb595cec7bf
Andreas Müller <schnitzeltony@gmail.com>
2025-02-02 23:03:25 +0100

    test_adj_module_offset: Speedup by starting system only on demand
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/adjustmentmodule/tests/test_adj_module_offset.h
+++ modules/adjustmentmodule/tests/test_adj_module_offset.h
@@ -1,18 +1,28 @@
 #ifndef TEST_ADJ_MODULE_OFFSET_H
 #define TEST_ADJ_MODULE_OFFSET_H
 
+#include "modulemanagertestrunner.h"
+#include "scpimoduleclientblocked.h"
 #include <QObject>
 
 class test_adj_module_offset : public QObject
 {
     Q_OBJECT
 private slots:
+    void init();
+
     void validActValuesWithPermission();
     void validActValuesWithoutPermission();
 
     // from here on with permission
     void denyNonOffsetChannel();
     void denyRangeNotSet();
+
+private:
+    void destroyCommonTestRunner();
+
+    std::unique_ptr<ModuleManagerTestRunner> m_testRunner;
+    std::unique_ptr<ScpiModuleClientBlocked> m_scpiClient;
 };
 
 #endif // TEST_ADJ_MODULE_OFFSET_H
