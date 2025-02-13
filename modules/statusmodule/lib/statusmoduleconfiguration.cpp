d88797a41b5728622ad162c9275ac0efbc226173
Andreas Müller <schnitzeltony@gmail.com>
2025-02-14 00:23:04 +0100

    statusmodule: Move xsd-resource to lib/config
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/statusmodule/lib/statusmoduleconfiguration.cpp
+++ modules/statusmodule/lib/statusmoduleconfiguration.cpp
@@ -3,12 +3,18 @@
 #include "statusmoduleconfigdata.h"
 #include <xmlconfigreader.h>
 
+static void initResource()
+{
+    Q_INIT_RESOURCE(statusmodulexmlschema);
+}
+
 namespace STATUSMODULE
 {
 static const char* defaultXSDFile = "://statusmodule.xsd";
 
 cStatusModuleConfiguration::cStatusModuleConfiguration()
 {
+    initResource();
     m_pStatusModulConfigData = 0;
     connect(m_pXMLReader, &Zera::XMLConfig::cReader::valueChanged, this, &cStatusModuleConfiguration::configXMLInfo);
     connect(m_pXMLReader, &Zera::XMLConfig::cReader::finishedParsingXML, this, &cStatusModuleConfiguration::completeConfiguration);
