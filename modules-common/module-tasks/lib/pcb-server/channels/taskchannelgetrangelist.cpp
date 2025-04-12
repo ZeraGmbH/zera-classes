bbf86b67bf2fdde55aee3ee20226d9d300536768
Andreas Müller <schnitzeltony@gmail.com>
2025-04-12 19:10:28 +0200

    TaskChannelGetRangeList: Shrink down to create() by using TaskGetScpiStringList
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/module-tasks/lib/pcb-server/channels/taskchannelgetrangelist.cpp
+++ modules-common/module-tasks/lib/pcb-server/channels/taskchannelgetrangelist.cpp
@@ -1,37 +1,14 @@
 #include "taskchannelgetrangelist.h"
-#include "taskdecoratortimeout.h"
-#include <reply.h>
+#include "taskgetscpistringlist.h"
 
 TaskTemplatePtr TaskChannelGetRangeList::create(Zera::PcbInterfacePtr pcbInterface,
                                                 QString channelName,
                                                 std::shared_ptr<QStringList> rangeList,
                                                 int timeout, std::function<void ()> additionalErrorHandler)
 {
-    return TaskDecoratorTimeout::wrapTimeout(timeout,
-                                             std::make_unique<TaskChannelGetRangeList>(
-                                                 pcbInterface,
-                                                 channelName,
-                                                 rangeList),
-                                             additionalErrorHandler);
-}
-
-TaskChannelGetRangeList::TaskChannelGetRangeList(Zera::PcbInterfacePtr pcbInterface,
-                                                 QString channelName,
-                                                 std::shared_ptr<QStringList> rangeList) :
-    TaskServerTransactionTemplate(pcbInterface),
-    m_pcbInterface(pcbInterface),
-    m_channelName(channelName),
-    m_rangeList(rangeList)
-{
-}
-
-quint32 TaskChannelGetRangeList::sendToServer()
-{
-    return m_pcbInterface->getRangeList(m_channelName);
-}
-
-bool TaskChannelGetRangeList::handleCheckedServerAnswer(QVariant answer)
-{
-    *m_rangeList = answer.toStringList();
-    return true;
+    return TaskGetScpiStringList::create(pcbInterface,
+                                         QString("SENS:%1:RANG:CAT?").arg(channelName),
+                                         rangeList,
+                                         timeout,
+                                         additionalErrorHandler);
 }
--- modules-common/module-tasks/lib/pcb-server/channels/taskchannelgetrangelist.h
+++ modules-common/module-tasks/lib/pcb-server/channels/taskchannelgetrangelist.h
@@ -1,24 +1,16 @@
 #ifndef TASKCHANNELREADRANGES_H
 #define TASKCHANNELREADRANGES_H
 
-#include "taskservertransactiontemplate.h"
+#include <tasktemplate.h>
 #include <pcbinterface.h>
 
-class TaskChannelGetRangeList : public TaskServerTransactionTemplate
+class TaskChannelGetRangeList
 {
-    Q_OBJECT
 public:
     static TaskTemplatePtr create(Zera::PcbInterfacePtr pcbInterface,
-                                   QString channelName,
-                                   std::shared_ptr<QStringList> rangeList,
-                                   int timeout, std::function<void()> additionalErrorHandler = []{});
-    TaskChannelGetRangeList(Zera::PcbInterfacePtr pcbInterface, QString channelName, std::shared_ptr<QStringList>rangeList);
-private:
-    quint32 sendToServer() override;
-    bool handleCheckedServerAnswer(QVariant answer) override;
-    Zera::PcbInterfacePtr m_pcbInterface;
-    QString m_channelName;
-    std::shared_ptr<QStringList> m_rangeList;
+                                  QString channelName,
+                                  std::shared_ptr<QStringList> rangeList,
+                                  int timeout, std::function<void()> additionalErrorHandler = []{});
 };
 
 #endif // TASKCHANNELREADRANGES_H
