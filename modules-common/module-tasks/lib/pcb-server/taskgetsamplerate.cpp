2bfa00db0819e56b35cb17deec75806c29e80ab4
Andreas Müller <schnitzeltony@gmail.com>
2025-04-12 21:59:08 +0200

    TaskGetSampleRate: Shrink down to create() by using TaskGetScpiInt
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/module-tasks/lib/pcb-server/taskgetsamplerate.cpp
+++ modules-common/module-tasks/lib/pcb-server/taskgetsamplerate.cpp
@@ -1,32 +1,13 @@
 #include "taskgetsamplerate.h"
-#include <taskdecoratortimeout.h>
+#include "taskgetscpiint.h"
 
 TaskTemplatePtr TaskGetSampleRate::create(Zera::PcbInterfacePtr pcbInterface,
                                           std::shared_ptr<int> valueReceived,
                                           int timeout, std::function<void ()> additionalErrorHandler)
 {
-    return TaskDecoratorTimeout::wrapTimeout(timeout,
-                                             std::make_unique<TaskGetSampleRate>(
-                                                 pcbInterface,
-                                                 valueReceived),
-                                             additionalErrorHandler);
-}
-
-TaskGetSampleRate::TaskGetSampleRate(Zera::PcbInterfacePtr pcbInterface, std::shared_ptr<int> valueReceived) :
-    TaskServerTransactionTemplate(pcbInterface),
-    m_pcbInterface(pcbInterface),
-    m_valueReceived(valueReceived)
-{
-}
-
-quint32 TaskGetSampleRate::sendToServer()
-{
-    return m_pcbInterface->getSampleRate();
-}
-
-bool TaskGetSampleRate::handleCheckedServerAnswer(QVariant answer)
-{
-    bool ok;
-    *m_valueReceived = answer.toInt(&ok);
-    return ok;
+    return TaskGetScpiInt::create(pcbInterface,
+                                  "SAMPLE:SRATE?",
+                                  valueReceived,
+                                  timeout,
+                                  additionalErrorHandler);
 }
--- modules-common/module-tasks/lib/pcb-server/taskgetsamplerate.h
+++ modules-common/module-tasks/lib/pcb-server/taskgetsamplerate.h
@@ -11,13 +11,6 @@ public:
     static TaskTemplatePtr create(Zera::PcbInterfacePtr pcbInterface,
                                   std::shared_ptr<int> valueReceived,
                                   int timeout, std::function<void()> additionalErrorHandler = []{});
-    TaskGetSampleRate(Zera::PcbInterfacePtr pcbInterface,
-                      std::shared_ptr<int> valueReceived);
-private:
-    quint32 sendToServer() override;
-    bool handleCheckedServerAnswer(QVariant answer) override;
-    Zera::PcbInterfacePtr m_pcbInterface;
-    std::shared_ptr<int> m_valueReceived;
 };
 
 #endif // TASKGETSAMPLERATE_H
