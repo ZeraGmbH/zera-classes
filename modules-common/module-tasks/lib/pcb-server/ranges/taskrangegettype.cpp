54725d70534e3899d25f613d69a155524273647b
Andreas Müller <schnitzeltony@gmail.com>
2025-04-13 00:16:47 +0200

    TaskRangeGetType: Shrink down to create() by using TaskGetScpiInt
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/module-tasks/lib/pcb-server/ranges/taskrangegettype.cpp
+++ modules-common/module-tasks/lib/pcb-server/ranges/taskrangegettype.cpp
@@ -1,38 +1,14 @@
 #include "taskrangegettype.h"
-#include <taskdecoratortimeout.h>
+#include "taskgetscpiint.h"
 
 TaskTemplatePtr TaskRangeGetType::create(Zera::PcbInterfacePtr pcbInterface,
                                          QString channelMName, QString rangeName,
                                          std::shared_ptr<int> valueReceived,
                                          int timeout, std::function<void ()> additionalErrorHandler)
 {
-    return TaskDecoratorTimeout::wrapTimeout(timeout,
-                                             std::make_unique<TaskRangeGetType>(
-                                                 pcbInterface,
-                                                 channelMName, rangeName,
-                                                 valueReceived),
-                                             additionalErrorHandler);
-}
-
-TaskRangeGetType::TaskRangeGetType(Zera::PcbInterfacePtr pcbInterface,
-                                   QString channelMName, QString rangeName,
-                                   std::shared_ptr<int> valueReceived) :
-    TaskServerTransactionTemplate(pcbInterface),
-    m_pcbInterface(pcbInterface),
-    m_channelMName(channelMName), m_rangeName(rangeName),
-    m_valueReceived(valueReceived)
-
-{
-}
-
-quint32 TaskRangeGetType::sendToServer()
-{
-    return m_pcbInterface->getType(m_channelMName, m_rangeName);
-}
-
-bool TaskRangeGetType::handleCheckedServerAnswer(QVariant answer)
-{
-    bool ok;
-    *m_valueReceived = answer.toInt(&ok);
-    return ok;
+    return TaskGetScpiInt::create(pcbInterface,
+                                  QString("SENSE:%1:%2:TYPE?").arg(channelMName, rangeName),
+                                  valueReceived,
+                                  timeout,
+                                  additionalErrorHandler);
 }
--- modules-common/module-tasks/lib/pcb-server/ranges/taskrangegettype.h
+++ modules-common/module-tasks/lib/pcb-server/ranges/taskrangegettype.h
@@ -1,27 +1,16 @@
 #ifndef TASKRANGEGETTYPE_H
 #define TASKRANGEGETTYPE_H
 
-#include <taskservertransactiontemplate.h>
 #include <pcbinterface.h>
+#include <tasktemplate.h>
 
-class TaskRangeGetType : public TaskServerTransactionTemplate
+class TaskRangeGetType
 {
-    Q_OBJECT
 public:
     static TaskTemplatePtr create(Zera::PcbInterfacePtr pcbInterface,
                                   QString channelMName, QString rangeName,
                                   std::shared_ptr<int> valueReceived,
                                   int timeout, std::function<void()> additionalErrorHandler = []{});
-    TaskRangeGetType(Zera::PcbInterfacePtr pcbInterface,
-                     QString channelMName, QString rangeName,
-                     std::shared_ptr<int> valueReceived);
-private:
-    quint32 sendToServer() override;
-    bool handleCheckedServerAnswer(QVariant answer) override;
-    Zera::PcbInterfacePtr m_pcbInterface;
-    QString m_channelMName;
-    QString m_rangeName;
-    std::shared_ptr<int> m_valueReceived;
 };
 
 #endif // TASKRANGEGETTYPE_H
