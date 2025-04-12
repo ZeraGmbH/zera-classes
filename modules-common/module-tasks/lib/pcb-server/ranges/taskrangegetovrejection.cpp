be12044c00df27b2d75de9ed074ff6a0b640019d
Andreas Müller <schnitzeltony@gmail.com>
2025-04-12 22:40:45 +0200

    TaskRangeGetOvRejection: Shrink down to create() by using TaskGetScpiDouble
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/module-tasks/lib/pcb-server/ranges/taskrangegetovrejection.cpp
+++ modules-common/module-tasks/lib/pcb-server/ranges/taskrangegetovrejection.cpp
@@ -1,38 +1,14 @@
 #include "taskrangegetovrejection.h"
-#include "taskdecoratortimeout.h"
+#include "taskgetscpidouble.h"
 
 TaskTemplatePtr TaskRangeGetOvRejection::create(Zera::PcbInterfacePtr pcbInterface,
                                                 QString channelMName, QString rangeName,
                                                 std::shared_ptr<double> valueReceived,
                                                 int timeout, std::function<void ()> additionalErrorHandler)
 {
-    return TaskDecoratorTimeout::wrapTimeout(timeout,
-                                             std::make_unique<TaskRangeGetOvRejection>(
-                                                 pcbInterface,
-                                                 channelMName, rangeName,
-                                                 valueReceived),
-                                             additionalErrorHandler);
-}
-
-TaskRangeGetOvRejection::TaskRangeGetOvRejection(Zera::PcbInterfacePtr pcbInterface,
-                                                 QString channelMName, QString rangeName,
-                                                 std::shared_ptr<double> valueReceived) :
-    TaskServerTransactionTemplate(pcbInterface),
-    m_pcbInterface(pcbInterface),
-    m_channelMName(channelMName),
-    m_rangeName(rangeName),
-    m_valueReceived(valueReceived)
-{
-}
-
-quint32 TaskRangeGetOvRejection::sendToServer()
-{
-    return m_pcbInterface->getOVRejection(m_channelMName, m_rangeName);
-}
-
-bool TaskRangeGetOvRejection::handleCheckedServerAnswer(QVariant answer)
-{
-    bool ok;
-    *m_valueReceived = answer.toDouble(&ok);
-    return ok;
+    return TaskGetScpiDouble::create(pcbInterface,
+                                     QString("SENSE:%1:%2:OVREJECTION?").arg(channelMName, rangeName),
+                                     valueReceived,
+                                     timeout,
+                                     additionalErrorHandler);
 }
--- modules-common/module-tasks/lib/pcb-server/ranges/taskrangegetovrejection.h
+++ modules-common/module-tasks/lib/pcb-server/ranges/taskrangegetovrejection.h
@@ -1,27 +1,16 @@
 #ifndef TASKRANGEGETOVREJECTION_H
 #define TASKRANGEGETOVREJECTION_H
 
-#include "taskservertransactiontemplate.h"
 #include <pcbinterface.h>
+#include <tasktemplate.h>
 
-class TaskRangeGetOvRejection : public TaskServerTransactionTemplate
+class TaskRangeGetOvRejection
 {
-    Q_OBJECT
 public:
     static TaskTemplatePtr create(Zera::PcbInterfacePtr pcbInterface,
                                   QString channelMName, QString rangeName,
                                   std::shared_ptr<double> valueReceived,
                                   int timeout, std::function<void()> additionalErrorHandler = []{});
-    TaskRangeGetOvRejection(Zera::PcbInterfacePtr pcbInterface,
-                            QString channelMName, QString rangeName,
-                            std::shared_ptr<double> valueReceived);
-private:
-    quint32 sendToServer() override;
-    bool handleCheckedServerAnswer(QVariant answer) override;
-    Zera::PcbInterfacePtr m_pcbInterface;
-    QString m_channelMName;
-    QString m_rangeName;
-    std::shared_ptr<double> m_valueReceived;
 };
 
 #endif // TASKRANGEGETOVREJECTION_H
