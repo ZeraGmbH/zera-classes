8e3e6d49c4115fd193750609e7f8e639cfee0e79
Andreas Müller <schnitzeltony@gmail.com>
2025-04-25 04:05:46 +0200

    Add VfModuleMetaData::getInstanceCount()
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules-common/vf-moduleinterface/vfmodulemetadata.cpp
+++ modules-common/vf-moduleinterface/vfmodulemetadata.cpp
@@ -1,14 +1,28 @@
 #include <QJsonObject>
 #include "vfmodulemetadata.h"
 
-VfModuleMetaData::VfModuleMetaData(QString name, QVariant value)
-    :m_sName(name), m_vValue(value)
+int VfModuleMetaData::m_instanceCount = 0;
+
+VfModuleMetaData::VfModuleMetaData(QString name, QVariant value) :
+    m_sName(name),
+    m_vValue(value)
 {
+    m_instanceCount++;
+}
+
+VfModuleMetaData::~VfModuleMetaData()
+{
+    m_instanceCount--;
 }
 
 void VfModuleMetaData::exportMetaData(QJsonObject &jsObj)
 {
     jsObj.insert(m_sName, m_vValue.toString());
+}
+
+int VfModuleMetaData::getInstanceCount()
+{
+    return m_instanceCount;
 }
 
 void VfModuleMetaData::setValue(QVariant value)
--- modules-common/vf-moduleinterface/vfmodulemetadata.h
+++ modules-common/vf-moduleinterface/vfmodulemetadata.h
@@ -9,12 +9,15 @@ class VfModuleMetaData: public QObject
     Q_OBJECT
 public:
     VfModuleMetaData(QString name, QVariant value);
+    ~VfModuleMetaData();
     virtual void exportMetaData(QJsonObject &jsObj);
+    static int getInstanceCount();
 public slots:
     void setValue(QVariant value);
 private:
     QString m_sName;
     QVariant m_vValue;
+    static int m_instanceCount;
 };
 
 #endif // VEINMODULEMETADATA_H
