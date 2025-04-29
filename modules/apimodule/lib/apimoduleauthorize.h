9a6b20c26746e25bd73233a4b8f9b038b9a895ab
mdoerr2s <m.doerr@zera.de>
2025-04-29 16:35:29 +0200

    apimodule: handle gui request response (zeragmbh/websam#1012)
    Signed-off-by: mdoerr2s <m.doerr@zera.de>

--- modules/apimodule/lib/apimoduleauthorize.h
+++ modules/apimodule/lib/apimoduleauthorize.h
@@ -17,13 +17,22 @@ public slots:
     void deactivate() override;
 
 private:
+    QJsonArray readTrustList();
+    bool jsonArrayContains(const QJsonArray& array, const QJsonObject& target);
+
+    const QString m_trustListPath = "/opt/websam-vein-api/authorize/trustlist.json";
     cApiModule* m_module;
 
     VfModuleParameter* m_pPendingRequestPar;
+    VfModuleParameter* m_pGuiDialogFinished;
     VfModuleComponent* m_pPendingRequestAct;
     VfModuleComponent* m_pRequestStatusAct;
     VfModuleComponent* m_pTrustListAct;
     VfModuleComponent* m_pTrustListChangeCountAct;
+
+private slots:
+    void onNewPendingRequest(QVariant pendingRequest);
+    void onGuiDialogFinished(QVariant dialogFinished);
 };
 }
 
