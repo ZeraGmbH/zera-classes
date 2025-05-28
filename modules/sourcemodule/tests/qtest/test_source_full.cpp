62bc58e203803a27889e042126a93f781c21341d
Andreas Müller <schnitzeltony@gmail.com>
2025-05-28 18:25:42 +0200

    sourcemodule: Add a test dumping vein events on MT310s2 switch on
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/sourcemodule/tests/qtest/test_source_full.cpp
+++ modules/sourcemodule/tests/qtest/test_source_full.cpp
@@ -1,17 +1,34 @@
 #include "test_source_full.h"
-#include "modulemanagertestrunner.h"
+#include <jsonparamapi.h>
 #include <vs_dumpjson.h>
+#include <vf_client_component_setter.h>
+#include <mocktcpnetworkfactory.h>
 #include <testloghelpers.h>
+#include <timemachinefortest.h>
 #include <QTest>
 
 QTEST_MAIN(test_source_full)
 
 static int constexpr sourceEntityId = 1300;
 
+void test_source_full::cleanup()
+{
+    m_testRunner->getModManFacade()->clearModuleSystems();
+    m_testRunner->getModManFacade()->getEventHandler()->clearSubsystems();
+    if(m_netClient)
+        m_netClient->getEventHandler()->clearSubsystems();
+    TimeMachineObject::feedEventLoop();
+    m_testRunner.reset();
+    m_netClient.reset();
+    m_entityItem.reset();
+    m_serverTcpSystem.reset();
+    m_serverNetSystem.reset();
+}
+
 void test_source_full::entity_avail_mt310s2()
 {
-    ModuleManagerTestRunner testRunner(":/sessions/minimal.json", false, "mt310s2");
-    VeinStorage::AbstractEventSystem* veinStorage = testRunner.getVeinStorageSystem();
+    setupServerAndClient(":/sessions/minimal.json", "mt310s2");
+    VeinStorage::AbstractEventSystem* veinStorage = m_testRunner->getVeinStorageSystem();
     QList<int> entityList = veinStorage->getDb()->getEntityList();
     QCOMPARE(entityList.count(), 2);
     QVERIFY(veinStorage->getDb()->hasEntity(sourceEntityId));
@@ -19,8 +36,8 @@ void test_source_full::entity_avail_mt310s2()
 
 void test_source_full::entity_avail_mt581s2()
 {
-    ModuleManagerTestRunner testRunner(":/sessions/minimal.json", false, "mt581s2");
-    VeinStorage::AbstractEventSystem* veinStorage = testRunner.getVeinStorageSystem();
+    setupServerAndClient(":/sessions/minimal.json", "mt581s2");
+    VeinStorage::AbstractEventSystem* veinStorage = m_testRunner->getVeinStorageSystem();
     QList<int> entityList = veinStorage->getDb()->getEntityList();
     QCOMPARE(entityList.count(), 2);
     QVERIFY(veinStorage->getDb()->hasEntity(sourceEntityId));
@@ -28,9 +45,9 @@ void test_source_full::entity_avail_mt581s2()
 
 void test_source_full::vein_dump_mt310s2()
 {
-    ModuleManagerTestRunner testRunner(":/sessions/minimal.json", false, "mt310s2");
+    setupServerAndClient(":/sessions/minimal.json", "mt310s2");
     QByteArray jsonExpected = TestLogHelpers::loadFile(":/veinDumps/dumpInitialMt310s2.json");
-    VeinStorage::AbstractEventSystem* veinStorage = testRunner.getVeinStorageSystem();
+    VeinStorage::AbstractEventSystem* veinStorage = m_testRunner->getVeinStorageSystem();
 
     QByteArray jsonDumped = VeinStorage::DumpJson::dumpToByteArray(veinStorage->getDb(), QList<int>() << sourceEntityId);
     QVERIFY(TestLogHelpers::compareAndLogOnDiff(jsonExpected, jsonDumped));
@@ -38,10 +55,89 @@ void test_source_full::vein_dump_mt310s2()
 
 void test_source_full::vein_dump_mt581s2()
 {
-    ModuleManagerTestRunner testRunner(":/sessions/minimal.json", false, "mt581s2");
+    setupServerAndClient(":/sessions/minimal.json", "mt581s2");
     QByteArray jsonExpected = TestLogHelpers::loadFile(":/veinDumps/dumpInitialMt581s2.json");
-    VeinStorage::AbstractEventSystem* veinStorage = testRunner.getVeinStorageSystem();
+    VeinStorage::AbstractEventSystem* veinStorage = m_testRunner->getVeinStorageSystem();
 
     QByteArray jsonDumped = VeinStorage::DumpJson::dumpToByteArray(veinStorage->getDb(), QList<int>() << sourceEntityId);
     QVERIFY(TestLogHelpers::compareAndLogOnDiff(jsonExpected, jsonDumped));
+}
+
+void test_source_full::switch_on_mt310s2()
+{
+    setupServerAndClient(":/sessions/minimal.json", "mt310s2");
+    setDemoSourceCount(1);
+    QJsonObject jsonEventDump;
+    setupServerAndClientSpies(jsonEventDump);
+
+    VeinStorage::AbstractEventSystem* veinStorage = m_testRunner->getVeinStorageSystem();
+    VeinStorage::StorageComponentPtr storedState = veinStorage->getDb()->findComponent(sourceEntityId, "PAR_SourceState0");
+    JsonParamApi paramApi;
+    QJsonObject oldValue = storedState->getValue().toJsonObject();
+    paramApi.setParams(oldValue);
+    paramApi.setOn(true);
+
+    QEvent* event = VfClientComponentSetter::generateEvent(sourceEntityId, "PAR_SourceState0",
+                                                           oldValue, paramApi.getParams());
+    m_entityItem->sendEvent(event);
+    TimeMachineForTest::getInstance()->processTimers(5000);
+
+    QFile file(":/veinEventDumps/dumpSwitchOnMt310s2.json");
+    QVERIFY(file.open(QFile::ReadOnly));
+    QByteArray jsonExpected = file.readAll();
+    QByteArray jsonDumped = TestLogHelpers::dump(jsonEventDump);
+    QVERIFY(TestLogHelpers::compareAndLogOnDiff(jsonExpected, jsonDumped));
+}
+
+void test_source_full::setupServerAndClient(const QString &session, const QString &dut)
+{
+    constexpr int veinServerPort = 12000;
+    m_testRunner = std::make_unique<ModuleManagerTestRunner>(session, false, dut);
+    m_serverNetSystem = std::make_unique<VeinNet::NetworkSystem>();
+    m_serverNetSystem->setOperationMode(VeinNet::NetworkSystem::VNOM_SUBSCRIPTION);
+    m_serverTcpSystem = std::make_unique<VeinNet::TcpSystem>(VeinTcp::MockTcpNetworkFactory::create());
+    ModuleManagerSetupFacade *modmanFacade = m_testRunner->getModManFacade();
+    modmanFacade->addSubsystem(m_serverNetSystem.get());
+    modmanFacade->addSubsystem(m_serverTcpSystem.get());
+    m_serverTcpSystem->startServer(veinServerPort);
+
+    m_netClient = std::make_unique<VfCoreStackClient>(VeinTcp::MockTcpNetworkFactory::create());
+    m_netClient->connectToServer("127.0.0.1", veinServerPort);
+    TimeMachineObject::feedEventLoop();
+    m_entityItem = VfEntityComponentEventItem::create(sourceEntityId);
+    m_netClient->addItem(m_entityItem);
+    m_netClient->subscribeEntity(sourceEntityId);
+    TimeMachineObject::feedEventLoop();
+    QCOMPARE(m_serverNetSystem->getSubscriberCount(sourceEntityId), 1);
+}
+
+void test_source_full::setupServerAndClientSpies(QJsonObject &jsonEvents)
+{
+    ModuleManagerSetupFacade *modmanFacade = m_testRunner->getModManFacade();
+
+    m_serverCmdEventSpyTop = std::make_unique<TestJsonSpyEventSystem>(&jsonEvents, "server-enter");
+    modmanFacade->prependModuleSystem(m_serverCmdEventSpyTop.get());
+    /*m_serverCmdEventSpyBottom = std::make_unique<TestJsonSpyEventSystem>(&jsonEvents, "server-fallthrough");
+    connect(modmanFacade->getEventHandler(), &VeinEvent::EventHandler::sigEventAccepted,
+            m_serverCmdEventSpyBottom.get(), &TestJsonSpyEventSystem::onEventAccepted);
+    modmanFacade->addSubsystem(m_serverCmdEventSpyBottom.get());*/
+
+    m_clientCmdEventSpyTop = std::make_unique<TestJsonSpyEventSystem>(&jsonEvents, "client-enter");
+    m_netClient->prependEventSystem(m_clientCmdEventSpyTop.get());
+    /*m_clientCmdEventSpyBottom = std::make_unique<TestJsonSpyEventSystem>(&jsonEvents, "client-fallthrough");
+    connect(m_netClient->getEventHandler(), &VeinEvent::EventHandler::sigEventAccepted,
+            m_clientCmdEventSpyBottom.get(), &TestJsonSpyEventSystem::onEventAccepted);
+    m_netClient->appendEventSystem(m_clientCmdEventSpyBottom.get());*/
+}
+
+void test_source_full::setDemoSourceCount(int sourceCount)
+{
+    VeinStorage::AbstractEventSystem* veinStorage = m_testRunner->getVeinStorageSystem();
+    VeinStorage::StorageComponentPtr stored = veinStorage->getDb()->findComponent(sourceEntityId, "PAR_DemoSources");
+    QVariant oldValue = stored->getValue();
+
+    QEvent* event = VfClientComponentSetter::generateEvent(sourceEntityId, "PAR_DemoSources",
+                                                           oldValue, sourceCount);
+    m_entityItem->sendEvent(event);
+    TimeMachineObject::feedEventLoop();
 }
--- modules/sourcemodule/tests/qtest/test_source_full.h
+++ modules/sourcemodule/tests/qtest/test_source_full.h
@@ -1,16 +1,39 @@
 #ifndef TEST_SOURCE_FULL_H
 #define TEST_SOURCE_FULL_H
 
-#include <QObject>
+#include "modulemanagertestrunner.h"
+#include "vf_core_stack_client.h"
+#include "vf_entity_component_event_item.h"
+#include "testjsonspyeventsystem.h"
+#include <QJsonObject>
+#include <memory>
 
 class test_source_full : public QObject
 {
     Q_OBJECT
 private slots:
+    void cleanup();
+
     void entity_avail_mt310s2();
     void entity_avail_mt581s2();
     void vein_dump_mt310s2();
     void vein_dump_mt581s2();
+    void switch_on_mt310s2();
+private:
+    void setupServerAndClient(const QString &session, const QString &dut);
+    void setupServerAndClientSpies(QJsonObject &jsonEvents);
+    void setDemoSourceCount(int sourceCount);
+
+    std::unique_ptr<ModuleManagerTestRunner> m_testRunner;
+    std::unique_ptr<VeinNet::NetworkSystem> m_serverNetSystem;
+    std::unique_ptr<VeinNet::TcpSystem> m_serverTcpSystem;
+
+    std::unique_ptr<VfCoreStackClient> m_netClient;
+    VfCmdEventItemEntityPtr m_entityItem;
+    std::unique_ptr<TestJsonSpyEventSystem> m_clientCmdEventSpyTop;
+    std::unique_ptr<TestJsonSpyEventSystem> m_serverCmdEventSpyBottom;
+    std::unique_ptr<TestJsonSpyEventSystem> m_serverCmdEventSpyTop;
+    std::unique_ptr<TestJsonSpyEventSystem> m_clientCmdEventSpyBottom;
 };
 
 #endif // TEST_SOURCE_FULL_H
