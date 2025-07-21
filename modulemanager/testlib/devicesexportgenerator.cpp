bfa6f0fdae669b2335ea6575bc6d5817d9eb34d0
amutalik <a.mutalik@zera.de>
2025-07-21 11:25:10 +0200

    devicesexportgenerator: Simplify and provide only 'exportAll'
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- modulemanager/testlib/devicesexportgenerator.cpp
+++ modulemanager/testlib/devicesexportgenerator.cpp
@@ -8,22 +8,25 @@ DevicesExportGenerator::DevicesExportGenerator(LxdmSessionChangeParam lxdmParam)
 {
 }
 
-void DevicesExportGenerator::exportDevIfaceXmls(QString xmlDir)
-{
-    m_xmlDir = xmlDir;
-    QDir().mkdir(m_xmlDir);
-    exportEnabler selectionFlags {true, false, false};
-    exportSelected(selectionFlags);
-}
-
 void DevicesExportGenerator::exportAll(QString xmlDir, QString snapshotDir)
 {
     m_xmlDir = xmlDir;
     QDir().mkdir(m_xmlDir);
     m_snapshotDir = snapshotDir;
     QDir().mkdir(m_snapshotDir);
-    exportEnabler selectionFlags {true, true, true};
-    exportSelected(selectionFlags);
+
+    SessionExportGenerator sessionExportGenerator(m_lxdmParam);
+    QStringList devices = {"mt310s2", "com5003"};
+    for(const QString &device: devices) {
+        sessionExportGenerator.setDevice(device);
+        for(const QString &session: sessionExportGenerator.getAvailableSessions()) {
+            sessionExportGenerator.changeSession(session);
+            sessionExportGenerator.generateDevIfaceXml(m_xmlDir);
+            m_veinDumps[session] = sessionExportGenerator.getVeinDump();
+            sessionExportGenerator.generateSnapshotJsons(m_snapshotDir);
+        }
+        m_instanceCounts.append(sessionExportGenerator.getInstanceCountsOnModulesDestroyed());
+    }
 }
 
 VeinDumps DevicesExportGenerator::getVeinDumps()
@@ -34,23 +37,4 @@ VeinDumps DevicesExportGenerator::getVeinDumps()
 QList<TestModuleManager::TModuleInstances> DevicesExportGenerator::getInstanceCountsOnModulesDestroyed()
 {
     return m_instanceCounts;
-}
-
-void DevicesExportGenerator::exportSelected(exportEnabler selectionFlags)
-{
-    SessionExportGenerator sessionExportGenerator(m_lxdmParam);
-    QStringList devices = {"mt310s2", "com5003"};
-    for(const QString &device: devices) {
-        sessionExportGenerator.setDevice(device);
-        for(const QString &session: sessionExportGenerator.getAvailableSessions()) {
-            sessionExportGenerator.changeSession(session);
-            if(selectionFlags.exportXmls)
-                sessionExportGenerator.generateDevIfaceXml(m_xmlDir);
-            if(selectionFlags.exportVeindumps)
-                m_veinDumps[session] = sessionExportGenerator.getVeinDump();
-            if(selectionFlags.exportSnapshots)
-                sessionExportGenerator.generateSnapshotJsons(m_snapshotDir);
-        }
-        m_instanceCounts.append(sessionExportGenerator.getInstanceCountsOnModulesDestroyed());
-    }
 }
