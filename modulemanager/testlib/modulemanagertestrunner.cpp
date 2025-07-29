8d52ed1e0c3f2f438befcf6151cbeecf2cc293a5
amutalik <a.mutalik@zera.de>
2025-07-29 15:42:51 +0200

    sessionexportgenerator: Use 'ModuleManagerTestRunner'
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- modulemanager/testlib/modulemanagertestrunner.cpp
+++ modulemanager/testlib/modulemanagertestrunner.cpp
@@ -1,6 +1,11 @@
 #include "modulemanagertestrunner.h"
 #include "vf_client_component_setter.h"
+#include <contentsetsotherfromcontentsetsconfig.h>
+#include <contentsetszeraallfrommodmansessions.h>
+#include <loggercontentsetconfig.h>
+#include <testsqlitedb.h>
 #include <timemachineobject.h>
+#include <QDir>
 
 ModuleManagerTestRunner::ModuleManagerTestRunner(QString sessionFileName, bool initialAdjPermission, QString deviceName, LxdmSessionChangeParam lxdmParam)
 {
@@ -30,10 +35,52 @@ ModuleManagerTestRunner::~ModuleManagerTestRunner()
     m_licenseSystem = nullptr;
 }
 
+void ModuleManagerTestRunner::setupVfLogger()
+{
+    m_testSignaller = std::make_unique<TestDbAddSignaller>();
+    const VeinLogger::DBFactory sqliteFactory = [this]() {
+        return std::make_shared<TestSQLiteDB>(m_testSignaller.get());
+    };
+
+    m_dataLoggerSystem = std::make_unique<VeinLogger::DatabaseLogger>(
+        m_modmanFacade->getStorageSystem(),
+        sqliteFactory,
+        this,
+        QList<int>()
+            << 0    /* SYSTEM */
+            << 1150 /* STATUS */);
+
+    QString OE_MODMAN_CONTENTSET_PATH = QDir::cleanPath(QString(OE_INSTALL_ROOT) + "/" + QString(MODMAN_CONTENTSET_PATH));
+    QString OE_MODMAN_SESSION_PATH = QDir::cleanPath(QString(OE_INSTALL_ROOT) + "/" + QString(MODMAN_SESSION_PATH));
+    VeinLogger::LoggerContentSetConfig::setJsonEnvironment(OE_MODMAN_CONTENTSET_PATH, std::make_shared<ContentSetsOtherFromContentSetsConfig>());
+    VeinLogger::LoggerContentSetConfig::setJsonEnvironment(OE_MODMAN_SESSION_PATH, std::make_shared<ContentSetsZeraAllFromModmanSessions>());
+
+    m_dataLoggerSystemInitialized = false;
+    connect(m_modmanFacade->getLicenseSystem(), &LicenseSystemInterface::sigSerialNumberInitialized, this, [&](){
+        if(m_licenseSystem->isSystemLicensed(m_dataLoggerSystem->entityName()))
+        {
+            if(!m_dataLoggerSystemInitialized)
+            {
+                m_dataLoggerSystemInitialized = true;
+                qInfo("Starting DataLoggerSystem...");
+                m_modmanFacade->addSubsystem(m_dataLoggerSystem.get());
+            }
+        }
+    });
+
+    m_modmanFacade->getLicenseSystem()->setDeviceSerial("foo");
+    TimeMachineObject::feedEventLoop();
+}
+
 void ModuleManagerTestRunner::start(QString sessionFileName)
 {
-    m_modMan->loadSession(sessionFileName);
+    m_modMan->changeSessionFile(sessionFileName);
     m_modMan->waitUntilModulesAreReady();
+}
+
+QList<TestModuleManager::TModuleInstances> ModuleManagerTestRunner::getInstanceCountsOnModulesDestroyed()
+{
+    return m_modMan->getInstanceCountsOnModulesDestroyed();
 }
 
 VeinStorage::AbstractEventSystem *ModuleManagerTestRunner::getVeinStorageSystem()
