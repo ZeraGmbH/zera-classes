03aff5a19cc4063e4636ab1daef7c2ffb19b0fe8
Asmae Kaouissi <a.kaouissi@zera.de>
2025-10-10 09:48:05 +0200

    add RPCReadLockState
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

---
+++ modules/sem1module/lib/rpc/rpcreadlockstate.h
@@ -0,0 +1,19 @@
+#ifndef RPCREADLOCKSTATE_H
+#define RPCREADLOCKSTATE_H
+
+#include "vfeventsytemmoduleparam.h"
+#include <vf-cpp-rpc-simplified.h>
+
+class RPCReadLockState : public VfCpp::VfCppRpcSimplified
+{
+    Q_OBJECT
+public:
+    RPCReadLockState(VfEventSytemModuleParam *eventSystem, int entityId);
+    void onReadLockStateCompleted(const QUuid &callId, bool success, QString errorMsg, QVariant value);
+signals:
+    void sigReadLockState(const QUuid &callId);
+private slots:
+    void callRPCFunction(const QUuid &callId, const QVariantMap &parameters) override;
+};
+
+#endif // RPCREADLOCKSTATE_H
