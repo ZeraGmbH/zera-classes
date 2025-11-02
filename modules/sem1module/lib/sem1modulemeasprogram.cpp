a72467db9db4d36dd3d1375b4daf0840e2c40484
Andreas Müller <schnitzeltony@gmail.com>
2025-11-02 19:16:08 +0100

    Sync EMOB lock state description to sec/sem/.. measurement state
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/sem1module/lib/sem1modulemeasprogram.cpp
+++ modules/sem1module/lib/sem1modulemeasprogram.cpp
@@ -373,7 +373,7 @@ void cSem1ModuleMeasProgram::generateVeinInterface()
     std::shared_ptr<RPCReadLockState> rpcEmobReadLockState = std::make_shared<RPCReadLockState>(m_pcbInterface, rpcEventSystem, m_pModule->getEntityId());
     rpcEventSystem->addRpc(rpcEmobReadLockState);
     m_pEmobLockStateRpc = std::make_shared<VfModuleRpc>(rpcEmobReadLockState,
-                                          "Query EMOB plug log state 0:unknown / 1:open / 2:locking / 3:locked / 3:error");
+                                          "EMOB plug lock state: 0:unknown 1:open 2:locking 3:locked 4:error");
     m_pEmobLockStateRpc->setRPCScpiInfo("CALCULATE",
                                         QString("%1:EMLOCKSTATE").arg(modNr),
                                         SCPI::isQuery,
