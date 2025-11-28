574d6a3fddcd1dd7e6ef506053b0e8f725476dba
Andreas Müller <schnitzeltony@gmail.com>
2025-11-28 23:17:55 +0100

    Add MockDspInterface from zenux-services
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ modules-common/service-interfaces/demolib/mockdspinterface.cpp
@@ -0,0 +1,40 @@
+#include "mockdspinterface.h"
+#include "notzeronumgen.h"
+#include "reply.h"
+
+void MockDspInterface::fireActValInterrupt(QVector<float> actualValues, int irqNo)
+{
+    m_actualValues = actualValues;
+    // Dsp sends DSPINT:%1. Example clients do not seem to care about
+    // 'DSPINT': they look just for irq number (after ':'). See how this
+    // evolves once working on SEC.
+    QString message = QString("DSPINT:%1").arg(irqNo);
+    constexpr quint32 messageNum = 0; // async interrupt
+    emit serverAnswer(messageNum, ack, message);
+}
+
+quint32 MockDspInterface::dataAcquisition(cDspMeasData *memgroup)
+{
+    memgroup->setData(m_actualValues);
+    return sendCmdResponse("");
+}
+
+quint32 MockDspInterface::activateInterface()
+{
+    quint32 ret = Zera::cDSPInterface::activateInterface();
+    emit sigDspStarted(); // How can we get rid of this??
+    return ret;
+}
+
+quint32 MockDspInterface::sendCmdResponse(QString answer)
+{
+    quint32 messageNum = NotZeroNumGen::getMsgNr();
+    QMetaObject::invokeMethod(this,
+                              "serverAnswer",
+                              Qt::QueuedConnection,
+                              Q_ARG(quint32, messageNum),
+                              Q_ARG(quint8, ack),
+                              Q_ARG(QVariant, QVariant(answer))
+                              );
+    return messageNum;
+}
---
+++ modules-common/service-interfaces/demolib/mockdspinterface.h
@@ -0,0 +1,25 @@
+#ifndef MOCKDSPINTERFACE_H
+#define MOCKDSPINTERFACE_H
+
+#include "dspinterface.h"
+
+class MockDspInterface : public Zera::cDSPInterface
+{
+    Q_OBJECT
+public:
+    void fireActValInterrupt(QVector<float> actualValues, int irqNo);
+    quint32 dataAcquisition(cDspMeasData* memgroup) override;
+    quint32 activateInterface() override;
+
+signals:
+    void sigDspMemoryWrite(QString name, QVector<float> values);
+    void sigDspStarted();
+protected:
+    quint32 sendCmdResponse(QString answer);
+private:
+    QVector<float> m_actualValues;
+};
+
+typedef std::shared_ptr<MockDspInterface> MockDspInterfacePtr;
+
+#endif // MOCKDSPINTERFACE_H
