7f7d03fd3462e31e11d90d79d2d374ca805e1c21
Andreas Müller <schnitzeltony@gmail.com>
2026-03-07 12:55:25 +0100

    adjustvalidator: Move code out of last century - most notable: avoid/fix mem-leaks
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/adjustmentmodule/lib/adjustvalidator.cpp
+++ modules/adjustmentmodule/lib/adjustvalidator.cpp
@@ -1,37 +1,20 @@
+#include "adjustmentmodulemeasprogram.h"
+#include "adjustvalidator.h"
 #include <QJsonArray>
 #include <QJsonObject>
 
-#include "adjustmentmodulemeasprogram.h"
-#include "adjustvalidator.h"
-
-cAdjustValidator3d::cAdjustValidator3d(cAdjustmentModuleMeasProgram *measprogram)
-    :m_pMeasprogram(measprogram)
+cAdjustValidator3d::cAdjustValidator3d(cAdjustmentModuleMeasProgram *measprogram) :
+    m_pMeasprogram(measprogram)
 {
 }
 
-cAdjustValidator3d::~cAdjustValidator3d()
+void cAdjustValidator3d::addValidator(const QString &chnName, const QStringList &rList, const cDoubleValidator &parVal)
 {
-    QStringList keylist;
-
-    keylist = m_adjustValidatorHash.keys();
-    for (int i = 0; i < keylist.count(); i++)
-    {
-        adjValidatorDatad* adjValidator;
-        adjValidator = m_adjustValidatorHash[keylist.at(i)];
-        delete adjValidator;
-    }
-}
-
-
-void cAdjustValidator3d::addValidator(QString chnName, QStringList rList, cDoubleValidator parVal)
-{
-    adjValidatorDatad* adjValidator = new adjValidatorDatad();
-    adjValidator->rangeList = rList;
-    adjValidator->dValidator = parVal;
-
+    adjValidatorDatad adjValidator;
+    adjValidator.rangeList = rList;
+    adjValidator.dValidator = parVal;
     m_adjustValidatorHash[chnName] = adjValidator;
 }
-
 
 bool cAdjustValidator3d::isValidParam(QVariant &newValue)
 {
@@ -39,17 +22,15 @@ bool cAdjustValidator3d::isValidParam(QVariant &newValue)
     if (sl.count() == 3) {
         QString key = sl.at(0);
         if (m_adjustValidatorHash.contains(key))
-            if (m_adjustValidatorHash[key]->rangeList.contains(sl.at(1))) {
-                QVariant var;
-                var = (QVariant)(sl.at(2).toDouble());
-                if (m_adjustValidatorHash[key]->dValidator.isValidParam(var))
+            if (m_adjustValidatorHash[key].rangeList.contains(sl.at(1))) {
+                QVariant var = (QVariant)(sl.at(2).toDouble());
+                if (m_adjustValidatorHash[key].dValidator.isValidParam(var))
                     return true;
             }
     }
     qWarning("cAdjustValidator3d: Param %s is invalid!", qPrintable(newValue.toString()));
     return false;
 }
-
 
 void cAdjustValidator3d::exportMetaData(QJsonObject &jsObj)
 {
@@ -59,39 +40,27 @@ void cAdjustValidator3d::exportMetaData(QJsonObject &jsObj)
     jsObj.insert("Data", jsonArr);
 }
 
-
-cAdjustValidator2::cAdjustValidator2(cAdjustmentModuleMeasProgram *measprogram)
-    :m_pMeasprogram(measprogram)
+cAdjustValidator2::cAdjustValidator2(cAdjustmentModuleMeasProgram *measprogram) :
+    m_pMeasprogram(measprogram)
 {
 }
 
-
-void cAdjustValidator2::addValidator(QString chnName, QStringList rList)
+void cAdjustValidator2::addValidator(const QString &chnName, const QStringList &rList)
 {
-    QStringList* sl;
-    sl = new QStringList(rList);
-
-    m_adjustValidatorHash[chnName] = sl;
+    m_adjustValidatorHash[chnName] = rList;
 }
-
 
 bool cAdjustValidator2::isValidParam(QVariant &newValue)
 {
-    QStringList sl;
-
-    sl = newValue.toString().split(',');
-    if (sl.count() == 2)
-    {
-        QString key;
-        key = sl.at(0);
+    QStringList sl = newValue.toString().split(',');
+    if (sl.count() == 2) {
+        QString key = sl.at(0);
         if (m_adjustValidatorHash.contains(key))
-            if (m_adjustValidatorHash[key]->contains(sl.at(1)))
+            if (m_adjustValidatorHash[key].contains(sl.at(1)))
                 return true;
     }
-
     return false;
 }
-
 
 void cAdjustValidator2::exportMetaData(QJsonObject &jsObj)
 {
@@ -101,51 +70,40 @@ void cAdjustValidator2::exportMetaData(QJsonObject &jsObj)
     jsObj.insert("Data", jsonArr);
 }
 
-
-cAdjustValidator3i::cAdjustValidator3i(cAdjustmentModuleMeasProgram *measprogram)
-    :m_pMeasprogram(measprogram)
+cAdjustValidator3i::cAdjustValidator3i(cAdjustmentModuleMeasProgram *measprogram) :
+    m_pMeasprogram(measprogram)
 {
 }
 
-
-cAdjustValidator3i::cAdjustValidator3i(const cAdjustValidator3i &ref)
+cAdjustValidator3i::cAdjustValidator3i(const cAdjustValidator3i &ref) :
+    m_adjustValidatorHash(ref.m_adjustValidatorHash)
 {
-    m_adjustValidatorHash = ref.m_adjustValidatorHash;
     m_pMeasprogram = ref.m_pMeasprogram;
 }
 
-
-void cAdjustValidator3i::addValidator(QString chnName, QStringList rList, cIntValidator parVal)
+void cAdjustValidator3i::addValidator(const QString &chnName, const QStringList &rList, const cIntValidator &parVal)
 {
-    adjValidatorDatai* adjValidator = new adjValidatorDatai();
-    adjValidator->rangeList = rList;
-    adjValidator->iValidator = parVal;
+    adjValidatorDatai adjValidator;
+    adjValidator.rangeList = rList;
+    adjValidator.iValidator = parVal;
 
     m_adjustValidatorHash[chnName] = adjValidator;
 }
 
-
 bool cAdjustValidator3i::isValidParam(QVariant &newValue)
 {
-    QStringList sl;
-
-    sl = newValue.toString().split(',');
-    if (sl.count() == 3)
-    {
-        QString key;
-        key = sl.at(0);
+    QStringList sl = newValue.toString().split(',');
+    if (sl.count() == 3) {
+        QString key = sl.at(0);
         if (m_adjustValidatorHash.contains(key))
-            if (m_adjustValidatorHash[key]->rangeList.contains(sl.at(1)))
-            {
+            if (m_adjustValidatorHash[key].rangeList.contains(sl.at(1))) {
                 QVariant var = (QVariant)(sl.at(2).toInt());
-                if (m_adjustValidatorHash[key]->iValidator.isValidParam(var))
+                if (m_adjustValidatorHash[key].iValidator.isValidParam(var))
                     return true;
             }
     }
-
     return false;
 }
-
 
 void cAdjustValidator3i::exportMetaData(QJsonObject &jsObj)
 {
@@ -160,7 +118,6 @@ bool cAdjustValidatorFine::isValidParam(QVariant &)
 {
     return true; // always good
 }
-
 
 void cAdjustValidatorFine::exportMetaData(QJsonObject &jsObj)
 {
--- modules/adjustmentmodule/lib/adjustvalidator.h
+++ modules/adjustmentmodule/lib/adjustvalidator.h
@@ -1,45 +1,38 @@
 #ifndef ADJUSTVALIDATOR
 #define ADJUSTVALIDATOR
 
+#include "adjustmentmodulemeasprogram.h"
+#include "doublevalidator.h"
+#include "intvalidator.h"
 #include <QString>
 #include <QStringList>
 #include <QHash>
 
-#include "adjustmentmodulemeasprogram.h"
-#include "doublevalidator.h"
-#include "intvalidator.h"
-
 class adjValidatorDatad
 {
 public:
-    adjValidatorDatad(){}
     QStringList rangeList;
     cDoubleValidator dValidator;
 };
 
-
 class adjValidatorDatai
 {
 public:
-    adjValidatorDatai(){}
     QStringList rangeList;
     cIntValidator iValidator;
 };
 
-
 class cAdjustValidator3d: public ValidatorInterface
 {
 public:
-    cAdjustValidator3d(cAdjustmentModuleMeasProgram *measprogram);
-    virtual ~cAdjustValidator3d();
+    explicit cAdjustValidator3d(cAdjustmentModuleMeasProgram *measprogram);
 
-    virtual void addValidator(QString chnName, QStringList rList, cDoubleValidator parVal);
-
-    virtual bool isValidParam(QVariant& newValue);
-    virtual void exportMetaData(QJsonObject& jsObj);
+    void addValidator(const QString &chnName, const QStringList &rList, const cDoubleValidator &parVal);
+    bool isValidParam(QVariant& newValue) override;
+    void exportMetaData(QJsonObject& jsObj) override;
 
 private:
-    QHash<QString, adjValidatorDatad*> m_adjustValidatorHash; // the channels name is the key
+    QHash<QString, adjValidatorDatad> m_adjustValidatorHash; // the channels name is the key
     cAdjustmentModuleMeasProgram *m_pMeasprogram;
 };
 
@@ -47,17 +40,15 @@ private:
 class cAdjustValidator3i: public ValidatorInterface
 {
 public:
-    cAdjustValidator3i(cAdjustmentModuleMeasProgram *measprogram);
-    cAdjustValidator3i(const cAdjustValidator3i& ref);
-    virtual ~cAdjustValidator3i(){}
+    explicit cAdjustValidator3i(cAdjustmentModuleMeasProgram *measprogram);
+    explicit cAdjustValidator3i(const cAdjustValidator3i& ref);
 
-    virtual void addValidator(QString chnName, QStringList rList, cIntValidator parVal);
-
-    virtual bool isValidParam(QVariant& newValue);
-    virtual void exportMetaData(QJsonObject& jsObj);
+    void addValidator(const QString &chnName, const QStringList &rList, const cIntValidator &parVal);
+    bool isValidParam(QVariant& newValue) override;
+    void exportMetaData(QJsonObject& jsObj) override;
 
 private:
-    QHash<QString, adjValidatorDatai*> m_adjustValidatorHash; // the channels name is the key
+    QHash<QString, adjValidatorDatai> m_adjustValidatorHash; // the channels name is the key
     cAdjustmentModuleMeasProgram *m_pMeasprogram;
 };
 
@@ -66,16 +57,14 @@ private:
 class cAdjustValidator2: public ValidatorInterface
 {
 public:
-    cAdjustValidator2(cAdjustmentModuleMeasProgram *measprogram);
-    virtual ~cAdjustValidator2(){}
+    explicit cAdjustValidator2(cAdjustmentModuleMeasProgram *measprogram);
 
-    virtual void addValidator(QString chnName, QStringList rList);
-
-    virtual bool isValidParam(QVariant& newValue);
-    virtual void exportMetaData(QJsonObject& jsObj);
+    void addValidator(const QString &chnName, const QStringList &rList);
+    bool isValidParam(QVariant& newValue) override;
+    void exportMetaData(QJsonObject& jsObj) override;
 
 private:
-    QHash<QString, QStringList*> m_adjustValidatorHash; // the channels name is the key
+    QHash<QString, QStringList> m_adjustValidatorHash; // the channels name is the key
     cAdjustmentModuleMeasProgram *m_pMeasprogram;
 };
 
@@ -83,11 +72,8 @@ private:
 class cAdjustValidatorFine: public ValidatorInterface
 {
 public:
-    cAdjustValidatorFine(){}
-    virtual ~cAdjustValidatorFine(){}
-
-    virtual bool isValidParam(QVariant& );
-    virtual void exportMetaData(QJsonObject& jsObj);
+    bool isValidParam(QVariant& ) override;
+    void exportMetaData(QJsonObject& jsObj) override;
 };
 
 #endif // ADJUSTVALIDATOR
